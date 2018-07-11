e215c53434b0a77f4f901e2bed12ed22d3b5b0e1
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-07-11 20:24:25 +0200

    how to improve it by cmake

---
+++ python/conda.recipe/treelas/conda_build.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+set -e
+set -x
+
+build_dir=build.conda.$BUILD
+config=Release
+
+mkdir $build_dir
+cd $build_dir
+cmake -G "$CMAKE_GENERATOR" \
+      -DBUILD_TESTS=OFF \
+      -DCMAKE_BUILD_TYPE=config \
+      -DCMAKE_CXX_FLAGS="$CXXFLAGS" \
+      -DPYBIND_CMAKE=~/.miniconda/share/cmake/pybind11 \
+      "$SRC_DIR"
+
+make -j $CPU_COUNT _treelas
+$STRIP _treelas*
+mkdir -p $SP_DIR/treelas
+cp "$SRC_DIR/python/treelas/*.py" $SP_DIR/treelas
+cp _treelas* $SP_DIR/treelas/
