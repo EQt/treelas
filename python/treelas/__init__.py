df8e2e4276b7a09a2c7bf8d7448433b27f513ffe
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-07-11 15:29:01 +0200

    started it

---
+++ python/treelas/__init__.py
@@ -0,0 +1,20 @@
+"""
+Treelas: a fused lasso solver for trees
+---------------------------------------
+
+See https://github.com/eqt/treelas
+"""
+import re
+# from .tree import Tree, TreeInstance
+# from .children import ChildrenIndex
+from ._treelas import (
+    __compiler__,
+    __author__
+)
+#     dp_tree, dp_tree_w,
+#     dp_dual,
+#     dp_gamma,
+#     post_order,
+#     dp_forward, dp_reverse,
+#     dp_line_w, glmgen_line
+# )
