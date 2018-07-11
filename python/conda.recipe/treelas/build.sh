ee5842fd511b5fb965af016cc10bce6207ea910d
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-07-11 20:21:23 +0200

    make it shorter

--- python/conda.recipe/treelas/build.sh
+++ python/conda.recipe/treelas/build.sh
@@ -2,20 +2,22 @@
 set -e
 set -x
 
-build_dir=build.conda.$BUILD
-config=Release
+# build_dir=build.conda.$BUILD
+# config=Release
 
-mkdir $build_dir
-cd $build_dir
-cmake -G "$CMAKE_GENERATOR" \
-      -DBUILD_TESTS=OFF \
-      -DCMAKE_BUILD_TYPE=config \
-      -DCMAKE_CXX_FLAGS="$CXXFLAGS" \
-      -DPYBIND_CMAKE=~/.miniconda/share/cmake/pybind11 \
-      "$SRC_DIR"
+# mkdir $build_dir
+# cd $build_dir
+# cmake -G "$CMAKE_GENERATOR" \
+#       -DBUILD_TESTS=OFF \
+#       -DCMAKE_BUILD_TYPE=config \
+#       -DCMAKE_CXX_FLAGS="$CXXFLAGS" \
+#       -DPYBIND_CMAKE=~/.miniconda/share/cmake/pybind11 \
+#       "$SRC_DIR"
 
-make -j $CPU_COUNT _treelas
-$STRIP _treelas*
-mkdir -p $SP_DIR/treelas
-cp "$SRC_DIR/python/treelas/*.py" $SP_DIR/treelas
-cp _treelas* $SP_DIR/treelas/
+# make -j $CPU_COUNT _treelas
+# $STRIP _treelas*
+# mkdir -p $SP_DIR/treelas
+# cp "$SRC_DIR/python/treelas/*.py" $SP_DIR/treelas
+# cp _treelas* $SP_DIR/treelas/
+
+$PYTHON setup.py install
