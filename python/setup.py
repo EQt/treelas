0bd919b3ddd5c726d07ce3f47759e191bf5d9503
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-07-15 19:02:30 +0200

    -std=c++11 flag

--- python/setup.py
+++ python/setup.py
@@ -1,8 +1,10 @@
 """
 http://www.benjack.io/2017/06/12/python-cpp-tests.html
 """
+import sys
 from setuptools import setup
 from setuptools.extension import Extension
+from setuptools.command.build_ext import build_ext
 from subprocess import check_output
 
 
@@ -26,7 +28,7 @@ class GetPyBindInc():
 _treelas = Extension("treelas._treelas",
                      ["_treelas.cpp",
                      ],
-                     language='c++',
+                     language='c++11',
                      include_dirs=[GetPyBindInc(False),
                                    GetPyBindInc(True)]
 )
@@ -40,12 +42,35 @@ def describe_tag(default):
         return default
 
 
+class BuildExt(build_ext):
+    """A custom build extension for adding compiler-specific options."""
+    c_opts = {
+        'msvc': ['/EHsc'],
+        'unix': [],
+    }
+
+    if sys.platform == 'darwin':
+        c_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
+
+    def build_extensions(self):
+        ct = self.compiler.compiler_type
+        opts = self.c_opts.get(ct, [])
+        if ct == 'unix':
+            opts.append('-std=c++11')
+            opts.append('-g0')
+            opts.append('-O3')
+        for ext in self.extensions:
+            ext.extra_compile_args = opts
+        build_ext.build_extensions(self)
+
+
 setup(name="treelas",
       version=describe_tag("v0.7"),
       author="Elias Kuthe",
       author_email="elias.kuthe@tu-dortmund.de",
       license="MIT",
+      packages=['treelas'],
       install_requires=['pybind11>=2.2'],
       ext_modules=[_treelas],
-      packages=['treelas']
+      cmdclass={'build_ext': BuildExt}
 )
