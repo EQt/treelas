683585914a7a0bd07731e59df57de552296cddb1
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-07-15 20:37:30 +0200

    also c++ support

--- extern/condat/condat_tv_v2.hpp
+++ extern/condat/condat_tv_v2.hpp
@@ -1,10 +1,14 @@
 #pragma once
 
+#ifdef __cplusplus
 extern "C" {
+#endif
 
 void TV1D_denoise_v2(const double* input,
                      double* output,
                      unsigned int width,
                      const double lambda);
 
+#ifdef __cplusplus
 }
+#endif
