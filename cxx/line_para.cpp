22cec6e265105b46585807425d91aaaa971e82f0
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-07-16 19:17:39 +0200

    started the parallel framework

---
+++ cxx/line_para.cpp
@@ -0,0 +1,22 @@
+#include "line_parallel.hpp"
+#incldue "line.hpp"
+
+template <typename float_>
+void
+line_para(const int n,
+          const float_ *y,
+          const float_ lam,
+          float_ *beta)
+{
+    
+}
+
+
+template
+void
+line_para(const int n,
+          const double *y,
+          const double lam,
+          double *beta);
+
+
---
+++ cxx/line_para.hpp
@@ -0,0 +1,19 @@
+/**
+   Parallel implementation.
+*/
+#pragma once
+
+template <typename float_ = double>
+void
+line_para(const int n,
+          const float_ *y,
+          const float_ lam,
+          float_ *beta);
+
+
+extern template
+void
+line_para(const int n,
+          const double *y,
+          const double lam,
+          double *beta);
