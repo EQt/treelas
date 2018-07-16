1b2efd7862d160892438c72a08a19c0303286ed8
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-07-16 11:54:49 +0200

    more line tests

--- python/test/test_condat.py
+++ python/test/test_condat.py
@@ -10,8 +10,18 @@ def test_condat1():
 
 
 def test_condat2():
+    """with pre-allocated output array"""
     y = np.array([1, 0, 0.5])
     lam = 0.5
     x = y.copy()
     line_condat(y, lam, x)
     assert (x == 0.5).all()
+
+
+def test_condat3():
+    """different lambda"""
+    y = np.array([1, 0, 0.5])
+    lam = 0.1
+    x = y.copy()
+    line_condat(y, lam, x)
+    assert (x == [0.9, 0.2, 0.4]).all()
--- python/test/test_glmgen.py
+++ python/test/test_glmgen.py
@@ -10,8 +10,19 @@ def test_glmgen1():
 
 
 def test_glmgen2():
+    """with pre-allocated output array"""
     y = np.array([1, 0, 0.5])
     lam = 0.5
     x = y.copy()
     line_glmgen(y, lam, x)
     assert (x == 0.5).all()
+
+
+def test_glmgen3():
+    """different lambda"""
+    y = np.array([1, 0, 0.5])
+    lam = 0.1
+    x = y.copy()
+    line_glmgen(y, lam, x)
+    expected = np.array([0.9, 0.2, 0.4])
+    assert (np.abs(x - expected) < 1e-16).all()
