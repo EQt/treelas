954af1d5d65e0e15e45fb049bef4120da7ec5cd6
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-07-17 15:39:37 +0200

    remove annoying warning

--- python/setup.py
+++ python/setup.py
@@ -64,6 +64,9 @@ class BuildExt(build_ext):
             opts.append('-std=c++11')
             opts.append('-g')
             opts.append('-O3')
+            if hasattr(self.compiler, "compiler"):
+                if self.compiler.compiler[0] == 'gcc':
+                    self.compiler.compiler_so.remove('-Wstrict-prototypes')
         for ext in self.extensions:
             ext.extra_compile_args = opts
         build_ext.build_extensions(self)
