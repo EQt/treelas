20ab62b921edde2d51111792c1b05657100ba717
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-07-17 21:31:30 +0200

    moved that code

--- cxx/line.cpp
+++ cxx/line.cpp
@@ -213,28 +213,3 @@ dp_line(const size_t n,
         const double *y,
         const double lam,
         const bool);
-
-
-
-#ifndef PARALLEL
-#  define PARALLEL 0
-#endif
-#if PARALLEL
-
-#include <thread>
-
-// https://github.com/uchicago-cs/cmsc12300/tree/master/examples/cpp/concurrency/simple
-
-void
-dp_line_par()
-{
-    const int n0 = n / 2;
-    const int n1 = n - n0;
-    std::thread t0 (dp_forward, n0, lb, ub);
-    std::thread t1 (dp_forward, n1, lb, ub);
-
-    t0.join();
-    t1.join();
-}
-
-#endif
--- cxx/line_para.hpp
+++ cxx/line_para.hpp
@@ -17,3 +17,27 @@ line_para(const int n,
           const double *y,
           const double lam,
           double *beta);
+
+
+#ifndef PARALLEL
+#  define PARALLEL 0
+#endif
+#if PARALLEL
+
+#include <thread>
+
+// https://github.com/uchicago-cs/cmsc12300/tree/master/examples/cpp/concurrency/simple
+
+void
+dp_line_par()
+{
+    const int n0 = n / 2;
+    const int n1 = n - n0;
+    std::thread t0 (dp_forward, n0, lb, ub);
+    std::thread t1 (dp_forward, n1, lb, ub);
+
+    t0.join();
+    t1.join();
+}
+
+#endif
