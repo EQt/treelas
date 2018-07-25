a3904eeeaf71c3ba21277cf798e8f7fa1bc40fe0
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-07-25 22:16:26 +0200

    started it all...

---
+++ python/test/test_dual.py
@@ -0,0 +1,15 @@
+import numpy as np
+import treelas as tl
+
+
+def test_dual3(root=0):
+    """Tree with 3 nodes"""
+    parent = np.array([0, 0, 0], dtype=np.int32)
+    x = np.array([-3., 1., 2])
+    assert x.mean() == 0.0
+    alpha = tl.dp_dual(parent, x)
+    assert alpha.shape == x.shape
+    assert np.isnan(alpha[root])
+    assert root == 0
+    assert (alpha[1:] == [-1., -2.]).all()
+    assert x[root] == 0.0
