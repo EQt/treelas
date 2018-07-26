fb95a4041982b2e92a01549ed7f46b4983a6d4bc
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-07-26 23:42:43 +0200

    wip

--- python/test/test_adjidx.py
+++ python/test/test_adjidx.py
@@ -4,9 +4,13 @@ from treelas.adjidx import BiAdjecent
 
 def test_1():
     head = np.array([0, 1, 2, 3], dtype=np.int32)
-    tail = np.array([1, 1, 1, 2], dtype=np.int32)
+    tail = np.array([1, 3, 1, 2], dtype=np.int32)
     index = BiAdjecent(head, tail)
     assert repr(index) == "BiAdjacent[m = 4, n = 4]"
 
     i2 = index[2]
     assert len(i2) == 2
+
+    assert list(i2) == [1, 3]
+    assert list(index[0]) == [1]
+    assert list(index[1]) == [0, 3, 2]
