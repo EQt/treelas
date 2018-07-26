be4c79e19dcec7310b32573796ccb007383e20b5
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-07-26 18:01:22 +0200

    it is more a stack...

--- cxx/prim_mst.hpp
+++ cxx/prim_mst.hpp
@@ -10,23 +10,23 @@ random_spanning_tree(const BiAdjacent &index,
     const auto n = index.num_nodes();
 
     std::vector<int> parent (n, -1);
-    std::vector<int> queue_ (n, -1);
+    std::vector<int> stack_ (n, -1);
 
-    int *queue = queue_.data();
+    int *stack = stack_.data();
     std::mt19937_64 rng (seed);
 
 
     int root = int(rng() % n);
     int len = 0;
-    queue[len++] = root;
+    stack[len++] = root;
     while (len > 0) {
-        const int v = queue[--len];
+        const int v = stack[--len];
         for (const int u : index[v]) {
             if (parent[u] < 0) {
                 parent[u] = v;
-                queue[len++] = u;
+                stack[len++] = u;
                 const int i = int(rng() % len);
-                std::swap(queue[i], queue[len-1]);
+                std::swap(stack[i], stack[len-1]);
             }
         }
     }
