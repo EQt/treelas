b85cac6dd0a3c33c4e9ee61395e0863426ac7dc4
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-07-27 16:05:58 +0200

    wip

--- python/test/test_spanning.py
+++ python/test/test_spanning.py
@@ -6,5 +6,9 @@ from treelas._treelas import BiAdjacent, random_spanning_tree
 def test_clique5():
     edges = zip(*combinations(range(5), 2))
     head, tail = map(lambda l: np.array(l, dtype=np.int32), edges)
+    assert (head < tail).all()
     index = BiAdjacent(head, tail)
+    # parent = -2*np.ones(5, dtype=np.int32)
+    assert list(index[4]) == [0, 1, 2, 3]
     parent = random_spanning_tree(index)
+    assert (parent == [1,2,3,4,5]).all(), parent
