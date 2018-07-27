9b218620ddffe5a70dae6988429b31f8c4acac7b
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-07-27 19:33:18 +0200

    clean up

--- python/_treelas.cpp
+++ python/_treelas.cpp
@@ -18,20 +18,6 @@ void reg_biadjacent(py::module &m);
 void reg_spanning(py::module &m);
 
 
-/**
-   For testing: create an array with 3 elements
- */
-py::array_t<double>
-_test_create_array()
-{
-    double *x = new double[3];
-    x[0] = 13.0;
-    x[1] = -1.0;
-    x[2] = 42.0;
-    return py::array_t<double>({3}, {sizeof(double)}, x);
-}
-
-
 PYBIND11_MODULE(_treelas, m)
 {
     m.doc() = R"pbdoc(
@@ -49,9 +35,18 @@ PYBIND11_MODULE(_treelas, m)
     m.attr("__author__") = "Elias Kuthe <elias.kuthe@tu-dortmund.de>";
     m.attr("__compiler__") = compiler_info();
 
-    m.def("_test_create_array", &_test_create_array, R"pbdoc(
-        Test to create an array with elements [13., -1., 42]
-    )pbdoc");
+    m.def("_test_create_array",
+          []() -> py::array_f64
+          {
+              double *x = new double[3];
+              x[0] = 13.0;
+              x[1] = -1.0;
+              x[2] = 42.0;
+              return py::array_t<double>({3}, {sizeof(double)}, x);
+          },
+          R"pbdoc(
+              Test to create an array with elements [13., -1., 42]
+          )pbdoc");
 
     m.def("_empty_array_f64", []() { return py::array_t<double>(); }, R"pbdoc(
         Create an empty np.float64 array
@@ -71,8 +66,6 @@ PYBIND11_MODULE(_treelas, m)
       R"pbdoc(
         std::sort from libstdc++ (to be compared against numpy.sort)
       )pbdoc");
-
-    reg_line(m);
 
     m.def("prufer2parent",
           [](const py::array_i32 &prufer, py::array_i32 parent) -> py::tuple
@@ -104,6 +97,7 @@ PYBIND11_MODULE(_treelas, m)
           )pbdoc",
           py::arg("parent"));
 
+    reg_line(m);
     reg_tree(m);
     reg_biadjacent(m);
     reg_spanning(m);
