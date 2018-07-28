529a915cceb49bc430a88526e7a43b71c7a00f58
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-07-28 17:02:32 +0200

    reactivate tests

--- python/test/test_prufer.py
+++ python/test/test_prufer.py
@@ -1,6 +1,6 @@
 import numpy as np
 from treelas import _treelas as t
-# from treelas import Tree
+from treelas import Tree
 
 
 def test_prufer3():
@@ -10,22 +10,22 @@ def test_prufer3():
     assert r == 4
 
 
-# def test_tree_from_prufer3():
-#     p = np.array([2, 2, 3], np.int32)
-#     t = Tree.from_prufer(p)
-#     assert t.root == 4
-#     assert (t.parent == [2, 2, 3, 4, 4]).all()
+def test_tree_from_prufer3():
+    p = np.array([2, 2, 3], np.int32)
+    t = Tree.from_prufer(p)
+    assert t.root == 4
+    assert (t.parent == [2, 2, 3, 4, 4]).all()
 
 
-# if __name__ == '__main__':
-#     import argparse
+if __name__ == '__main__':
+    import argparse
 
-#     p = argparse.ArgumentParser(description=__doc__)
-#     p.add_argument('-s', '--seed', type=int, default=2018,
-#                    help='Random seed [default=2018]')
-#     args = p.parse_args()
+    p = argparse.ArgumentParser(description=__doc__)
+    p.add_argument('-s', '--seed', type=int, default=2018,
+                   help='Random seed [default=2018]')
+    args = p.parse_args()
 
-#     children_spec = np.array([2, 2, 3])
-#     t = Tree.generate(children_spec, seed=args.seed)
-#     print(f"Generated tree with {t.n} nodes")
-#     t.show()
+    children_spec = np.array([2, 2, 3])
+    t = Tree.generate(children_spec, seed=args.seed)
+    print(f"Generated tree with {t.n} nodes")
+    t.show()
