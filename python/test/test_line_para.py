25b132cb0ff2351d529a0847b7e887e0ff294597
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-07-30 18:49:39 +0200

    failing test for next project

---
+++ python/test/test_line_para.py
@@ -0,0 +1,28 @@
+import numpy as np
+from treelas import line_para
+
+
+def test_line1():
+    y = np.array([1, 0, 0.5])
+    lam = 0.5
+    x = line_para(y, lam)
+    assert (x == 0.5).all(), repr(x)
+
+
+def test_line2():
+    """with pre-allocated output array"""
+    y = np.array([1, 0, 0.5])
+    lam = 0.5
+    x = y.copy()
+    line_para(y, lam, x)
+    assert (x == 0.5).all()
+
+
+def test_line3():
+    """different lambda"""
+    y = np.array([1, 0, 0.5])
+    lam = 0.1
+    x = y.copy()
+    line_para(y, lam, x)
+    expected = np.array([0.9, 0.2, 0.4])
+    assert (np.abs(x - expected) < 1e-15).all()
