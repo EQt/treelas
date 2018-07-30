ffe2a0cfa4f2c18ca995a58e449012802b2a98ca
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-07-30 19:16:41 +0200

    vector.reserve

--- cxx/malloc_vs_new.cpp
+++ cxx/malloc_vs_new.cpp
@@ -21,6 +21,20 @@ test_malloc(const size_t n)
 
 
 void
+test_vector_res(const size_t n)
+{
+    Timer timer ("vector.reserve");
+    std::vector<char> a;
+    a.reserve(n);
+    char *b = a.data();
+    timer.stop();
+    for (size_t i = 0; i < n; i += 8)
+        b[i] = char(i);
+    Timer::startit("free");
+}
+
+
+void
 test_vector(const size_t n)
 {
     Timer timer ("vector");
@@ -41,6 +55,8 @@ main()
     for (size_t i = 1; i < 10; i++) {
         std::cout << "n = " << i * n << std::endl;
         test_vector(i*n);
+        Timer::stopit();
+        test_vector_res(i*n);
         Timer::stopit();
         test_malloc(i*n);
         Timer::stopit();
