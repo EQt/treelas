68c33d17bb699c92a557dcebe226985d2a26a18d
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-07-30 20:47:51 +0200

    also test increasing=True

--- python/test/test_line.py
+++ python/test/test_line.py
@@ -26,3 +26,29 @@ def test_line3():
     line_las(y, lam, x)
     expected = np.array([0.9, 0.2, 0.4])
     assert (np.abs(x - expected) < 1e-15).all()
+
+
+def test_line1_rev():
+    y = np.array([1, 0, 0.5])
+    lam = 0.5
+    x = line_las(y, lam, increasing=True)
+    assert (x == 0.5).all(), repr(x)
+
+
+def test_line2_rev():
+    """with pre-allocated output array"""
+    y = np.array([1, 0, 0.5])
+    lam = 0.5
+    x = y.copy()
+    line_las(y, lam, x, increasing=True)
+    assert (x == 0.5).all()
+
+
+def test_line3_rev():
+    """different lambda"""
+    y = np.array([1, 0, 0.5])
+    lam = 0.1
+    x = y.copy()
+    line_las(y, lam, x, increasing=True)
+    expected = np.array([0.9, 0.2, 0.4])
+    assert (np.abs(x - expected) < 1e-15).all()
