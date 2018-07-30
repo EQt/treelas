3229fdedec0d85ae10e1492c16a8c2d83ddf85c6
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-07-30 16:20:37 +0200

    wip

---
+++ cxx/utils/plotpwl.hpp
@@ -0,0 +1,55 @@
+#pragma once
+#include <sstream>          // for std::stringstream
+#include <iomanip>          // std::setw
+
+#include "../event.hpp"
+#include "../queue.hpp"
+
+
+/** end is exclusive! */
+std::string
+pwl_csv(const Event *begin, const Event *end, const double margin = 1.0)
+{
+    double sumo = 0.0;
+    for (auto it = begin; it != end; it++) {
+        sumo += it->offset;
+    }
+    double
+        offset = -sumo/2.0,
+        slope = 0.0,
+        x = begin->x - margin;
+
+    std::stringstream res;
+    auto out = [&] () {
+        double y = (x * slope + offset);
+        if (std::abs(y) < 1e-15)
+            y = 0.0;
+        res << std::showpos << x << ","
+            << std::showpos << y << std::endl;
+    };
+    out();
+    for (const Event *e = begin; e != end; e++) {
+        x = e->x;
+        offset += e->offset;
+        slope += e->slope;
+        out();
+    }
+    x += margin;
+    out();
+    return res.str();
+}
+
+
+/** Printout the knots given by events `els` in the range `q` */
+std::string
+pwl_csv(const Queue &q, const Event *els)
+{
+    return pwl_csv(els + q.start, els + q.stop + 1);
+}
+
+
+std::string
+pwl_csv(const Queue &q, const std::vector<Event> els)
+{
+    return pwl_csv(q, els.data());
+}
