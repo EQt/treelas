baa9f913a5385cf217a9a4743fc1c5f69b432e62
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-07-30 16:07:02 +0200

    more tests

---
+++ test/test_postorder.cpp
@@ -0,0 +1,50 @@
+#include <gtest/gtest.h>
+#include "../cxx/postorder.hpp"
+#include "../cxx/utils/perm.hpp"
+
+
+TEST(postorder, size)
+{
+    const std::vector<int> parent = {0, 0, 1, 2, 3, 0, 7, 8, 3, 8};
+    PostOrder pord (parent);
+    ASSERT_EQ(parent.size(), 10);
+    ASSERT_EQ(pord.size(), parent.size()-1);
+}
+
+
+TEST(postorder, size_with_root)
+{
+    const std::vector<int> parent = {0, 0, 1, 2, 3, 0, 7, 8, 3, 8};
+    int *postord = nullptr;
+    const int root = 0;
+    const bool include_root = true;
+    PostOrder pord (parent, postord, root, include_root);
+    ASSERT_EQ(parent.size(), 10);
+    ASSERT_EQ(pord.size(), parent.size());
+}
+
+
+TEST(postorder, is_perm)
+{
+    const std::vector<int> parent = {0, 0, 1, 2, 3, 0, 7, 8, 3, 8};
+    int *postord = nullptr;
+    const int root = 0;
+    const bool include_root = true;
+    PostOrder pord (parent, postord, root, include_root);
+    std::vector<int> vpord (pord.begin(), pord.end());
+    ASSERT_TRUE(is_perm_mut(vpord));
+}
+
+
+TEST(postorder, tree_mini)
+{
+    const std::vector<int> parent {0, 0, 1, 0, 1, 2};
+    const int n = parent.size();
+    const int root = 0;
+
+    std::vector<int> postord (n, -1);
+    post_order(n, parent.data(), root, postord.data());
+
+    const std::vector<int> expected_ord { 3, 4, 5, 2, 1, 0 };
+    ASSERT_EQ(postord, expected_ord);
+}
