ceff9b95a2cc7f9f3583f24ad45d345c3d1e36fd
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-07-30 20:25:08 +0200

    now, only hdf5 v1.10 works

--- python/test/test_hdf5.py
+++ python/test/test_hdf5.py
@@ -4,10 +4,9 @@ from pytest import mark
 from distutils.version import LooseVersion
 
 
-@mark.xfail
 def test_libversion():
     h5v = '.'.join(map(str, h5py.h5.get_libversion()))
-    assert h5v <= LooseVersion("1.8")
+    assert h5v >= LooseVersion("1.10")
 
 
 @mark.skip
