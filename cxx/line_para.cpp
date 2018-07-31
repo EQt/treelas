bd1922d3ef58d41c3fc0fec8a20a2facf995dab6
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-07-31 13:14:16 +0200

    first try

--- cxx/line_para.cpp
+++ cxx/line_para.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <thread>
 
 #include "utils/viostream.hpp"
 #include "utils/timer.hpp"
@@ -37,8 +38,10 @@ line_para(const size_t n,
     Queue pq1 {int(2*n0 + n1), int(2*n0 + n1-1)};
 
     {   Timer _ ("forward");
-        dp_forward(y, lam, lb, ub, event, pq0, 0, n0+1);
-        dp_reverse(y, lam, lb+1, ub+1, event, pq1, n0, n);
+        std::thread t0 (dp_forward, y, lam, lb+0, ub+0, event, pq0, size_t(0), size_t(n0+1));
+        std::thread t1 (dp_reverse, y, lam, lb+1, ub+1, event, pq1, n0, n);
+        t0.join();
+        t1.join();
     }
 
     Queue pq = merge(pq0, pq1, event);
@@ -69,8 +72,6 @@ line_para(const size_t n,
 #  define PARALLEL 0
 #endif
 #if PARALLEL
-
-#include <thread>
 
 // https://github.com/uchicago-cs/cmsc12300/tree/master/examples/cpp/concurrency/simple
 
