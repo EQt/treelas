869facc42bb3b72cac03be38f554edce024153f7
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-07-31 19:20:38 +0200

    another check

--- python/test/test_tree.py
+++ python/test/test_tree.py
@@ -19,14 +19,16 @@ def test_degree3():
 def test_tree5():
     parent = [0, 0, 1, 2, 3, 0, 7, 8, 3, 8]
     y = [8.2, 7.0, 9.5, 6.8, 5.8, 6.3, 4.3, 2.2, 1.2, 2.8]
+    lam = 1.0
     t = Tree(parent)
     assert t.root == 0
-    ti = TreeInstance(y, t.parent, lam=1.0)
+    ti = TreeInstance(y, t.parent, lam=lam)
     ti.solve()
     diff = np.abs(ti.x*3 - [22.7, 22.7, 22.7, 18.9, 18.9, 21.9,  9.9,  8.2,  8.2,  8.2])
     assert diff.max() < 1e-14, f'{diff}, x={ti.x}'
 
     alpha = ti.dual
+    assert (np.abs(alpha[1:]) <= lam).all()
     assert np.isnan(alpha[0])
     diff = alpha[1:] * 3 - [ 1.1,  2.8, -3. , -1.5, -3. , -3. , -1.4, -3. ,  0.2]
     assert np.abs(diff).max() < 1e-14
