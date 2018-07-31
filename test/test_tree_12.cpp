379d99724e45830deee3ed9db905385d467a9142
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-07-31 17:50:45 +0200

    remove that test

--- test/test_tree_12.cpp
+++ test/test_tree_12.cpp
@@ -47,39 +47,3 @@ TEST(Nodes, sizeof_node)
 {
     ASSERT_EQ(sizeof(approx::Node<float, int>), 3*sizeof(float) + sizeof(int));
 }
-
-
-
-// TEST(Nodes, init)
-// {
-//     const std::vector<int>
-//         parent {0, 0, 1, 0, 1, 2},
-//         order  {3, 4, 5, 2, 1, 0},
-//         iordex {5, 4, 3, 0, 1, 2};
-//     const int n = parent.size();
-
-//     const auto iorder = iperm(order);
-//     ASSERT_EQ(iorder, iordex);
-
-//     approx::Nodes<double, int> nodes;
-//     nodes.resize(n);
-//     ASSERT_EQ(nodes.ny.size(), n);
-//     ASSERT_EQ(nodes.nx.size(), n);
-//     ASSERT_EQ(nodes.deriv.size(), n);
-
-//     const std::vector<double> y {0, 1, 2, 3, 4, 5};
-//     ASSERT_EQ(y.size(), n);
-
-//     std::cout << "postorder=" << order;
-
-//     const double x0 = -42;
-//     init(nodes, parent, order, iorder, y, x0);
-//     for (int i = 0; i < n; i++) {
-//         const auto ii = order[i];
-//         ASSERT_EQ(-nodes.ny[i], double(ii))
-//             << "i=" << i << "; ii=" << ii << std::endl;
-//         ASSERT_EQ(nodes.nx[i], x0) << "i=" << i;
-//         ASSERT_EQ(nodes.parent(i), parent[ii])
-//             << "i=" << i << "; ii=" << ii << std::endl;
-//     }
-// }
