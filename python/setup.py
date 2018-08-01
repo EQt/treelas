67027621d02f41b31e37dcd086c1841df7c999b9
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-08-01 21:47:41 +0200

    strip option -s does not have an effect

--- python/setup.py
+++ python/setup.py
@@ -73,7 +73,6 @@ class BuildExt(build_ext):
             # opts.append('-g')
             opts.append('-O3')
             opts.append('-Wall')
-            opts.append('-s')
             if hasattr(self.compiler, "compiler"):
                 if self.compiler.compiler[0] in ['gcc', 'g++', 'c++']:
                     cso = self.compiler.compiler_so
