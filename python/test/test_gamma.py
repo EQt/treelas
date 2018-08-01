cfdcbd8a2883258634fc41c106b99ed84b749216
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-08-01 16:35:35 +0200

    more tests

--- python/test/test_gamma.py
+++ python/test/test_gamma.py
@@ -1,4 +1,4 @@
-from treelas import TreeInstance
+from treelas import TreeInstance, post_order
 import numpy as np
 
 
@@ -12,3 +12,20 @@ def test_gamma3():
     g = t.gamma
     assert (g >= 0).all(), f'gamma={g}'
     assert (g < 1e-10).all(), f'gamma={g}'
+
+
+def test_gamma_line3(eps=1e-14):
+    ti = TreeInstance(root=2,
+                      y = np.array([ 0.8, -0.6,  0. ]),
+                      mu = 1.0,
+                      lam = 0.2,
+                      parent = np.array([2, 0, 2], dtype=np.int32))
+
+    po = post_order(ti.parent, include_root=True)
+    assert (po == [1, 0, 2]).all()
+
+    ti.solve()
+    assert ((ti.x - ti.y)[po] / ti.lam).astype(int).tolist() == [1, -2, 1]
+
+    assert ti.gamma.min() >= -eps, ti.gamma.min()
+    assert ti.gamma.max() <= +eps, f'gamma = {ti.gamma.max()}\n{ti}'
