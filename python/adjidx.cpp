a6a65162841e1853ce2c70070e4e21590595e683
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-08-02 13:08:46 +0200

    make it a subclass

--- python/adjidx.cpp
+++ python/adjidx.cpp
@@ -11,8 +11,34 @@ void
 reg_biadjacent(py::module &m)
 {
     using IndexIter_int = IndexIter<int>;
+    using AdjacencyIndex_int = AdjacencyIndex<int>;
 
-    py::class_<BiAdjacent>(m, "BiAdjacent")
+    py::class_<AdjacencyIndex_int> PyAdjacencyIndex_int (m, "AdjacencyIndex_int");
+    PyAdjacencyIndex_int
+        // .def(py::init([](const py::array_i32 &value,
+        //                  const py::array_i32 &index)
+        //               {
+        //                   return new AdjacencyIndex_int(
+        //                       std::vector<int>(value.data(), value.data() + value.size()),
+        //                       std::vector<int>(index.data(), value.data() + value.size())
+        //                   );
+        //               }),
+        //      py::arg("value"),
+        //      py::arg("index"))
+        .def("__getitem__",
+             [](const AdjacencyIndex_int &b, const int i) -> IndexIter_int
+             {
+                 return b[i];
+             })
+        .def("__len__",
+             [](const AdjacencyIndex_int &self) -> size_t
+             {
+                 return self.size();
+             })
+
+        ;
+
+    py::class_<BiAdjacent> (m, "BiAdjacent", PyAdjacencyIndex_int)
         .def(py::init([](const py::array_i32 &head,
                          const py::array_i32 &tail)
                       {
@@ -28,11 +54,6 @@ reg_biadjacent(py::module &m)
                  return std::string("BiAdjacent[") +
                      "m = " + std::to_string(b.num_edges()) + ", " +
                      "n = " + std::to_string(b.num_nodes()) + "]";
-             })
-        .def("__getitem__",
-             [](const BiAdjacent &b, const int i) -> IndexIter_int
-             {
-                 return b[i];
              })
         ;
 
