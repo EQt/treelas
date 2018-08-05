0792c7956855137e30380a2545df42c0f492676d
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-08-05 20:11:30 +0200

    move code

--- cxx/tree_12.hpp
+++ cxx/tree_12.hpp
@@ -65,20 +65,6 @@ tree12_iter(Nodes<float_, int_> &nodes,
 
 template<typename float_, typename int_>
 void
-compute_dfs_orders(const size_t n,
-                   const int_ *parent,
-                   int_ *order,
-                   int_ *iorder,
-                   int_ *ipostord)
-{
-
-}
-
-
-
-
-template<typename float_, typename int_>
-void
 tree_12(const size_t n,
         const float_ *y,
         const float_ lam,
@@ -87,49 +73,7 @@ tree_12(const size_t n,
         const int_ *iorder,
         const int_ *ipostord,
         double *x,
-        const size_t max_iter = 20)
-{    
-    float_ y_min, y_max;
-    {   Timer _ ("Analyze y");
-        find_minmax(y, n, y_min, y_max);
-    }
-    float_
-        delta = float_(0.5 * (y_max - y_min)),
-        y_mid = float_(0.5 * (y_max + y_min));
-
-    std::vector<Node<float_, int_>> nodes;
-    {   Timer _ ("Init nodes");
-        nodes.resize(n);
-        for (size_t i = 0; i < n; i++) {
-            const int ii = order[i];
-            nodes[i].y = y[ii];
-            nodes[i].x = y_mid;
-            nodes[i].set_parent(ipostord[parent[ii]], true);
-        }
-    }
-
-    {   Timer _ ("Iterations:\n");
-        for (size_t it = 0; it < max_iter; it++) {
-            delta *= float_(0.5);
-            // if (n <= 20) {
-            //     printf("delta=%.4f;   x: ", delta);
-            //     for (size_t i = 0; i < n; i++)
-            //         x[order[i]] = nodes[i].x;
-            //     print(xv, 3, stdout);
-            // }
-            Timer::log("%2ld ...", it+1);
-            const auto changed = tree12_iter(nodes, iorder, delta, lam);
-            if (changed)
-                Timer::log("  %d", changed);
-            Timer::log("\n");
-        }
-    }
-
-    {   Timer _ ("Extract x");
-        for (size_t i = 0; i < n; i++)
-            x[order[i]] = nodes[i].x;
-    }
-}
+        const size_t max_iter = 20);
 
 
 template<typename float_>
