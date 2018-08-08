fd5b6f2d3ef2e5aede7a179fd117822c8a9fbf8e
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-08-08 17:15:11 +0200

    wip

--- cxx/tree_debug.hpp
+++ cxx/tree_debug.hpp
@@ -27,10 +27,10 @@
 #pragma once
 #define DEBUG 0
 
-#include <cstring>      // memcpy
-
 #include "event.hpp"
 #include "queue.hpp"
+#include "utils/hex.hpp"
+
 
 #ifdef DEBUG
 #  define CLIP_START()  const Queue pq0 (pq); const double slope0 = slope;
@@ -92,23 +92,6 @@
     }
 
 
-inline std::string
-hexify(uint64_t x)
-{
-    char h [2*sizeof(x) + 3 + 3 + 2];
-    snprintf(h, sizeof(h), "d64(0x%016lx)", x);
-    return std::string(h);
-}
-
-
-inline std::string
-hex(double x)
-{
-    static_assert(sizeof(uint64_t) == sizeof(double), "Wrong architecture");
-    uint64_t i;
-    memcpy(&i, &x, sizeof(uint64_t));
-    return hexify(i);
-}
 
 #else
 #  define CLIP_START() {}
---
+++ cxx/utils/hex.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+#include <cstring>      // for memcpy
+
+
+inline std::string
+hexify(uint64_t x)
+{
+    char h [2*sizeof(x) + 3 + 3 + 2];
+    snprintf(h, sizeof(h), "d64(0x%016lx)", x);
+    return std::string(h);
+}
+
+
+inline std::string
+hex(double x)
+{
+    static_assert(sizeof(uint64_t) == sizeof(double), "Wrong architecture");
+    uint64_t i;
+    memcpy(&i, &x, sizeof(uint64_t));
+    return hexify(i);
+}
