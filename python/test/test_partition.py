a2113ccb594117c6f4ad04dbd77a0edb76256a70
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-12-19 23:18:53 +0100

    found more difficult test case

--- python/test/test_partition.py
+++ python/test/test_partition.py
@@ -8,3 +8,9 @@ def test_num5():
 
     for i in range(3):
         assert set(np.where(num5 == i)[0]) == set(pidx[i]), f"i = {i}"
+
+
+def test_0001():
+    pidx = PartitionIndex([0, 0, 0, 1])
+    assert set(pidx[0]) == {0, 1, 2}
+    assert set(pidx[1]) == {3}
