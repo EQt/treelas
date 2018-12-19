24592b67a2d0394c091d0cef70bf1a1a23696dd2
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-12-19 23:16:18 +0100

    linear (vector) test case will make it easier

--- python/test/test_cluster.py
+++ python/test/test_cluster.py
@@ -5,10 +5,13 @@ from test_biadjacent import square
 
 def test_cluster_2x2():
     neigh = square()
-    x = np.array([[+0.0, 0.1],
-                  [-0.1, 1.1]], order='F')
+    x = np.array([+0.0, 0.1, -0.1, 1.1])
     pidx = cluster(x, neigh, eps=0.2)
     # assert len(pidx) == 2
     parts = set(map(frozenset, (pidx[0], pidx[1])))
     expect = set(map(frozenset, ([3], [0, 1, 2])))
     assert parts == expect
+
+
+if __name__ == '__main__':
+    test_cluster_2x2()
