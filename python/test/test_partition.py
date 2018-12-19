174cfae67fb358551d92367bddbd73d1470f8dc0
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-12-19 22:00:00 +0100

    fix tests

--- python/test/test_partition.py
+++ python/test/test_partition.py
@@ -3,8 +3,8 @@ from treelas.idx import PartitionIndex
 
 
 def test_num5():
-    num5 = [1, 2, 0, 0, 2]
+    num5 = np.array([1, 2, 0, 0, 2])
     pidx = PartitionIndex(num5)
 
     for i in range(3):
-        assert set(np.where(num5 == i)[0]) == set(pidx[i])
+        assert set(np.where(num5 == i)[0]) == set(pidx[i]), f"i = {i}"
