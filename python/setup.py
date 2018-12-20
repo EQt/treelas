a13a60557454b5004096670441f1134a801b2f55
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-12-20 16:46:59 +0100

    new version

--- python/setup.py
+++ python/setup.py
@@ -90,7 +90,7 @@ class BuildExt(build_ext):
 
 
 setup(name="treelas",
-      version=describe_tag(default="0.10.1"),
+      version=describe_tag(default="0.10.2"),
       author="Elias Kuthe",
       author_email="elias.kuthe@tu-dortmund.de",
       license="MIT",
