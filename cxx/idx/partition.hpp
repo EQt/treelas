018935e09fd80331fb7dfcd605e0a374ef90915a
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-12-20 16:28:59 +0100

    iterate over partitions

--- cxx/idx/partition.hpp
+++ cxx/idx/partition.hpp
@@ -2,6 +2,9 @@
 #include "index.hpp"
 #include "groupby.hpp"
 
+template <typename int_>
+class PartitionIter;           // forward declaration
+
 
 template <typename int_ = int>
 class PartitionIndex : public AdjacencyIndex<int_>
@@ -20,5 +23,41 @@ public:
                 n,
                 ungrouped,
                 int_(-1));
+    }
+
+    PartitionIter<int_> begin() const {
+        return {this, 0};
+    }
+
+    PartitionIter<int_> end() const {
+        return {this, this->size()};
+    }
+};
+
+
+template<typename int_ = int>
+struct PartitionIter
+{
+    const PartitionIndex<int_> *p;
+    size_t i;
+
+    PartitionIter(const PartitionIndex<int_> *p, size_t j) :
+        p(p), i(j) { }
+
+    IndexIter<int_> operator*() const {
+        const int_
+            *start = p->value.data() + p->index[i+0],
+            *stop  = p->value.data() + p->index[i+1];
+
+        return {start, stop};
+    }
+
+    PartitionIter<int_>& operator++() {
+        this->i++;
+        return *this;
+    }
+
+    bool operator==(const PartitionIter<int_> &other) const {
+        return other.i == this->i;
     }
 };
