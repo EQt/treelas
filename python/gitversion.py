8250aa037b3ede8ab6a2739fd88a40753c74a7e0
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-12-20 16:42:02 +0100

    append .dev<i> ?

--- python/gitversion.py
+++ python/gitversion.py
@@ -2,12 +2,12 @@ import re
 import subprocess as sp
 
 
-def describe_tag(default):
+def describe_tag(default, with_dev=True):
     try:
         ver = sp.check_output('git describe --tag'.split())
         ver = ver.decode().strip()
         m = re.match(r'v(?P<ver>\d+(\.\d+)*)((-(?P<dev>\d+))(-.*))?', ver)
-        if m.group('dev') is not None:
+        if with_dev and m.group('dev') is not None:
             return f"{m.group('ver')}.dev{m.group('dev')}"
         return m.group('ver')
     except sp.CalledProcessError:
