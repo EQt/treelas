73d28adbdfa7bf9da04c2daa555524c8e1b6d985
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-12-24 00:27:26 +0100

    started to create grid graph "index"

--- extern/graphidx/cxx/idx/grid.hpp
+++ extern/graphidx/cxx/idx/grid.hpp
@@ -1,1 +1,12 @@
 /** "Index" on a grid graph */
+#include <cstdint>
+
+
+class GridIndex
+{
+public:
+    GridIndex(int n1, int n2)  : n1(n1), n2(n2) { }
+
+private:
+    int n1, n2;
+};
---
+++ extern/graphidx/cxx/test/test_grid.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+
+#include "idx/grid.hpp"
+
+
+TEST(GridIndex, grid_3x2)
+{
+    GridIndex g(3, 2);
+}
