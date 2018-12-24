31320cc56e9a052f548d89bb5a14fac39597aff0
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-12-24 01:30:21 +0100

    binnotation

--- extern/graphidx/cxx/test/test_grid.cpp
+++ extern/graphidx/cxx/test/test_grid.cpp
@@ -7,3 +7,12 @@ TEST(GridIndex, grid_3x2)
 {
     GridIndex g(3, 2);
 }
+
+
+TEST(GridIndex, binnotation)
+{
+    ASSERT_EQ(0b1001, 9);
+    ASSERT_EQ(0b1000, 8);
+    ASSERT_EQ(0b0000, 0);
+}
+        
