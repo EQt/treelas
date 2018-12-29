83b79a1c0bd7fffa0b42d3ca3f812dfcf2638d97
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-12-29 20:39:28 +0100

    another test

--- deps/graphidx/julia/test/test_neighbors.jl
+++ deps/graphidx/julia/test/test_neighbors.jl
@@ -15,6 +15,7 @@ include("../src/neighbors.jl")
     @test Set(idx[4]) == Set([(2, 3), (3, 4)])
 end
 
+
 @testset "NeighborIndex: Square -1       " begin
     #       1  2  3  4
     head = [1, 1, 2, 0]
@@ -27,5 +28,17 @@ end
     @test Set(idx[4]) == Set([(2, 3)])
 end
 
+
+@testset "NeighborIndex: Test_1          " begin
+    #       1  2  3  4
+    head = [1, 2, 3, 4]
+    tail = [2, 4, 2, 3]
+    idx = NeighborIndex(4, head, tail)
+    @test length(idx) == 4
+    @test Set(idx[1]) == Set([(2, 1)])
+    @test Set(idx[2]) == Set([(1, 1), (4, 2), (3, 3)])
+    @test Set(idx[3]) == Set([(2, 3), (4, 4)])
+    @test Set(idx[4]) == Set([(2, 2), (3, 4)])
+end
 
 end
