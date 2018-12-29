b51be9718574d7703d736ddd9e661312b4d971e9
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-12-29 01:24:56 +0100

    start to construct a tree example

--- julia/test/test_dp_tree.jl
+++ julia/test/test_dp_tree.jl
@@ -1,5 +1,15 @@
 module TestDPTree
 include("../src/dp_tree.jl")
 
+using Test
+import .DPTree
+
+@testset "dp_tree: 1                     " begin
+    root = 1
+    pi = Int[0, 0, 1, 2, 3, 0, 7, 8, 3, 8] .+ 1
+    n = length(pi)
+    dfs = fill(-1, n)
+    t = DPTree.Tree(root, pi, dfs)
+end
 
 end
