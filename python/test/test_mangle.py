58a737ca08765118510e6ebd553d18d566a9addc
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-12-29 15:38:04 +0100

    define the problem

--- python/test/test_mangle.py
+++ python/test/test_mangle.py
@@ -18,12 +18,26 @@ def test_cluster_name():
     - `n1` is defined in cluster.cpp.o
     - `n2` is expected in _treelas.cpython*.so
 
+
+    Let's go through the complete parameter list:
+    - 'm' is `unsigned long` (first parameter, [4])
+    - 'P' is a pointer
+    - 'K' tags it as `const`, called `<CV-qualifier>` [5]
+    - 'd' is `double` [4]
+    - 'R' is a reference `<ref-qualifier>` [5]
+
     The final 'S' marks a substitution [1], i.e. S0_ refers to the
     second identifier (see example in [2]).
 
+    The 'T' probably marks a template parameter [3].
+
+    Both are valid. But why does the compiler choose different mangled names?
+
     [1]: https://itanium-cxx-abi.github.io/cxx-abi/abi.html#mangling-compression
     [2]: https://itanium-cxx-abi.github.io/cxx-abi/abi.html#mangle.seq-id
-
+    [3]: https://itanium-cxx-abi.github.io/cxx-abi/abi.html#mangle.template-param
+    [4]: https://itanium-cxx-abi.github.io/cxx-abi/abi.html#mangling-builtin
+    [5]: https://itanium-cxx-abi.github.io/cxx-abi/abi.html#mangle.qualified-type
     """
 
     n1 = "_Z7clusterIi10BiAdjacentE14PartitionIndexIT_EmPKdRKS0_di"
