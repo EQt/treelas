1c39d52205d1b9fb37371d7b0564171bce18f432
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-12-29 12:17:18 +0100

    old implementation

--- julia/src/order.jl
+++ julia/src/order.jl
@@ -6,16 +6,32 @@ function find_root(pi::Vector{Int})::Int
 end
 
 
-function dfs_finish(pi::Vector{Int}, root=0)
+function dfs_finish(parent::Vector{Int}, root=0)
     if root <= 0    # find root node
         root = find_root(pi)
     end
+    @assert parent[root] == root
 
-    n = length(pi)
-    stack = Vector{Int}()
-    sizehint!(stack, n)
-    push!(stack, root)
-    while !isempty(stack)
-        v = pop!(stack)
+    n = length(parent)
+    neigh = [Int[] for i=1:n]
+    for (u, v) in enumerate(parent)
+        push!(neigh[u], v)
+        push!(neigh[v], u)
     end
+    dfs = zeros(Int, n)
+    stack = Int[root]
+    sizehint!(stack, n)
+    time = 0
+    while !isempty(stack)
+        time += 1
+        v = pop!(stack)
+        dfs[time] = v
+        for u in neigh[v]
+            if parent[v] == u
+                continue
+            end
+            push!(stack, u)
+        end
+    end
+    return dfs
 end 
