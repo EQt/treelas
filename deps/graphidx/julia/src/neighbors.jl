e5f61bc7b8a3a2cbace26817147d7c963db26782
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-12-29 20:13:24 +0100

    update code

--- deps/graphidx/julia/src/neighbors.jl
+++ deps/graphidx/julia/src/neighbors.jl
@@ -6,21 +6,27 @@ end
 @inline Base.getindex(c::NeighborIndex, j::Int) =
     view(c.pi, c.idx[j]:c.idx[j+1]-1)
 
+Base.length(c::NeighborIndex) = length(c.pi)
 
-function compute_undirected_index(selected::Vector{Bool},
-                                  head::Vector{Int},
-                                  tail::Vector{Int},
-                                  n::Int)
-    m = length(selected)
-    @assert m >= n-1
+
+"""
+    NeighborIndex(n, head, tail)
+
+Construct an index for `n` nodes of the (undirected) neighbors given
+by the edges `zip(head, tail)`.
+If `head[i] <= 0` then edge `i` is excluded.
+""" 
+function NeighborIndex(n::Int, head::Vector{Int}, tail::Vector{Int})
+    m = length(head)
+    @assert length(head) == length(tail)
     pi = Vector{Tuple{Int,Int}}(undef, 2m)
-    idx = zeros(T, n+1)
-    for eii in selected         # compute degrees
-        ei = edges[eii]
-        idx[source(ei)] += 1
-        idx[target(ei)] += 1
+    idx = zeros(Int, n+1)
+    for (h, t) in zip(head, tail)  # compute degrees
+        h <= 0 && continue
+        idx[h] += 1
+        idx[t] += 1
     end
-    acc = 1                     # accumulate degrees ==> positions
+    acc = 1                        # accumulate degrees ==> positions
     deg_i = 0
     deg_ii = idx[1]
     for i = 1:n
@@ -31,9 +37,8 @@ function compute_undirected_index(selected::Vector{Bool},
     idx[n+1] = acc
     @assert(idx[end] + deg_i == 2m + 1,
             "idx[$(length(idx))]: $(idx[end] + deg_i) != $(2m + 1)")
-    for (i, eii) in enumerate(selected)
-        ei = edges[eii]
-        u, v = source(ei), target(ei)
+    for (i, (u, v)) in enumerate(zip(head, tail))
+        u <= 0 && continue
         pi[idx[u+1]] = (v, i)
         idx[u+1] += 1
         pi[idx[v+1]] = (u, i)
@@ -43,7 +48,7 @@ function compute_undirected_index(selected::Vector{Bool},
     return NeighborIndex(idx, pi)
 end
 
-
+#=
 function compute_undirected_index(edges::E, n, T::Type = Int) where E
     m = length(edges)
     pi = Vector{Tuple{T,T}}(undef, 2m)
@@ -81,3 +86,4 @@ function _neighbor_index(m::Int, n::Int, pi::Vector{Tuple{Int,Int}},
     @assert(idx[end] == 2m + 1, "$(idx[end]) vs $(2m + 1)")
     return NeighborIndex(idx, pi)
 end
+=#
