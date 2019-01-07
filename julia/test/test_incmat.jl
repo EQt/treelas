2f2bdff8acc38dcfcbe853a2f5c7c175f5db0033
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-01-07 18:38:58 +0100

    final test

--- julia/test/test_incmat.jl
+++ julia/test/test_incmat.jl
@@ -12,8 +12,15 @@ include("../src/incmat.jl")
     edges = collect(zip(head, tail))::Edges
     lambda = fill(0.5, length(edges))
     D = create_D(edges, lambda, n)
+    D_expected =
+        [0.5  -0.5   0.0   0.0
+         0.5   0.0  -0.5   0.0
+         0.0   0.5   0.0  -0.5
+         0.0   0.0   0.5  -0.5]
+
     @test size(D) == (m, n)
     @test nnz(D) == 2m
+    @test D_expected ≈ Array(D)
 end
 
 end
