b577d58cdd86a2634a54abe1a05ae3175917e8a3
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-01-07 18:50:43 +0100

    actually call the code

--- julia/test/test_mgt.jl
+++ julia/test/test_mgt.jl
@@ -2,5 +2,14 @@ module TestMGT
 using Test
 
 include("../src/mgt.jl")
+include("square.jl")
+
+@testset "MGT     : Square               " begin
+    edges, n = square_edges()
+    m = length(edges)
+    lambda = fill(0.5, length(edges))
+    y = Float64[0 1; 0 0.5]
+    x = MGT.max_gap_tree(y, edges, lambda)
+end
 
 end
