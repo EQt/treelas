9549f93d98703efe760e8ce7109535def0492e0a
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-01-07 17:21:56 +0100

    newer version

--- julia/src/mgt.jl
+++ julia/src/mgt.jl
@@ -1,116 +1,189 @@
-max_gap_tree(y::Matrix{Float64}, g::FLSA.ImgGraph; params...) =
-    reshape(max_gap_tree(vec(y), g; params...), g.n1, g.n2)
+include("dp_tree.jl")
 
-ONE_FUNCTION = i -> 1.0
+module MGT
+include(joinpath("FLSAv1", "src", "mst.jl"))
 
-function max_gap_tree(y::Vector{Float64}, g::FLSA.ImgGraph;
-                      c0::Real = 0.0,
-                      dual_mu::Bool = false,
-                      mu_f::Function = ONE_FUNCTION,
-                      mu::Vector{Float64} = Vector{Float64}(),
-                      alpha::Vector{Float64} = Vector{Float64}(),
-                      eps_gap::Float64 = 1e-14,
-                      max_iter::Integer=1,
-                      logger::LoggerT = LoggerT(),
-                      random_tree::Bool=false,
-                      abs_tree::Bool=false,
-                      verbose::Bool=true,
+import FLSA2: _alloc_queues, _dp_tree, _init_dp_tree, ChildrenIndex, Queue
+import FLSA.Graph: source, target
+import FLSA: IncMat
+import FLSA
+
+
+"""
+Compute the gap vector (modifies dif and x) and stores it in γ.
+"""
+function gap_vec!(γ::Vector{Float64},
+                  dif::Vector{Float64},
+                  x::Vector{Float64},
+                  y::Vector{Float64},
+                  D::IncMat,
+                  Dt::IncMat,
+                  α::Vector{Float64})
+    A_mul_B!(x, Dt, α)
+    x .-= y
+    A_mul_B!(dif, D, x)
+    γ .= (α .* dif) .+ abs.(dif)
+    return γ
+end
+
+
+max_gap_tree(y::Matrix{Float64}, g::FLSA.ImgGraph; args...) =
+    reshape(max_gap_tree(vec(y), g; args...), size(y)...)
+
+
+# ONE_FUNCTION = i -> 1.0
+
+        # if gap <= eps_gap
+        #     break
+        # end
+        # if verbose
+        #     time = toq()
+        #     total += time
+        #     _field(logger, "time", time)
+        #     _field(logger, "flsa", dual_mu ? flsa0(x, y, g.D, mu)
+        #                                    : flsa(x, y, g.D))
+        #     gap = FLSA.duality_gap(vec(y), alpha, g)
+        #     _field(logger, "gap", gap)
+        #     _field(logger, "dual", FLSA.dual_obj(alpha, y, g.D))
+        #     process(x)
+        #     dprocess(alpha)
+        #     println(@sprintf("%4d %f %f %f",
+        #                      it,
+        #                      logger["flsa"][end],
+        #                      logger["dual"][end],
+        #                      logger["gap"][end]))
+        #     if assert_decreasing && length(logger["flsa"]) >= 2
+        #         @assert logger["flsa"][end] <= logger["flsa"][end-1]
+        #     end
+        # end
+        # it >= max_iter && break
+
+                      # dual_mu::Bool = false,
+                      # mu_f::Function = ONE_FUNCTION,
+                      # c0::Real = 0.0,
+                      # mu::Vector{Float64} = Vector{Float64}(),
+                      # alpha::Vector{Float64} = Vector{Float64}(),
+                      # max_iter::Integer=1,
+                      # random_tree::Bool=false,
+                      # logger::LoggerT = LoggerT(),
+                      # abs_tree::Bool=false,
+                      # eps_gap::Float64 = 1e-14,
+                      # verbose::Bool=true,
+                      # process::Function=x->nothing,
+                      # assert_decreasing::Bool=false,
+                      # verbose_debug::Bool=false,
+                      # root_node::Int = 1)
+
+
+function max_gap_tree(y::Vector{Float64},
+                      g::FLSA.ImgGraph;
+                      root_node::Int = 1,
+                      mu::Float64 = 1.0,
+                      max_iter::Int = 3,
+                      verbose::Bool = true,
                       process::Function=x->nothing,
                       dprocess::Function=α->nothing,
-                      assert_decreasing::Bool=false,
-                      verbose_debug::Bool=false,
-                      root_node::Int = 1)
-    function logg(msg...)
-        if verbose_debug
-            println(msg...)
-        end
-    end
-
-
+                      tprocess::Function=(t,w)->nothing,
+                      c0::Float64 = 0.0)
+    r = Int(root_node)
+    edges = g.graph.edges
+    D = g.D
+    Dt = g.D'
+    m, n = size(g.D)
+    alpha = c0 * sign.(g.D*vec(y))
+    dif = zeros(m)
+    γ = zeros(m)
     x = copy(y)
-    if length(alpha) <= 0
-        alpha = c0 * sign.(g.D*vec(y))
-        logg("sign alpha")
-    else
-        x = vec(y) - g.D' * alpha
-        logg("predfined x")
-    end
+    xbuf = Vector{Float64}(n)
+    z = similar(y)
+    selected = Vector{Int}(n)
+    selected[root_node] = 0
+    lb, ub, elements = _init_dp_tree(n)
+    pq, proc_order, stack, childs = _alloc_queues(n)
+    tlam = Vector{Float64}(n)
+    finished, dist, parent, neighbors, mst_pq = _init_spantree(edges, n)
     total = 0.0
-    gap = Inf
-    tic()
-    for it = 0:max_iter
-        if gap <= eps_gap
-            break
-        end
-        if verbose
-            time = toq()
-            total += time
-            _field(logger, "time", time)
-            _field(logger, "flsa", dual_mu ? flsa0(x, y, g.D, mu)
-                                           : flsa(x, y, g.D))
-            gap = FLSA.duality_gap(vec(y), alpha, g)
-            _field(logger, "gap", gap)
-            _field(logger, "dual", FLSA.dual_obj(alpha, y, g.D))
-            process(x)
-            dprocess(alpha)
-            println(@sprintf("%4d %f %f %f",
-                             it,
-                             logger["flsa"][end],
-                             logger["dual"][end],
-                             logger["gap"][end]))
-            if assert_decreasing && length(logger["flsa"]) >= 2
-                @assert logger["flsa"][end] <= logger["flsa"][end-1]
-            end
-        end
+    # if length(alpha) <= 30
+    #     println("α0 ≈ ", round.(alpha, 2))
+    # end
+    for it in 0:max_iter
+        # @time begin
+        dprocess(alpha)
+        # # println("part1")
+        # @time begin
         it >= max_iter && break
-
-        tic()
-        if random_tree
-            weights = randn(num_edges(g.graph))
-        elseif abs_tree
-            weights = abs(alpha)
-        else
-            weights = - FLSA.gap_vec(vec(y), alpha, g)
+        gap_vec!(γ, dif, x, y, D, Dt, alpha)
+        gap_value = sum(γ)
+        if verbose
+            #     time = toq()
+                println(@sprintf("%4d %f",
+                                 it, gap_value))
         end
-        logg("weights")
-        mst, wmst = kruskal_minimum_spantree(g.graph, weights)
-
-        logg("tree")
-        t = FLSA.subtree(g.graph, mst, root_node)
-        logg("created subtree")
-        z = vec(y) - FLSA.non_tree(g.D, mst)'*alpha
-        logg("non_tree")
-        Lam = fill(Inf, length(y))
-        for e in mst
+        γ .*= -1.0
+        _minimum_spantree_e(γ, finished, dist, parent,
+                            neighbors, selected, mst_pq, r)
+        # tprocess(γ, parent)
+        z .= y
+        for (i, e) in enumerate(edges)
             v, u = source(e), target(e)
-            if t.parent[v] == u
-                Lam[v] = g.lambda[e.index]
+            if parent[v] == u
+                tlam[v] = g.lambda[i]
+            elseif parent[u] == v
+                tlam[u] = g.lambda[i]
             else
-                Lam[u] = g.lambda[e.index]
+                z[v] -= g.lambda[i] * alpha[i]
+                z[u] += g.lambda[i] * alpha[i]
             end
         end
-        logg("sub_lambda")
 
-        x = if length(mu) > 0
-            FLSA.dp_tree(z, Lam, mu, t)
-        else
-            FLSA.dp_tree(z, i->Lam[i], mu_f, t)
+        x_old = x
+        # if length(z) == 10
+        #     println("z:")
+        #     show(STDOUT, MIME("text/plain"), reshape(z, 5, 2))
+        #     println("\n")
+        # end
+        # if length(z) <= 10
+        #     println("weights:  ", round.(γ*10, 2))
+        #     println("parent:   ", parent)
+        # end
+        x = _dp_tree(z,
+                     i -> tlam[i],
+                     i -> mu,
+                     root_node,
+                     parent,
+                     lb,
+                     ub,
+                     elements,
+                     pq,
+                     proc_order,
+                     stack,
+                     childs)
+        # if length(z) == 10
+        #     println("x:")
+        #     show(STDOUT, MIME("text/plain"), reshape(x, 5, 2))
+        #     println("\n")
+        # end
+        process(x)
+        ub = x_old
+        begin # compute dual ==> update alpha
+            const mu_i = mu
+            xbuf .= x .- z
+            for i in proc_order
+                p = parent[i]
+                eidx = selected[i]
+                # e = edges[eidx]
+                xbuf[p] += mu_i * xbuf[i]
+                c = xbuf[i] * (i < p ? -mu_i : +mu_i)
+                alpha[eidx] = c / g.lambda[eidx]
+            end
         end
-        @assert all(isfinite.(x))
-        @assert !any(isnan.(x))
-        logg("dp_tree")
-        alpha_t = dual_mu ? FLSA.dual_tree0(z, x, t, mu_f) :
-                            FLSA.dual_tree(z, x, t)
-        @assert all(isfinite.(alpha_t))
-        @assert !any(isnan.(alpha_t))
-        logg("dual_tree: \n$(alpha_t[1:min(5, length(alpha_t))])")
-
-        for (i,e) in enumerate(mst)
-            alpha[e.index] = alpha_t[i] / g.lambda[e.index]
-            @assert abs(g.lambda[e.index]) > 1e-15 (@val e.index)
-        end
-        @assert all(isfinite.(alpha))
-        @assert !any(isnan.(alpha))
+        # if length(alpha) <= 30
+        #     println("α ≈ ", round.(alpha, 2))
+        # end
+    # end
+    # end
     end
     return x
+end
+
 end
