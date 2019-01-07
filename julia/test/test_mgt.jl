f26d2ecc8352bf0ca185823a75763973856af9b2
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-01-07 19:13:10 +0100

    first tests

--- julia/test/test_mgt.jl
+++ julia/test/test_mgt.jl
@@ -4,12 +4,15 @@ using Test
 include("../src/mgt.jl")
 include("square.jl")
 
+
 @testset "MGT     : Square               " begin
     edges, n = square_edges()
     m = length(edges)
     lambda = fill(0.5, length(edges))
     y = Float64[0 1; 0 0.5]
     x = MGT.max_gap_tree(y, edges, lambda)
+    @test size(x) == size(y)
+    @test sum(x) ≈ sum(y)
 end
 
 end
