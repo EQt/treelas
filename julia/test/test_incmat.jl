6190ed99d6f989478fccf67418db7808210491f7
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-01-07 18:37:05 +0100

    first tests

--- julia/test/test_incmat.jl
+++ julia/test/test_incmat.jl
@@ -1,15 +1,19 @@
 module TestIncMat
 using Test
+import SparseArrays: nnz
 
 include("../src/incmat.jl")
 
 @testset "Create D: Square               " begin
     head = [1, 1, 2, 3]
     tail = [2, 3, 4, 4]
-    n = length(head)
+    n = max(maximum(tail), maximum(head))
+    m = length(edges)
     edges = collect(zip(head, tail))::Edges
     lambda = fill(0.5, length(edges))
     D = create_D(edges, lambda, n)
+    @test size(D) == (m, n)
+    @test nnz(D) == 2m
 end
 
 end
