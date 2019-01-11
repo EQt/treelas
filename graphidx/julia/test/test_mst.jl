d3040dee2cd3ec3cce89479a105864c960efe1d3
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-01-11 18:38:49 +0100

    works

--- graphidx/julia/test/test_mst.jl
+++ graphidx/julia/test/test_mst.jl
@@ -8,7 +8,8 @@ function from_networkx(graph)
     graph = reshape(reinterpret(Int, graph), 3, length(graph))'
     edges = vec(mapslices(Tuple, graph[:, 1:2] .+ 1, dims = [2]))
     weights = Float64.(graph[:, 3])
-    return edges, weights
+    n = maximum(map(maximum, edges))
+    return n, edges, weights
 end
 
 
@@ -16,15 +17,20 @@ end
     # Adapted from
     # https://github.com/networkx/networkx/blob/master/
     #   networkx/algorithms/tree/tests/test_mst.py
-    graph_x = [(0, 1, 7), (0, 3, 5), (1, 2, 8), (1, 3, 9), (1, 4, 7),
-               (2, 4, 5), (3, 4, 15), (3, 5, 6), (4, 5, 8), (4, 6, 9),
+    graph_x = [(0, 1, 7),
+               (0, 3, 5),
+               (1, 2, 8),
+               (1, 3, 9),
+               (1, 4, 7),
+               (2, 4, 5),
+               (3, 4, 15),
+               (3, 5, 6),
+               (4, 5, 8),
+               (4, 6, 9),
                (5, 6, 11)]
-
-    edges, weights = from_networkx(graph_x)
-    n = maximum(map(maximum, edges))
+    n, edges, weights = from_networkx(graph_x)
 
     selected = kruskal_mst(n, edges, weights)
-
     expected_x = [(0, 1, 7),
                   (0, 3, 5),
                   (1, 4, 7),
@@ -32,6 +38,7 @@ end
                   (3, 5, 6),
                   (4, 6, 9)]
     expected = [e in expected_x for e in graph_x]
+    # println(Int.(selected .⊻ expected))
     @test selected == expected
 end
 
