347bbc11a253510e7f2919807cc4e29425200ab5
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-01-11 18:13:25 +0100

    test Wikipedia instance

--- graphidx/julia/test/test_mst.jl
+++ graphidx/julia/test/test_mst.jl
@@ -3,4 +3,36 @@ using Test
 
 include("../src/mst.jl")
 
+
+function from_networkx(graph)
+    graph = reshape(reinterpret(Int, graph), 3, length(graph))'
+    edges = vec(mapslices(Tuple, graph[:, 1:2] .+ 1, dims = [2]))
+    weights = Float64.(graph[:, 3])
+    return edges, weights
+end
+
+
+@testset "MST: Kruskal Wikipedia         " begin
+    # Adapted from
+    # https://github.com/networkx/networkx/blob/master/
+    #   networkx/algorithms/tree/tests/test_mst.py
+    graph_x = [(0, 1, 7), (0, 3, 5), (1, 2, 8), (1, 3, 9), (1, 4, 7),
+               (2, 4, 5), (3, 4, 15), (3, 5, 6), (4, 5, 8), (4, 6, 9),
+               (5, 6, 11)]
+
+    edges, weights = from_networkx(graph_x)
+    n = maximum(map(maximum, edges))
+
+    selected = kruskal_mst(n, edges, weights)
+
+    expected_x = [(0, 1, 7),
+                  (0, 3, 5),
+                  (1, 4, 7),
+                  (2, 4, 5),
+                  (3, 5, 6),
+                  (4, 6, 9)]
+    expected = [e in expected_x for e in graph_x]
+    @test selected == expected
+end
+
 end
