231fa75566a513429571393e2cc60b56d5c12031
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-01-11 18:52:59 +0100

    const

--- graphidx/julia/test/test_mst.jl
+++ graphidx/julia/test/test_mst.jl
@@ -13,42 +13,52 @@ function from_networkx(graph)
 end
 
 
+# Adapted from
+# https://github.com/networkx/networkx/blob/master/
+#   networkx/algorithms/tree/tests/test_mst.py
+const graph_x = [
+    (0, 1, 7),
+    (0, 3, 5),
+    (1, 2, 8),
+    (1, 3, 9),
+    (1, 4, 7),
+    (2, 4, 5),
+    (3, 4, 15),
+    (3, 5, 6),
+    (4, 5, 8),
+    (4, 6, 9),
+    (5, 6, 11),
+]
+
+const expected_min_x = [
+    (0, 1, 7),
+    (0, 3, 5),
+    (1, 4, 7),
+    (2, 4, 5),
+    (3, 5, 6),
+    (4, 6, 9),
+]
+
+const expected_max_x = [
+    (0, 1, 7),
+    (1, 2, 8),
+    (1, 3, 9),
+    (3, 4, 15),
+    (4, 6, 9),
+    (5, 6, 11),
+]
+
+
 @testset "MST: Kruskal Wikipedia         " begin
-    # Adapted from
-    # https://github.com/networkx/networkx/blob/master/
-    #   networkx/algorithms/tree/tests/test_mst.py
-    graph_x = [(0, 1, 7),
-               (0, 3, 5),
-               (1, 2, 8),
-               (1, 3, 9),
-               (1, 4, 7),
-               (2, 4, 5),
-               (3, 4, 15),
-               (3, 5, 6),
-               (4, 5, 8),
-               (4, 6, 9),
-               (5, 6, 11)]
     n, edges, weights = from_networkx(graph_x)
 
     selected = kruskal_mst(n, edges, weights)
-    expected_x = [(0, 1, 7),
-                  (0, 3, 5),
-                  (1, 4, 7),
-                  (2, 4, 5),
-                  (3, 5, 6),
-                  (4, 6, 9)]
-    expected = [e in expected_x for e in graph_x]
+    expected = [e in expected_min_x for e in graph_x]
     # println(Int.(selected .⊻ expected))
     @test selected == expected
 
     selected = kruskal_mst(n, edges, -weights)
-    expected_x = [(0, 1, 7),
-                  (1, 2, 8),
-                  (1, 3, 9),
-                  (3, 4, 15),
-                  (4, 6, 9),
-                  (5, 6, 11)]
-    expected = [e in expected_x for e in graph_x]
+    expected = [e in expected_max_x for e in graph_x]
     @test selected == expected
 end
 
