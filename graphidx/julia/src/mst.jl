adbd78a4e5cfbe1de77167f503b73425400da1c4
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-01-12 14:35:38 +0100

    move old methods to bottom...

--- graphidx/julia/src/mst.jl
+++ graphidx/julia/src/mst.jl
@@ -45,31 +45,19 @@ end
 
 
 """
-    minimum_spantree(n, edges, weights, [root = 1])
+    minimum_spantree_edges(g, weights, [root = 1])
 
 [Prim's algorithm](https://en.wikipedia.org/wiki/Prim%27s_algorithm)
 for minimum spanning tree.
-Start from node `root` (will become the root node of the spanning tree).
-Return the parent vector of the tree.
-"""
-function minimum_spantree(n, edges, weights, root = 1)::Vector{Int}
-    finished, dist, parent, neighbors, pq = _init_spantree(edges, n)
-    return _minimum_spantree(weights, finished,
-                             dist, parent, neighbors, pq, root)
-end
-
-
-"""
-    minimum_spantree_edges(g, weights, [root = 1])
-
-Like `minimum_spantree` but return the parent vector and a Boolean vector
-indicating for each edge whether the edge is part of the spanning tree.
+Start from node `root` (will become the root node of the spanning
+tree).  Return the parent vector and a Boolean vector indicating for
+each edge whether the edge is part of the spanning tree.
 """
 function minimum_spantree_edges(n, edges, weights, root = 1)
     selected = Vector{Int}(undef, n)
     finished, dist, parent, neighbors, pq = _init_spantree(edges, n)
-    _minimum_spantree_edges(weights, finished,dist, parent, neighbors,
-                            selected, pq, root)
+    _minimum_spantree_edges(weights, finished,dist, parent,
+                            neighbors, selected, pq, root)
     # swap root element to front
     selected[1], selected[root] = selected[root], selected[1]
     return parent, view(selected, 2:length(selected))
@@ -84,32 +72,6 @@ function _init_spantree(edges, n)
     neighbors = NeighborIndex(n, edges)
     pq = PriorityQueue{Int, Float64}(n)
     return finished, dist, parent, neighbors, pq
-end
-
-
-function _minimum_spantree(weights, finished, dist, parent, neighbors,
-                           pq::PriorityQueue{Int, Float64}, root = 1)
-    @assert isempty(pq)
-    sizehint!(pq, length(parent))
-    finished .= false
-    finished[root] = true
-    dist .= Inf
-    dist[root] = 0.0
-    parent[root] = root
-    pq[root] = dist[root]
-    while !isempty(pq)
-        u = dequeue!(pq)
-        for (v, eidx) in neighbors[u]
-            v == u && continue
-            if !finished[v] && weights[eidx] < dist[v]
-                dist[v] = weights[eidx]
-                decrease_key!(pq, v, dist[v])
-                parent[v] = u
-            end
-        end
-        finished[u] = true
-    end
-    return parent
 end
 
 
@@ -147,3 +109,45 @@ function _minimum_spantree_edges(weights, finished, dist,
     end
     return parent
 end
+
+
+#=
+"""
+    minimum_spantree(n, edges, weights, [root = 1])
+
+[Prim's algorithm](https://en.wikipedia.org/wiki/Prim%27s_algorithm)
+for minimum spanning tree.
+Start from node `root` (will become the root node of the spanning tree).
+Return the parent vector of the tree.
+"""
+function minimum_spantree(n, edges, weights, root = 1)::Vector{Int}
+    finished, dist, parent, neighbors, pq = _init_spantree(edges, n)
+    return _minimum_spantree(weights, finished,
+                             dist, parent, neighbors, pq, root)
+end
+
+function _minimum_spantree(weights, finished, dist, parent, neighbors,
+                           pq::PriorityQueue{Int, Float64}, root = 1)
+    @assert isempty(pq)
+    sizehint!(pq, length(parent))
+    finished .= false
+    finished[root] = true
+    dist .= Inf
+    dist[root] = 0.0
+    parent[root] = root
+    pq[root] = dist[root]
+    while !isempty(pq)
+        u = dequeue!(pq)
+        for (v, eidx) in neighbors[u]
+            v == u && continue
+            if !finished[v] && weights[eidx] < dist[v]
+                dist[v] = weights[eidx]
+                decrease_key!(pq, v, dist[v])
+                parent[v] = u
+            end
+        end
+        finished[u] = true
+    end
+    return parent
+end
+=#
