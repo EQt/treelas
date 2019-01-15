6d5b204ba3e0c417ee6ac7a6c81021cc8c197242
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-01-15 13:02:59 +0100

    test line_D

--- graphidx/julia/test/test_grid.jl
+++ graphidx/julia/test/test_grid.jl
@@ -3,7 +3,7 @@ include("../src/grid.jl")
 
 using Test
 import LinearAlgebra: norm, norm2
-import .Grid: Pixel
+import .Grid: Pixel, line_D
 
 
 @testset "Pixel                          " begin
@@ -18,6 +18,17 @@ import .Grid: Pixel
     p3 = Pixel(1, 3)
     @test norm2(p3) == 10
     @test norm(p3)  ≈ √10
+end
+
+@testset "line_D                         " begin
+    D = line_D(3)
+    @test D ≈ [1  -1   0
+               0  +1  -1]
+    D = line_D(5)
+    @test D ≈ [1  -1   0   0   0
+               0  +1  -1   0   0
+               0   0  +1  -1   0
+               0   0   0  +1  -1]
 end
 
 end
