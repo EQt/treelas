0fa6b0b61c2fc696bbaa2c5346e8ad89872e2554
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-01-25 14:56:40 +0100

    uniform name...

--- graphidx/cxx/bits/hex.hpp
+++ graphidx/cxx/bits/hex.hpp
@@ -1,11 +1,11 @@
 #pragma once
 #include <string>
 #include <cstring>      // for memcpy
-#include <cinttypes>    // PRIx64
+#include <cinttypes>    // for PRIx64
 
 
 inline std::string
-hexify(uint64_t x)
+hex(uint64_t x)
 {
     char h [2*sizeof(x) + 3 + 3 + 2];
     snprintf(h, sizeof(h), "d64(0x%0" PRIx64 ")", x);
@@ -19,5 +19,5 @@ hex(double x)
     static_assert(sizeof(uint64_t) == sizeof(double), "Wrong architecture");
     uint64_t i;
     memcpy(&i, &x, sizeof(uint64_t));
-    return hexify(i);
+    return hex(i);
 }
