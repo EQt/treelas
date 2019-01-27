c86e60fc14ac2fbc5ba1eca5445041650dddf6b8
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-01-27 16:06:40 +0100

    more bitstring tests

--- graphidx/cxx/bits/bitstring.hpp
+++ graphidx/cxx/bits/bitstring.hpp
@@ -5,3 +5,11 @@
 #include <string>
 #include <bitset>
 
+
+template <typename T>
+inline std::string
+bitstring(T x)
+{
+    static_assert(std::is_integral<T>::value, "Integral required.");
+    return std::bitset<8*sizeof(T)>(x).to_string();
+}
--- graphidx/cxx/test/test_bitstring.cpp
+++ graphidx/cxx/test/test_bitstring.cpp
@@ -5,12 +5,36 @@
 TEST(bitstring, std_bitset1)
 {
     std::bitset<8> bits {0x81};
-    ASSERT_EQ(bits.to_string(), "10000001") << bits.to_string();
+    ASSERT_EQ(bits.to_string(), "10000001");
 }
 
 
 TEST(bitstring, std_bitset2)
 {
     std::bitset<8> bits {std::string("10101111")};
-    ASSERT_EQ(bits.to_string(), "10101111") << bits.to_string();
+    ASSERT_EQ(bits.to_string(), "10101111");
+}
+
+
+TEST(bitstring, bitset_u8)
+{
+    ASSERT_EQ(bitstring(uint8_t(0x81)), "10000001");
+}
+
+
+TEST(bitstring, bitset_i8)
+{
+    ASSERT_EQ(bitstring(int8_t(-1)), "11111111");
+}
+
+
+TEST(bitstring, bitset_i32_neg)
+{
+    ASSERT_EQ(bitstring(int32_t(-1)), "11111111111111111111111111111111");
+}
+
+
+TEST(bitstring, bitset_i32_long)
+{
+    ASSERT_EQ(bitstring(int32_t(0x11223344)), "00010001001000100011001101000100");
 }
