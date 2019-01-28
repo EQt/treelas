c1c2cd27abb95ea1cb44117e9e8e7a1ffa108c2b
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-01-28 15:58:20 +0100

    template it

--- graphidx/cxx/tree/postorder.hpp
+++ graphidx/cxx/tree/postorder.hpp
@@ -1,6 +1,11 @@
 #pragma once
+#include <stdexcept>
+#include <string>
+
 #include "std/stack.hpp"
 #include "idx/children.hpp"
+#include "../../cxx/utils/timer.hpp"
+
 
 
 /**
@@ -8,19 +13,54 @@
    more specific the DFS finish time.
 
    If postord is preallocated, it must have a length >= n.
-   If postord  == nullptr, it will be allocated.
+   If postord  == nullptr, it will be allocated
+   (giving the responsible of `delete[]` to the caller).
  */
-int*
+template <typename int_ = int>
+int_*
 post_order(const size_t n,
-           const int *parent,
-           const int root = 0,
-           int *postord = nullptr);
+           const int_ *parent,
+           const int_ root = 0,
+           int_ *postord = nullptr)
+{
+    if (postord == nullptr) {
+        postord = new int_[n];
+    }
+    Timer tim("children index");
+    ChildrenIndex childs (n, parent, root);
+    tim.stop();
+    stack<int_> stack;
+    post_order(root, childs, stack, postord);
+    return postord;
+}
 
+
+template <typename int_ = int>
 void
-post_order(const int root,
+post_order(const int_ root,
            const ChildrenIndex &childs,
-           stack<int> &stack,
-           int *postord);
+           stack<int_> &stack,
+           int_ *postord)
+{
+    {   Timer _ ("alloc stack");
+        stack.reserve(2*childs.size());
+        stack.push_back(root);
+    }
+    Timer _ ("dfs walk");
+    size_t pos = 0;
+    while (!stack.empty()) {
+        const auto v = stack.back();
+        stack.pop_back(); // stack.pop()
+        if (v >= 0) {
+            stack.push_back(-v-1);
+            for (auto i : childs[v]) {
+                stack.push_back(i);
+            }
+        } else {
+            postord[pos++] = -v-1;
+        }
+    }
+}
 
 /**
    Compute a post_order (if called with nullptr).
