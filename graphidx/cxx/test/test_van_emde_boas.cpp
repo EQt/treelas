0921861b2e06142b34594090c8e80a87e942ee8d
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-01-30 19:03:07 +0100

    another test case

--- graphidx/cxx/test/test_van_emde_boas.cpp
+++ graphidx/cxx/test/test_van_emde_boas.cpp
@@ -3,14 +3,31 @@
 #include "../tree/van_emde_boas.hpp"
 
 
-TEST(veb, tree3)
+TEST(veb, tree_h2)
 {
-    const int n = 3;
+    const int height = 2;
+    const int n = (1 << height) - 1;
     std::vector<int> in (n), out (n, -1);
     for (int i = 0; i < n; i++)
         in[i] = i;
 
-    build_hp_tree(in.data(), out.data(), 2, 1);
+    build_hp_tree(in.data(), out.data(), height, 1);
     ASSERT_EQ(in,  std::vector<int>({0, 1, 2}));
     ASSERT_EQ(out, std::vector<int>({0, 1, 2}));
+}
+
+
+TEST(veb, tree_h4)
+{
+    const int height = 4;
+    const int n = (1 << height) - 1;
+    std::vector<int> in (n), out (n, -1);
+    for (int i = 0; i < n; i++)
+        in[i] = i;
+
+    build_hp_tree(in.data(), out.data(), height, 1);
+    ASSERT_EQ(out, std::vector<int>({0,
+                                     1,          2,
+                                     3,    6,    9,      12,
+                                     4, 5, 7, 8, 10, 11, 13, 14}));
 }
