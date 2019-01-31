f84adfa735da4c06e33349a6236ab1fb47d521f6
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-01-31 18:29:20 +0100

    references

--- graphidx/julia/src/lca.jl
+++ graphidx/julia/src/lca.jl
@@ -5,6 +5,16 @@ TODO: Refactor in the future.
 """
 module Tree
 
+"""
+    lowest_common_ancestor(tree, lcas)
+
+
+
+Compare to
+`networkx/algorithms/lowest_common_ancestors.py`
+
+[1]: https://en.wikipedia.org/wiki/Lowest_common_ancestor
+"""
 function lowest_common_ancestor(tree, lcas)
 end
 
