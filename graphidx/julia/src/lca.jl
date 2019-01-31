b5caa75f06f6dc0f9ca06b853cea9f4df27c33c1
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-01-31 18:11:53 +0100

    started lca function

---
+++ graphidx/julia/src/lca.jl
@@ -0,0 +1,12 @@
+"""
+Bundle functions regarding tree graphs.
+
+TODO: Refactor in the future.
+"""
+module Tree
+
+function lowest_common_ancestor(tree, lcas)
+end
+
+
+end
---
+++ graphidx/julia/test/test_lca.jl
@@ -0,0 +1,10 @@
+include("../src/lca.jl")
+
+using Test
+import .Tree: lowest_common_ancestor
+
+
+@testset "LCA                            " begin
+    
+end
+
