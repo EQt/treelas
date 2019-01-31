111de81d1af1c2ea059e3a9dd1d4e8f64e7e6805
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-01-31 15:47:05 +0100

    first method into csc.hpp

--- graphidx/cxx/sparse/csc.cpp
+++ graphidx/cxx/sparse/csc.cpp
@@ -80,63 +80,6 @@ T_mul_b(double *x,
 #endif
 
 
-CSCMat
-sparse(std::vector<int64_t> I,
-       std::vector<int64_t> J,
-       std::vector<double>  W,
-       int64_t m, int64_t n)
-{
-    const auto nnz = I.size();
-    if (J.size() != nnz)
-        throw "J.size() is wrong";
-    if (W.size() != nnz)
-        throw "W.size() is wrong";
-
-    std::vector<int64_t> order (nnz);
-    for (int i = 0; i < int(nnz); i++)
-        order[i] = i;
-
-    std::sort(order.begin(), order.end(), [&](int64_t i, int64_t j)
-              { return (J[i] < J[j]) || (J[i] == J[j] && I[i] < I[j]); });
-
-    // for (auto o : order) {
-    //     std::cerr << " " << o;
-    // }
-    // std::cerr << std::endl;
-
-    // for (auto o : order) {
-    //     std::cerr << " " << I[o];
-    // }
-    // std::cerr << std::endl;
-
-    // for (auto o : order) {
-    //     std::cerr << " " << J[o];
-    // }
-    // std::cerr << std::endl;
-
-    // for (auto o : order) {
-    //     std::cerr << " " << W[o];
-    // }
-    // std::cerr << std::endl;
-
-    double  *nzval = new double[nnz];
-    int64_t *rowval = new int64_t[nnz];
-    int64_t *colptr = new int64_t[n+1];
-    colptr[n] = nnz+1;
-    colptr[0] = 1;
-
-    int k = 1;
-    for (int i = 0; i < int(nnz); i++) {
-        while (J[order[i]] > k) {
-            colptr[k++] = i+1;
-        }
-        rowval[i] = I[order[i]];
-        nzval[i] = W[order[i]];
-    }
-
-    return CSCMat({m, n, colptr, rowval, nzval});
-}
-
 
 int64_t
 num_nonzeros(const CSCMat *mat)
--- graphidx/cxx/sparse/csc.hpp
+++ graphidx/cxx/sparse/csc.hpp
@@ -4,7 +4,8 @@
 #include <cstdio>
 
 /** Compressed Sparse Column Matrix */
-struct CSCMat {
+struct CSCMat
+{
     int64_t  m;         // number of rows
     int64_t  n;         // number of cols
     const int64_t *colptr;
@@ -29,11 +30,65 @@ A_mul_b(double *x,
 
 
 /** Create a new sparse matrix (I, J, and W don't need to be sorted). */
+template <typename Idx = int64_t,
+          typename Val = double>
 CSCMat
-sparse(std::vector<int64_t> I,
-       std::vector<int64_t> J,
-       std::vector<double>  W,
-       int64_t m, int64_t n);
+sparse(std::vector<Idx> I,
+       std::vector<Idx> J,
+       std::vector<Val> W,
+       const size_t m,
+       const size_t n)
+{
+    const auto nnz = I.size();
+    if (J.size() != nnz)
+        throw "J.size() is wrong";
+    if (W.size() != nnz)
+        throw "W.size() is wrong";
+
+    std::vector<Idx> order (nnz);
+    for (size_t i = 0; i < nnz; i++)
+        order[i] = Idx(i);
+
+    std::sort(order.begin(), order.end(), [&](Idx i, Idx j)
+        { return (J[i] < J[j]) || (J[i] == J[j] && I[i] < I[j]); });
+
+    // for (auto o : order) {
+    //     std::cerr << " " << o;
+    // }
+    // std::cerr << std::endl;
+
+    // for (auto o : order) {
+    //     std::cerr << " " << I[o];
+    // }
+    // std::cerr << std::endl;
+
+    // for (auto o : order) {
+    //     std::cerr << " " << J[o];
+    // }
+    // std::cerr << std::endl;
+
+    // for (auto o : order) {
+    //     std::cerr << " " << W[o];
+    // }
+    // std::cerr << std::endl;
+
+    Val  *nzval = new Val[nnz];
+    Idx *rowval = new Idx[nnz];
+    Idx *colptr = new Idx[n+1];
+    colptr[n] = nnz+1;
+    colptr[0] = 1;
+
+    Idx k = 1;
+    for (size_t i = 0; i < nnz; i++) {
+        while (J[order[i]] > k)
+            colptr[k++] = i+1;
+
+        rowval[i] = I[order[i]];
+        nzval[i] = W[order[i]];
+    }
+
+    return CSCMat({m, n, colptr, rowval, nzval});
+}
 
 
 /** Number of non-zero entry in a matrix */
