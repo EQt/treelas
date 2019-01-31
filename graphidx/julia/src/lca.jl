7419e8b139f14671fc272570d13fd0db74f1cddf
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-01-31 18:46:10 +0100

    wip

--- graphidx/julia/src/lca.jl
+++ graphidx/julia/src/lca.jl
@@ -4,9 +4,13 @@ Bundle functions regarding tree graphs.
 TODO: Refactor in the future.
 """
 module Tree
+include("unionfind.jl")
+include("children.jl")
+include("neighbors.jl")
+
 
 """
-    lowest_common_ancestor(tree, lcas)
+    lowest_common_ancestors(tree, lcas)
 
 
 
@@ -15,8 +19,14 @@ Compare to
 
 [1]: https://en.wikipedia.org/wiki/Lowest_common_ancestor
 """
-function lowest_common_ancestor(tree, lcas)
+function lowest_common_ancestors(tree, pairs::NeighborIndex)::Vector{Int}
+    k = length(pairs)
+    return fill(-1, k)
 end
+
+
+lowest_common_ancestors(tree, pairs::Vector{Tuple{Int, Int}}) =
+    lowest_common_ancestors(tree, NeighborIndex(length(tree), pairs))
 
 
 end
--- graphidx/julia/test/test_lca.jl
+++ graphidx/julia/test/test_lca.jl
@@ -1,10 +1,17 @@
 include("../src/lca.jl")
 
 using Test
-import .Tree: lowest_common_ancestor
+import .Tree: lowest_common_ancestors, ChildrenIndex
 
 
-@testset "LCA                            " begin
-    
+@testset "LCA Wikipedia                  " begin
+    #     1  2  3  4  5  6  7
+    pi = [1, 1, 1, 2, 4, 4, 6]
+    idx = ChildrenIndex(pi)
+    @test length(idx) == 7
+    pairs = [(5, 7), (5, 2), (5, 3), (2, 3)]
+    ans = [4, 2, 1, 1]
+    @test length(pairs) == length(ans)
+    @test lowest_common_ancestors(idx, pairs) == ans
 end
 
