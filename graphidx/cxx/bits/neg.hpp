b105834cea49fc0c0b2ec49b4ec242a5125dda91
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-01-31 17:34:15 +0100

    bit-wise level

--- graphidx/cxx/bits/neg.hpp
+++ graphidx/cxx/bits/neg.hpp
@@ -2,7 +2,7 @@
 #include <type_traits>      // std::is_integral
 
 /**
-   Negate numbers in a way that `neg(x) != x` (except for numerical overflows).
+   Negate numbers in a way that `neg(x) != x`.
    Additionally we require that `neg` is self-inverse, i.e. `neg(neg(x)) == x` for all `x`.
    This way, also `0` obtains a "sign".
    On the bit level, we just negate every single bit.
@@ -13,5 +13,6 @@ neg(const Int x)
 {
     static_assert(std::is_integral<Int>::value, "Works only for integers");
     static_assert(std::is_signed<Int>::value, "Negation needs sign");
-    return -(x + Int(1));
+    // return -(x + Int(1));    // mathematical interpretation
+    return ~x;                  // fewer overflow errors
 }
