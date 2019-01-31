2299c0cc7b7177e7f4a4838302106aa09fd93506
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-01-31 17:54:15 +0100

    assumptions on numeric_limits<int>::digits

--- graphidx/cxx/test/test_neg.cpp
+++ graphidx/cxx/test/test_neg.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include <cstdint>
+#include <limits>
 #include "../bits/neg.hpp"
 
 
@@ -8,4 +9,14 @@ TEST(neg, negneg)
     ASSERT_EQ(neg(neg(int64_t(123))), int64_t(123));
     ASSERT_EQ(neg(neg(int8_t(123))), int8_t(123));
     ASSERT_EQ(neg(neg(int32_t(123))), int32_t(123));
+}
+
+
+TEST(neg, digits)
+{
+    ASSERT_EQ(std::numeric_limits<uint8_t>::digits, 8);
+    ASSERT_EQ(std::numeric_limits<int8_t>::digits,  7);
+    ASSERT_EQ(std::numeric_limits<int16_t>::digits, 15);
+    ASSERT_EQ(std::numeric_limits<int32_t>::digits, 31);
+    ASSERT_EQ(std::numeric_limits<int64_t>::digits, 63);
 }
