f39c7b424b391eaf1915260229ae1bf4d15d7913
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-01-31 12:22:38 +0100

    refactor: const if possible

--- graphidx/cxx/test/test_van_emde_boas.cpp
+++ graphidx/cxx/test/test_van_emde_boas.cpp
@@ -7,9 +7,10 @@ TEST(veb, tree_h2)
 {
     const int height = 2;
     const int n = (1 << height) - 1;
-    std::vector<int> in ({1, 0, 2}), out (n, -1);
+    const std::vector<int> in ({1, 0, 2});
+    std::vector<int> out (n, -1);
 
-    binveb_from_inord(in.data(), out.data(), height);
+    binveb_from_inord(out.data(), in.data(), height);
     ASSERT_EQ(out, std::vector<int>({0, 1, 2}));
 }
 
@@ -18,11 +19,10 @@ TEST(veb, tree_h3)
 {
     const int height = 3;
     const int n = (1 << height) - 1;
-    std::vector<int>
-        out (n, -1),
-        in ({3, 1, 4, 0, 5, 2, 6});
+    const std::vector<int> in ({3, 1, 4, 0, 5, 2, 6});
+    std::vector<int> out (n, -1);
     ASSERT_EQ(in.size(), n);
-    binveb_from_inord(in.data(), out.data(), height, 1);
+    binveb_from_inord(out.data(), in.data(), height, 1);
     ASSERT_EQ(out, std::vector<int>({0, 1, 3, 4, 2, 5, 6}));
 }
 
@@ -31,12 +31,12 @@ TEST(veb, tree_h4)
 {
     const int height = 4;
     const int n = (1 << height) - 1;
-    std::vector<int>
-        out (n, -1),
+    const std::vector<int>
         in ({7, 3, 8, 1, 9, 4, 10, 0, 11, 5, 12, 2, 13, 6, 14});
+    std::vector<int> out (n, -1);
 
     ASSERT_EQ(in.size(), n);
-    binveb_from_inord(in.data(), out.data(), height);
+    binveb_from_inord(out.data(), in.data(), height);
     ASSERT_EQ(out, std::vector<int>({0,
                                      1,2,
                                      3, 
--- graphidx/cxx/tree/van_emde_boas.hpp
+++ graphidx/cxx/tree/van_emde_boas.hpp
@@ -13,33 +13,33 @@
 
 */
 template<typename T>
-void binveb_from_inord(const T *begin_in,
-                       T *begin_out,
-                       int height,
-                       int step = 1)
+void binveb_from_inord(T *a_out,
+                       const T *a_in,
+                       const int height,
+                       const int step = 1)
 {
-    int bottom_height = height == 2 ? 1 : int(hyperfloor(height-1));
-    int top_height    = height - bottom_height;
-    int bottom_size   = (1 << bottom_height) -1;
-    int top_size      = (1 << top_height) -1;
+    const int bottom_height = height == 2 ? 1 : int(hyperfloor(height-1));
+    const int top_height    = height - bottom_height;
+    const int bottom_size   = (1 << bottom_height) -1;
+    const int top_size      = (1 << top_height) -1;
 
     if (top_height == 1 && bottom_height == 1) {
-        begin_out[1] = begin_in[0];
-        begin_out[0] = begin_in[1*step];
-        begin_out[2] = begin_in[2*step];
+        a_out[1] = a_in[0];
+        a_out[0] = a_in[1*step];
+        a_out[2] = a_in[2*step];
         return;
     }
     if (top_height == 1) {
-        begin_out[0] = begin_in[bottom_size*step];
+        a_out[0] = a_in[bottom_size*step];
     } else {
-        binveb_from_inord(begin_in+bottom_size*step,
-                          begin_out,
+        binveb_from_inord(a_out,
+                          a_in+bottom_size*step,
                           top_height,
                           bottom_size*step+step);
     }
-    for(int i = 0; i <= top_size; i++) {
-        binveb_from_inord(begin_in+(i*bottom_size+i)*step,
-                          begin_out+top_size+i*bottom_size,
+    for (int i = 0; i <= top_size; i++) {
+        binveb_from_inord(a_out+top_size+i*bottom_size,
+                          a_in+(i*bottom_size+i)*step,
                           bottom_height,
                           step);
     }
