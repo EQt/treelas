dfb6b926b72e1522679ceef4f4d0d0779932ad16
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-02-01 10:41:46 +0100

    outsource problematic case

--- graphidx/julia/test/test_lca.jl
+++ graphidx/julia/test/test_lca.jl
@@ -1,10 +1,13 @@
 include("../src/lca.jl")
 
 using Test
-import .Tree: lowest_common_ancestors, ChildrenIndex
+import .Tree: lowest_common_ancestors, ChildrenIndex, NeighborIndex
 
 @testset "LCA NeighborIndex              " begin
     pairs = [(5, 7), (5, 2), (5, 3), (2, 3)]
+    idx = NeighborIndex(7, pairs)
+    @test Set(idx[5]) == Set([(7, 1), (2, 2), (3, 3)])
+    @test Set(idx[2]) == Set([(5, 2), (3, 4)])
 end
 
 
@@ -32,9 +35,15 @@ end
     @test Set(idx[6]) == Set([7])
     @test Set(idx[7]) == Set([])
     @test length(idx) == 7
-    pairs = [(5, 7), (5, 3), (2, 3), (5, 2)]
-    ans = [4, 1, 1, 2]
+    pairs = [(5, 7), (5, 3), (2, 3)]
+    ans = [4, 1, 1]
     @test length(pairs) == length(ans)
     @test lowest_common_ancestors(idx, pi, pairs) == ans
 end
 
+
+@testset "LCA Wikipedia (problematic)    " begin
+    pi = [1, 1, 1, 2, 4, 4, 6]
+    pairs = [(5, 2)]
+    @test lowest_common_ancestors(ChildrenIndex(idx), pi, pairs) == [2]
+end
