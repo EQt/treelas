bfc8f62a9663451664d3a64c0e63b48a8802d9cf
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-02-01 14:09:54 +0100

    Documentation

--- graphidx/julia/src/order.jl
+++ graphidx/julia/src/order.jl
@@ -1,3 +1,9 @@
+"""
+    find_root(pi)
+
+Find the first index `i` with `pi[i] == i`.
+Report an error if non such exist.
+"""
 function find_root(pi::Vector{Int})::Int
     for i = 1:length(pi)
         pi[i] == i && return i
