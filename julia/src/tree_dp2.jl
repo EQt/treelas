a39b38cfa4a9281774b0ab3dad813ccafecf99a6
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-02-07 15:29:28 +0100

    documentation

--- julia/src/tree_dp2.jl
+++ julia/src/tree_dp2.jl
@@ -17,19 +17,26 @@ include("weights.jl")
 import GraphIdx: ChildrenIndex, reset!, dfs_walk
 
 
+"""
+Set of double ended queues (DeQue) in a tree.
+Initially all leaf nodes have its own queue.
+Children's queues will be merged into the parent node.
+"""
 struct Queues
     events::Vector{Event}
     pq::Vector{Range}
-end
-
-
-Queues(n) =
+    Queues(n) =
     new(Vector{Event}(undef, 2n),
         Vector{Range}(undef,  n))
+end
+
 
 Base.getindex(q::Queues, i) = q.pq[i]
 
 
+"""
+Contains all memory needed for `tree_dp!`.
+"""
 struct TreeDPMem
 end
 
@@ -38,23 +45,40 @@ function TreeDPMem(n::Integer)
 end
 
 
-tree_dp(y::Array{F,N}, λ::F, µ::F = F(1.0), t::Tree)::Array{F,N} where {F,N} =
-    tree_dp!(similar(y), y, ConstantWeights(F(λ)), ConstantWeights(F(µ)), t)
+"""
+    tree_dp(y, t::Tree, λ [, μ = 1.0])
+
+Perform the dynamic programming algorithm on `y` and return the optimized `x`.
+
+The edge weighting λ should be either a constant (e.g. `Float64`) or a
+callable such that `λ(i)` returns the weight of the edge `(i, t.parent[i])`;
+for `λ(t.root)` it might return anything but not causean error.
+"""
+tree_dp(y::Array{F,N}, t::Tree, λ::F, µ::F = F(1.0))::Array{F,N} where {F,N} =
+    tree_dp!(similar(y), y, t, ConstantWeights(F(λ)), ConstantWeights(F(µ)))
 
 
-tree_dp(y::Array{F,N}, λ::Lam, µ::Mu, t::Tree)::Array{F,N} where {F,N,Lam,Mu} =
-    tree_dp!(similar(y), y, λ, µ, t)
+tree_dp(y::Array{F,N}, t::Tree, λ::Lam, µ::Mu)::Array{F,N} where {F,N,Lam,Mu} =
+    tree_dp!(similar(y), y, t, λ, µ)
 
 
+"""
+    tree_dp!(x, y, t, λ, μ [,mem])
 
-tree_dp!(x::Array{F,N}, y::Vector{F}, λ::Lam,
-         µ::Mu, t::Tree)::Array{F,N} where {F,N,Lam,Mu} =
-    tree_dp!(x, y, λ, µ, t, TreeDPMem(length(y)))
+Like `tree_dp` but do not allocate an output buffer for `x`.
+
+If `mem::TreeDPMem` is provided, no additional allocations will be
+needed.
+"""
+tree_dp!(x::Array{F,N}, y::Vector{F}, t::Tree, λ::Lam,
+         µ::Mu)::Array{F,N} where {F,N,Lam,Mu} =
+    tree_dp!(x, t, y, λ, µ, TreeDPMem(length(y)))
 
 
-function tree_dp!(x::Array{F,N}, y::Vector{F}, λ::Lam,
-                  µ::Mu, t::Tree, mem::TreeDPMem)::Array{F,N} where {F,N,Lam,Mu}
+function tree_dp!(x::Array{F,N}, y::Array{F,N}, t::Tree, λ::Lam,
+                  µ::Mu, mem::TreeDPMem)::Array{F,N} where {F,N,Lam,Mu}
     return x
 end
+
 
 end
