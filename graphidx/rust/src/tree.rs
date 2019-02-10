7e145acc0b6bd722fca7c7676f41541800a7f447
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-02-10 20:22:14 +0100

    another root test

--- graphidx/rust/src/tree.rs
+++ graphidx/rust/src/tree.rs
@@ -1,7 +1,7 @@
 /// Find the root node of a tree given its `parent` vector
 pub fn find_root(parent: &[usize]) -> Option<usize> {
     let n = parent.len();
-    for i in 0..(n - 1) {
+    for i in 0..n {
         if parent[i] == i {
             return Some(i);
         }
@@ -89,6 +89,14 @@ mod tests {
         let pi: Vec<usize> = vec![1, 0, 1, 2, 3, 0, 7, 8, 3, 8];
         let maybe_root = find_root(&pi);
         assert!(maybe_root.is_none());
+    }
+
+    #[test]
+    fn test_root_perm() {
+        let pi: Vec<usize> = vec!
+          // 0   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16  17
+            [6,  6,  6,  7,  7,  7,  8,  8, 17, 15, 15, 15, 16, 16, 16, 17, 17, 17];
+        assert_eq!(find_root(&pi).unwrap(), 17);
     }
 
     #[test]
