a7b734288bc41c1b397156e6ce01f71c807fb7f4
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-02-11 18:15:49 +0100

    more testing

--- graphidx/python/graphidx/test_hierarchy.py
+++ graphidx/python/graphidx/test_hierarchy.py
@@ -17,14 +17,35 @@ def test_unindent():
     assert unindent(test_unindent.__doc__) == "a\n  b\nc\n"
 
 
+def hierarchy_str(parent):
+    cidx = ChildrenIndex(parent)
+    buf = StringIO()
+    hierarchy(cidx, out=buf)
+    return buf.getvalue()
+
+
 def test_3():
     """
     0
     └╴1
       └╴2
     """
-    cidx = ChildrenIndex([0, 0, 1])
-    buf = StringIO()
-    hierarchy(cidx, out=buf)
-    assert buf.getvalue() == unindent(test_3.__doc__)
-    
+    assert hierarchy_str([0, 0, 1]) == unindent(test_3.__doc__)
+
+
+def test_10():
+    """
+    0
+    ├╴1
+    │ └╴2
+    │   └╴3
+    │     ├╴4
+    │     └╴8
+    │       ├╴7
+    │       │ └╴6
+    │       └╴9
+    └╴5
+    """
+    pi = [0, 0, 1, 2, 3, 0, 7, 8, 3, 8]
+    h = hierarchy_str(pi)
+    assert h == unindent(test_10.__doc__), "\n" + str(h)
