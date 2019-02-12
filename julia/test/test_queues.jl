8b9567ea57338dc8ef1efefc28453de64330fbc2
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-02-12 20:06:50 +0100

    fix it

--- julia/test/test_queues.jl
+++ julia/test/test_queues.jl
@@ -5,6 +5,9 @@ import GraphIdx.Tree: hierarchy_string, ChildrenIndex, parenthesis_string
 
 
 @testset "Queus: init 9 nodes            " begin
+    """
+    Compare to cxx/test/test_tree_dp.cpp: TEST(dptree, init_queues)
+    """
     t = Tree(Int[1, 1, 2, 3, 4, 1, 8, 9, 4, 9])
     cidx = ChildrenIndex(t.parent, t.root)
     @test strip(hierarchy_string(cidx)) == strip("""
@@ -23,19 +26,9 @@ import GraphIdx.Tree: hierarchy_string, ChildrenIndex, parenthesis_string
     pq = Vector{Range}(undef, n)
     proc_order = reset!(pq, cidx, Int[])
     @test proc_order == [6, 10, 7, 8, 9, 5, 4, 3, 2, 1]
-    #=
-    @test pq == Range[21:20,
-                      18:17,
-                      17:16,
-                      16:15,
-                      7:6,
-                      20:19,
-                      11:10,
-                      12:11,
-                      15:14,
-                      14:13]
-    =#
-    layout = """
+    rr = Range[21:20, 20:19, 19:18, 18:17, 17:16, 4:3, 13:12, 14:13, 15:14, 10:9]
+    @test pq == rr
+    """
       1
       ├─2
       │ └─3
