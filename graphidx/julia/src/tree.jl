3fa04658872d56da36c6587e7a1a144b4c643858
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-02-12 12:17:39 +0100

    automatically detect root node

--- graphidx/julia/src/tree.jl
+++ graphidx/julia/src/tree.jl
@@ -1,18 +1,22 @@
 """
-Bundle functions regarding tree graphs.
+Bundle functions regarding (rooted) tree graphs.
 """
 module Tree
+
+include("tree/root.jl")
 
 """
 A rooted tree.
 Often you might want to have a [`ChildrenIndex`](@ref).
+For debugging, the [`hierarchy`](@ref) function might be useful.
 """
 struct RootedTree
     root::Int
     parent::Vector{Int}
+    RootedTree(parent::Vector{Int}) =
+        RootedTree(find_root(parent), parent)
 end
 
-include("tree/root.jl")
 include("tree/degrees.jl")
 include("tree/children.jl")
 include("tree/dfs.jl")
