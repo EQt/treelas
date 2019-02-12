e2405c412fbe4504002a833d50be57d5e359010c
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-02-12 16:37:04 +0100

    outsource and document Events

--- julia/src/common.jl
+++ julia/src/common.jl
@@ -1,25 +1,5 @@
-import Printf: @sprintf
+include("event.jl")
 
-# Event ------------------------------------------------------------------
-struct Event
-    x::Float64      # position (to sort the queues)
-    slope::Float64  # delta slope
-end
-
-intercept(e::Event)::Float64 = -e.x * e.slope
-
-Base.isless(e1::Event, e2::Event) = isless(e1.x, e2.x)
-
-Base.zero(::Type{Event}) = Event(NaN, NaN, NaN)
-
-Base.isnan(e::Event) = isnan(e.x)
-
-Base.show(io::IO, e::Event) =
-    print(io,
-          if isnan(e) "Event(NaN)" else
-              @sprintf("% .2f -> % .2ft + % .3f",
-                       e.x, e.slope, intercept(e))
-          end)
 
 # Queues ----------------------------------------------------------------
 """
@@ -37,7 +17,6 @@ end
 Base.length(q::Range) = q.stop - q.start + 1
 
 Base.show(io::IO, q::Range) = print(io, q.start, ":", q.stop)
-
 
 
 function clip_front(elements::Vector{Event}, pqs::Vector{Range}, i::Int,
---
+++ julia/src/event.jl
@@ -0,0 +1,33 @@
+import Printf: @sprintf
+
+"""
+Change of the derivative
+"""
+struct Event
+    """position (to sort the queues)"""
+    x::Float64
+
+    """delta slope"""
+    slope::Float64
+end
+
+
+"""
+    intercept(e)
+
+Intercept (also called *offset*); implicitly encoded in the [`Event`](@ref).
+"""
+intercept(e::Event)::Float64 = -e.x * e.slope
+
+Base.isless(e1::Event, e2::Event) = isless(e1.x, e2.x)
+
+Base.zero(::Type{Event}) = Event(NaN, NaN, NaN)
+
+Base.isnan(e::Event) = isnan(e.x)
+
+Base.show(io::IO, e::Event) =
+    print(io,
+          if isnan(e) "Event(NaN)" else
+              @sprintf("% .2f -> % .2ft + % .3f",
+                       e.x, e.slope, intercept(e))
+          end)
