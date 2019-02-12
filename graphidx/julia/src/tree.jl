654b78cea8967a71b9f31280a61ab1fa68844cb2
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-02-12 12:46:08 +0100

    implicit assumptions

--- graphidx/julia/src/tree.jl
+++ graphidx/julia/src/tree.jl
@@ -6,7 +6,9 @@ module Tree
 include("tree/root.jl")
 
 """
-A rooted tree.
+A rooted tree represanted by its parent vector, i.e. if `u` is the parent node of `v` then `parent[v] == u`.
+We always require that root is its own parent (`parent[root] == root`) and `length(parent) == n`.
+
 Often you might want to have a [`ChildrenIndex`](@ref).
 For debugging, the [`hierarchy`](@ref) function might be useful.
 """
