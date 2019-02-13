cb1b230c0b2d4b5c08b4003c834b211c562e0a78
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-02-13 19:04:46 +0100

    improved output

--- julia/test/test_tree_dp.jl
+++ julia/test/test_tree_dp.jl
@@ -1,7 +1,9 @@
 module TestTreeDP
 
 using Test
+import Printf: @sprintf
 import TreeLas.TreeDP
+import GraphIdx.Tree: ChildrenIndex, hierarchy
 
 @testset "tree_dp: 10                    " begin
     t = TreeDP.Tree(Int[1, 1, 2, 3, 4, 1, 8, 9, 4, 9])
@@ -10,7 +12,12 @@ import TreeLas.TreeDP
 
     x = TreeDP.tree_dp(y, t, lam)
     @test size(x) == size(y)
-    @test 3x ≈ [22.7, 22.7, 22.7, 18.9, 18.9, 21.9,  9.9,  8.2,  8.2,  8.2]
+    expect3x = [22.7, 22.7, 22.7, 18.9, 18.9, 21.9,  9.9,  8.2,  8.2,  8.2]
+    @test 3x ≈ expect3x
+    hierarchy(ChildrenIndex(t.parent),
+              i ->
+            # @sprintf("%.1f=%.1f-%.1f", 3y[i] - expect3x[i], 3y[i], expect3x[i]))
+              @sprintf("%.1f:%.1f", 3y[i], expect3x[i]))
 end
 
 end
