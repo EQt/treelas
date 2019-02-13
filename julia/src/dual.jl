d843dd18f960c927e3f4c1714824ee901c35e517
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-02-13 19:15:04 +0100

    started to compute the dual ...

---
+++ julia/src/dual.jl
@@ -0,0 +1,24 @@
+function dual(
+    x::Array{F,N},
+    y::Array{F,N},
+    proc_order::Vector{I},
+    parent::Vector{I},
+) where {F,N,I}
+    dual!(Vector{F}(undef, length(proc_order)), x, y, proc_order, parent)
+end
+
+
+function dual!(
+    alpha::Vector{F},
+    x::Array{F,N},
+    y::Array{F,N},
+    proc_order::Vector{I},
+    parent::Vector{I},
+) where {F,N,I}
+    alpha .= x .- y
+    for v in @view proc_order[1:end-1]
+        alpha[parent[v]] += alpha[v]
+    end
+end
+
+
