3c50b985652dd022fd090744e0209bc6a7e2b16b
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-02-13 17:36:46 +0100

    sign was missing

--- julia/src/gap.jl
+++ julia/src/gap.jl
@@ -28,7 +28,9 @@ function gap_vec!(γ::Vector{F},
                   g::Graph,
                   c::F = F(1.0)) where {F,Graph}
     enumerate_edges(g) do ei::Int, u::Int, v::Int, lam::Float64
-        γ[ei] = c * (lam - α[ei]) * abs(x[u] - x[v])
+        let diff = x[u] - x[v]
+            γ[ei] = c * (lam*abs(diff) - α[ei] * diff)
+        end
     end
 end
 
