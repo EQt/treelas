151db9689abd2f3cda4c6980a431214c441d4fdb
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-02-15 16:33:21 +0100

    cumsum test...

--- julia/test/test_dual.jl
+++ julia/test/test_dual.jl
@@ -1,8 +1,9 @@
 using Test
 import TreeLas: dual!
+import Random: MersenneTwister
 
 
-@testset "test_dual3                     " begin
+@testset "dual: test_dual3               " begin
     root = 1
     local parent = Int[1, 1, 1]
     z = Float64[-3, 1, 2]
@@ -12,4 +13,19 @@ import TreeLas: dual!
     dual!(alpha, proc_order, parent, NaN)
     @test isnan(alpha[root])
     @test alpha[2:end] == [1, 2]
+end
+
+
+@testset "dual: cumsum                   " begin
+    n = 10
+    rseed() = MersenneTwister(42)
+    pi = collect(0:(n-1))
+    pi[1] = 1
+    post_order = n:-1:1 |> collect
+    z = randn(rseed(), n)
+    z .-= sum(z)/n
+    @test sum(z) <= eps() * n
+    alpha = copy(z)
+    dual!(alpha, post_order, pi)
+    @test alpha ≈ reverse!(cumsum(reverse(z)))
 end
