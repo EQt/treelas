f7d11a9ba85335af639abe7dc6e4f61967b57583
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-02-23 22:28:53 +0100

    beatify output

--- julia/test/test_tree_dp.jl
+++ julia/test/test_tree_dp.jl
@@ -34,20 +34,17 @@ end
     y = Float64[8.2, 7.0, 9.5, 6.8, 5.8, 6.3, 4.3, 2.2, 1.2, 2.8]
     lam = 1.0
 
-    x = TreeDP.tree_dp(y, t, lam)
+    x = TreeDP.tree_dp(copy(y), t, lam)
     @test size(x) == size(y)
     expect3x = [22.7, 22.7, 22.7, 18.9, 18.9, 21.9,  9.9,  8.2,  8.2,  8.2]
     @test 3x ≈ expect3x
     local cidx = ChildrenIndex(t.parent)
-    if false
+    if true
         let z = expect3x/3 - y
             local alpha = dual(z, t.parent, cidx)
-            @info alpha
+            @info "alpha=" *string(round.(alpha, digits=4))
+            hierarchy(cidx, i -> @sprintf("%.1f:%.1f", 3y[i], expect3x[i]))
         end
-    end
-    if false
-        hierarchy(cidx, i -> @sprintf("%.1f:%.1f", 3y[i], expect3x[i]))
-       # @sprintf("%.1f=%.1f-%.1f", 3y[i] - expect3x[i], 3y[i], expect3x[i]))
     end
 end
 
