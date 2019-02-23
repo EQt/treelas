d701514edd734d2e0f8491f424c70c0f468812fa
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-02-23 22:21:40 +0100

    printout alpha

--- julia/test/test_tree_dp.jl
+++ julia/test/test_tree_dp.jl
@@ -3,6 +3,7 @@ module TestTreeDP
 using Test
 import Printf: @sprintf
 import TreeLas.TreeDP
+import TreeLas: dual
 import GraphIdx.Tree: ChildrenIndex, hierarchy, hierarchy_with
 
 
@@ -37,11 +38,16 @@ end
     @test size(x) == size(y)
     expect3x = [22.7, 22.7, 22.7, 18.9, 18.9, 21.9,  9.9,  8.2,  8.2,  8.2]
     @test 3x ≈ expect3x
+    local cidx = ChildrenIndex(t.parent)
     if false
-        hierarchy(ChildrenIndex(t.parent),
-                  i ->
+        let z = expect3x/3 - y
+            local alpha = dual(z, t.parent, cidx)
+            @info alpha
+        end
+    end
+    if false
+        hierarchy(cidx, i -> @sprintf("%.1f:%.1f", 3y[i], expect3x[i]))
        # @sprintf("%.1f=%.1f-%.1f", 3y[i] - expect3x[i], 3y[i], expect3x[i]))
-                  @sprintf("%.1f:%.1f", 3y[i], expect3x[i]))
     end
 end
 
