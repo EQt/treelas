03f055100867b105969117bc14137cf17857f5d3
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-02-23 12:54:40 +0100

    more tests

--- julia/test/test_tree_dp.jl
+++ julia/test/test_tree_dp.jl
@@ -5,6 +5,19 @@ import Printf: @sprintf
 import TreeLas.TreeDP
 import GraphIdx.Tree: ChildrenIndex, hierarchy
 
+
+@testset "tree_dp: tree.mini.h5          " begin
+    t = TreeDP.Tree([1, 1, 2, 1, 2, 3])
+    y = [0.53 0.45 0.02
+         0.17 0.96 0.97]
+    lam = 0.2
+    x = TreeDP.tree_dp(y, t, lam)
+    @test size(x) == size(y)
+    @test x ≈ [0.4  0.4  0.4
+               0.37 0.76 0.77]
+end
+
+
 @testset "tree_dp: 10                    " begin
     t = TreeDP.Tree(Int[1, 1, 2, 3, 4, 1, 8, 9, 4, 9])
     y = Float64[8.2, 7.0, 9.5, 6.8, 5.8, 6.3, 4.3, 2.2, 1.2, 2.8]
