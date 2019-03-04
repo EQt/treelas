c1597013f655383952338e21f29b5b4f25e366ad
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-03-04 14:13:03 +0100

    formula included in docs

--- julia/src/TreeLas.jl
+++ julia/src/TreeLas.jl
@@ -1,5 +1,14 @@
 """
-Fused Lasso Solver on a tree graph.
+Fused Lasso Solver for tree graphs, i.e. we aim to **minimize** the following objective function
+
+```math
+f(x) = \\frac{1}{2} \\sum_{i=1}^n \\mu_i (x_i - y_i)^2 + \\sum_{(i,j) \\in E} \\lambda_{ij} |x_i - x_j|
+```
+Hereby is
+- ``y_i`` the input signal for node ``i``,
+- ``\\mu_i \\geq 0`` is the node weight for node ``i``,
+- ``E`` the edges of a tree, and
+- ``\\lambda_{ij} > 0`` is the edge weight for ``(i,j)``.
 """
 module TreeLas
 
