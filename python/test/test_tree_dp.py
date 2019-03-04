02c971256dda3dc110c99f8ff99fa97abf6f6f2a
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-03-04 21:57:03 +0100

    check post_order

--- python/test/test_tree_dp.py
+++ python/test/test_tree_dp.py
@@ -1,8 +1,17 @@
 import numpy as np
-from treelas import Tree, TreeInstance
+from treelas import post_order, TreeInstance
 
 
-def test_demo_7x3():
+def test_demo_3x7_postord():
+    parent = np.array([0, 4, 5, 0, 3, 4, 7, 8, 5, 6, 7, 8,
+                       9, 14, 17, 12, 15, 16, 19, 16, 17])
+    po = post_order(parent, include_root=True)
+    expect = np.array([12, 11, 19, 20, 21, 14, 15, 18, 17, 16, 13,
+                       10, 7, 8, 9, 3, 6, 2, 5, 4, 1], dtype='i4') - 1
+    assert (po == expect).all()
+
+
+def test_demo_3x7():
     y = np.fromstring("0.62 0.73 0.71 1.5 1.17 0.43 1.08 0.62 " +
                       "1.73 0.95 1.46 1.6 1.16 0.38 0.9 0.32 " +
                       "-0.48 0.95 1.08 0.02 0.4", sep=" ")
@@ -11,5 +20,6 @@ def test_demo_7x3():
     prob = TreeInstance(y, parent, lam=1.0)
     assert prob.root == 0
     assert prob.parent.dtype == np.int32
+    prob.solve()
     assert abs(prob.x.mean() - prob.y.mean()) < 1e-15
     assert len(np.unique(prob.x)) == 2
