66f91244c919ff1c4012c15a2dffe5c17ebd66c8
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-03-06 15:20:51 +0100

    uniform indention

--- julia/src/gap.jl
+++ julia/src/gap.jl
@@ -11,14 +11,16 @@ Compute the gap vector (modifies `dif` and `x`) and stores it in `γ`.
     This is the old method using Julia's `SparseArrays.SparseMatrixCSC`.
 
 """
-function gap_vec!(γ::Vector{Float64},
-                  dif::Vector{Float64},
-                  x::Vector{Float64},
-                  y::Vector{Float64},
-                  D::IncMat,
-                  Dt::IncMat,
-                  α::Vector{Float64},
-                  c::Float64 = Float64(1.0))
+function gap_vec!(
+    γ::Vector{Float64},
+    dif::Vector{Float64},
+    x::Vector{Float64},
+    y::Vector{Float64},
+    D::IncMat,
+    Dt::IncMat,
+    α::Vector{Float64},
+    c::Float64 = Float64(1.0),
+)
     mul!(x, Dt, α)
     x .-= y
     mul!(dif, D, x)
@@ -38,11 +40,13 @@ The graph `g` needs to implement a method `enumerate_edges(::Function, g)`.
 
 If provided, the result is multiplied by `c`.
 """
-function gap_vec!(γ::Vector{F},
-                  x::Array{F},
-                  α::Vector{F},
-                  g::Graph,
-                  c::F = F(1.0)) where {F,Graph}
+function gap_vec!(
+    γ::Vector{F},
+    x::Array{F},
+    α::Vector{F},
+    g::Graph,
+    c::F = F(1.0),
+) where {F,Graph}
     enumerate_edges(g) do ei::Int, u::Int, v::Int, lam::Float64
         let diff = x[u] - x[v]
             γ[ei] = c * (lam*abs(diff) - α[ei] * diff)
