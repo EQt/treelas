89d82aba6de9455b357c4026a6495d8b8f231d7d
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-03-07 16:58:34 +0100

    important to the top!

--- julia/src/gap.jl
+++ julia/src/gap.jl
@@ -3,6 +3,32 @@ import GraphIdx: enumerate_edges
 
 
 """
+    gap_vec!(γ, x, α, g [, c = 1.0])
+
+Compute the duality gap: for each edge `e` we set `γ[e]` to the
+difference of the edge cost ``\\lambda_{ij} |x_i - x_j|`` and the
+linear approximation via the dual ``α_{ij}(x_i - x_j)``.
+
+The graph `g` needs to implement a method `enumerate_edges(::Function, g)`.
+
+If provided, the result is multiplied by `c`.
+"""
+function gap_vec!(
+    γ::Vector{F},
+    x::Array{F},
+    α::Vector{F},
+    g::Graph,
+    c::F = F(1.0),
+) where {F<:Real,Graph}
+    enumerate_edges(g) do ei::Int, u::Int, v::Int, lam::Float64
+        let diff = x[u] - x[v]
+            γ[ei] = c * (lam*abs(diff) + α[ei] * diff)
+        end
+    end
+end
+
+
+"""
     gap_vec!(γ, dif, x, y, D, Dt, α, c)
 
 Compute the gap vector (modifies `dif` and `x`) and stores it in `γ`.
@@ -26,30 +52,4 @@ function gap_vec!(
     mul!(dif, D, x)
     γ .= c .* ((α .* dif) .+ abs.(dif))
     return γ
-end
-
-
-"""
-    gap_vec!(γ, x, α, g [, c = 1.0])
-
-Compute the duality gap: for each edge `e` we set `γ[e]` to the
-difference of the edge cost ``\\lambda_{ij} |x_i - x_j|`` and the
-linear approximation via the dual ``α_{ij}(x_i - x_j)``.
-
-The graph `g` needs to implement a method `enumerate_edges(::Function, g)`.
-
-If provided, the result is multiplied by `c`.
-"""
-function gap_vec!(
-    γ::Vector{F},
-    x::Array{F},
-    α::Vector{F},
-    g::Graph,
-    c::F = F(1.0),
-) where {F<:Real,Graph}
-    enumerate_edges(g) do ei::Int, u::Int, v::Int, lam::Float64
-        let diff = x[u] - x[v]
-            γ[ei] = c * (lam*abs(diff) + α[ei] * diff)
-        end
-    end
 end
