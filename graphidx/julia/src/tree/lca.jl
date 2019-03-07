befa4e44e91709d308795836eb639c77481d4137
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-03-07 16:18:25 +0100

    that does not exists, any longer

--- graphidx/julia/src/tree/lca.jl
+++ graphidx/julia/src/tree/lca.jl
@@ -13,20 +13,22 @@ however the impact is at most 4 for all practical instances (``n < 10^{100}``).
 See CLRS (3rd edition) page 584 or
 [`networkx`](https://github.com/networkx/networkx/blob/master/networkx/algorithms/lowest_common_ancestors.py).
 """
-function lowest_common_ancestors(tree::ChildrenIndex,
-                                 parent::Vector{Int},
-                                 pairs::NeighborIndex)::Vector{Int}
-    k = num_edges(pairs)
-    n = num_nodes(tree)
-    uf = UnionFind(n)
-
-    colors = fill(false, n)
-    lcas = fill(-1, k)
-    ancestors = collect(Int, 1:n)
-
+function lowest_common_ancestors(
+    tree::ChildrenIndex,
+    parent::Vector{Int},
+    pairs::NeighborIndex,
+)::Vector{Int}
     @assert parent[root_node(tree)] == root_node(tree)
+
+    local k = num_edges(pairs)
+    local n = num_nodes(tree)
+    local uf = UnionFind(n)
+    local colors = fill(false, n)
+    local ancestors = collect(Int, 1:n)
+    lcas = fill(-1, k)
+
     dfs_walk(tree) do v::Int
-        if v >= 0
+        if v >= 0   # v just finished
             for (u, ei) in pairs[v]
                 if colors[u]
                     lcas[ei] = ancestors[uf[u]]
