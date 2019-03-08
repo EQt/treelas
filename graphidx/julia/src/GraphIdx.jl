d710b7ba75af7f109a7b882742800f97819833d4
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-03-08 11:06:16 +0100

    be more formal about edges

--- graphidx/julia/src/GraphIdx.jl
+++ graphidx/julia/src/GraphIdx.jl
@@ -1,6 +1,6 @@
 """
-`GraphIdx.jl`
-=============
+Index-based Graphs
+==================
 
 Nodes
 -----
@@ -10,10 +10,31 @@ To be able to use a node as an index, the nodes should be within `1:n` whereby `
 Furtheron, we always use **signed** integers because we might include extra information within the first (sign) bit.
 
 
-Most important submodules are
-- [`LinA`](@ref): Linear algebra (e.g. incidence matrix)
+Edges
+-----
+
+By convention, a graph is considered *undirected* (unless specified else).
+Hence we only need to store one direction of every edge `(u, v)`; by default we assume `u < v`.
+
+TODO:
+- differences
+- incidence matrix
+
+If any *flow* `α``for an edge `(u, v)` has to be computed, the orientation is defined as going from `v` to `u` (big to small), i.e. we compute
+
+```julia
+x[u] += α
+x[v] -= α
+```
+
+Submodules
+----------
+
+The most important submodules are
+- [`Tree`](@ref): Rooted Trees (including minimum spanning trees).
 - [`Grid`](@ref): Specialized code for grid graphs.
-- [`Utils.Heap`](@ref): Priority queues (e.g. binary heap)
+- [`LinA`](@ref): Linear algebra (e.g. incidence matrix)
+- [`Utils`](@ref): Priority queues (e.g. binary heap)
 """
 module GraphIdx
 
@@ -23,6 +44,7 @@ include("utils/unionfind.jl")
 include("utils/heap.jl")
 
 end
+
 
 include("cluster.jl")
 include("neighbors.jl")
