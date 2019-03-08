0f1221bf72a22d94db2b3b6fea034ce37134437e
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-03-08 20:59:12 +0100

    be more specific

--- julia/src/stats.jl
+++ julia/src/stats.jl
@@ -12,10 +12,12 @@ Hereby, we minimize
 ```math
 \\min_{t} \\sum_{i} w_i |t - x_i| + f(t)
 ```
-whereby `∂` is the derivative of the convex function ``f`` (by default constant `0.5`.
+whereby `∂` is the derivative of the convex function ``f``
+(by default constant `0` for ordinary weighted median).
+
 
 """
-weighted_median(x::Vector{X}, w::Vector{W}, ∂::F=(x) -> W(0)) where {X,W,F} =
+weighted_median(x::Vector{X}, w::Vector{W}, ∂::F=(x) -> W(0)) where {X, W, F} =
     weighted_median!(copy(x), copy(w), ∂)
 
 
@@ -28,7 +30,7 @@ function weighted_median!(
     w::Vector{W},
     ∂::F = (x) -> W(0),
     pi::Vector{Int} = Int[],
-) where {X, W, F <: Function}
+)::Tuple{X,X} where {X, W, F <: Function}
     @assert length(x) == length(w)
     local n = length(x)
     resize!(pi, n)
@@ -39,7 +41,7 @@ function weighted_median!(
     # Precompute  wsum(i,j) = sum(w[i:j])
     cumsum!(w, w)
     local wsum0 = -w[end]
-    wsum(i::Int) = wsum0 + ∂(x[i]) + (i == 1 ? w[1] : w[i] + w[i-1])
+    wsum(i::Int) = wsum0 + ∂(x[i]) + w[i] + (i == 1 ? 0 : w[i-1])
         
     # sum of abs "derivative" if t < x[first] (and x is sorted)
     local first::Int, last::Int = 1, length(x)
