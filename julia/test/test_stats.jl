f77fa9df35328c0cb7c493bf797b6131d87a605a
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-03-08 21:24:39 +0100

    more tests

--- julia/test/test_stats.jl
+++ julia/test/test_stats.jl
@@ -19,3 +19,11 @@ end
     @test weighted_median([1, 2, 3, 4, 5], [2, 4, 2, 1, 5]) == (3, 3)
 end
 
+
+@testset "weighted median                " begin
+    @test weighted_median([1, 3, 2], [1, 3, 1]) == (2, 3)
+    @test (2, 2) == weighted_median([1, 3, 2], [1, 3, 1], x -> x)
+    @test (1, 1) == weighted_median([1, 3, 2], [1, 3, 1], x -> 5x)
+    @test (3, 3) == weighted_median([1, 3, 2], [1, 3, 1], x -> x-10)
+end
+
