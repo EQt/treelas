6bc0563e86559a9d17141d4d611360ce1ba455de
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-03-08 20:54:34 +0100

    should work...

--- julia/src/stats.jl
+++ julia/src/stats.jl
@@ -5,23 +5,28 @@ module Stats
 
 
 """
-    weighted_median(x, w, [∂ ])
+    weighted_median(x, [w, [∂ ]])
 
 Compute a generalized weighted order statistics.
 Hereby, we minimize
 ```math
 \\min_{t} \\sum_{i} w_i |t - x_i| + f(t)
 ```
-whereby `∂` is the derivative of the convex function ``f``.
+whereby `∂` is the derivative of the convex function ``f`` (by default constant `0.5`.
+
 """
-weighted_median(x::Vector{X}, w::Vector{W}, ∂::F=(x) -> W(0.5)) where {X,W,F} =
+weighted_median(x::Vector{X}, w::Vector{W}, ∂::F=(x) -> W(0)) where {X,W,F} =
     weighted_median!(copy(x), copy(w), ∂)
+
+
+weighted_median(x::Vector{X}) where {X} =
+    weighted_median!(copy(x), ones(length(x)))
 
 
 function weighted_median!(
     x::Vector{X},
     w::Vector{W},
-    ∂::F = (x) -> W(0.5),
+    ∂::F = (x) -> W(0),
     pi::Vector{Int} = Int[],
 ) where {X, W, F <: Function}
     @assert length(x) == length(w)
@@ -33,21 +38,33 @@ function weighted_median!(
 
     # Precompute  wsum(i,j) = sum(w[i:j])
     cumsum!(w, w)
-    wsum(i::Int, j::Int) = w[j] - w[i]
+    local wsum0 = -w[end]
+    wsum(i::Int) = wsum0 + ∂(x[i]) + (i == 1 ? w[1] : w[i] + w[i-1])
+        
     # sum of abs "derivative" if t < x[first] (and x is sorted)
-    local acc = -wsum(1, length(w))
-    local first, last = 1, length(x)
+    local first::Int, last::Int = 1, length(x)
     while last - first > 1
-        local mid = (first + last) ÷ 2
-        local mid_val = acc + 2*wsum(first, mid)
-        if mid_val + ∂(x[mid]) < 0
-            acc = mid_val
+        local mid::Int = (first + last) ÷ 2
+        local mid_val = wsum(mid)
+        @show mid
+        if wsum(mid) < 0
             first = mid
+            @show first
         else
             last = mid
+            @show last
         end
     end
-    return x[first], x[last]
+
+    @show wsum(first), wsum(last)
+
+    if wsum(first) >= 0
+        return x[first], x[first]
+    elseif wsum(last) <= 0
+        return x[last], x[last]
+    else
+        return x[first], x[last]
+    end
 end
     
 
