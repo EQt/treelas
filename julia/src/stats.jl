f35bb8500b6ae9ec0b5467557219ffbca606b7d6
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-03-08 20:09:04 +0100

    wip

--- julia/src/stats.jl
+++ julia/src/stats.jl
@@ -10,36 +10,44 @@ module Stats
 Compute a generalized weighted order statistics.
 Hereby, we minimize
 ```math
-\min_{t} \sum_{i} w_i |t - x_i| + f(t)
+\\min_{t} \\sum_{i} w_i |t - x_i| + f(t)
 ```
 whereby `∂` is the derivative of the convex function ``f``.
-
-At the moment, `w` and `x` will be altered.
 """
+weighted_median(x::Vector{X}, w::Vector{W}, ∂::F=(x) -> W(0.5)) where {X,W,F} =
+    weighted_median!(copy(x), copy(w), ∂)
+
+
 function weighted_median!(
-    x::Vector{F},
-    w::Vector{F},
-    ∂::Function = (x) -> F(0.5),
-) where {F}
-    sort!(w, lt=(i,j) -> x[i] < x[j])
-    sort!(x)
+    x::Vector{X},
+    w::Vector{W},
+    ∂::F = (x) -> W(0.5),
+    pi::Vector{Int} = Int[],
+) where {X, W, F <: Function}
+    @assert length(x) == length(w)
+    local n = length(x)
+    resize!(pi, n)
+    sortperm!(pi, x)
+    x = x[pi]
+    w = w[pi]
 
     # Precompute  wsum(i,j) = sum(w[i:j])
     cumsum!(w, w)
     wsum(i::Int, j::Int) = w[j] - w[i]
-    local acc = -wum(1, length(w))
+    # sum of abs "derivative" if t < x[first] (and x is sorted)
+    local acc = -wsum(1, length(w))
     local first, last = 1, length(x)
     while last - first > 1
         local mid = (first + last) ÷ 2
-        local mid_val = acc + 2*wsum(first, mid) + f(x[mid])
-        if mid_val < 0
-            acc = acc + 2*wsum(first, mid)
+        local mid_val = acc + 2*wsum(first, mid)
+        if mid_val + ∂(x[mid]) < 0
+            acc = mid_val
             first = mid
         else
             last = mid
         end
     end
-    return x[last], x[first]
+    return x[first], x[last]
 end
     
 
