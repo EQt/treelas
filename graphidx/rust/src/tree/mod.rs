db356a6b8bc22c249a7a1f4a4479d640a4d6d096
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-03-14 21:32:48 +0100

    struct Tree

--- graphidx/rust/src/tree/mod.rs
+++ graphidx/rust/src/tree/mod.rs
@@ -4,3 +4,29 @@ mod root;
 
 pub use children::*;
 pub use root::find_root;
+
+#[allow(dead_code)]
+pub struct Tree {
+    root: usize,
+    parent: Vec<usize>,
+}
+
+impl Tree {
+    pub fn from_parent(parent: Vec<usize>) -> Option<Self> {
+        Some(Self {
+            root: find_root(&parent)?,
+            parent: parent
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_from_root() {
+        let tree = Tree::from_parent(vec![1, 1, 0]).unwrap();
+        assert_eq!(tree.root, 1);
+    }
+}
