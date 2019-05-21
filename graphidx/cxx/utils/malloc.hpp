584bd52b9ce3ef2634fbd4d19755ade4aca8e336
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-05-21 13:17:56 +0200

    shorter notation

--- graphidx/cxx/utils/malloc.hpp
+++ graphidx/cxx/utils/malloc.hpp
@@ -12,19 +12,16 @@ struct Malloc
     size_t n;
     T *_x;
 
-    Malloc(size_t n) : n(n) {
-        _x = (T*) malloc(n * sizeof(T));
-    }
+    Malloc(size_t n) : n(n) { _x = (T*) malloc(n * sizeof(T));  }
 
-    ~Malloc() {
-        if (_x != nullptr)
-            free(_x);
-    }
+    ~Malloc() { if (_x != nullptr) free(_x); }
 
     operator T*() { return _x; }
+
     operator const T*() const { return _x; }
 
     T* data() { return _x; }
+
     const T* data() const { return _x; }
 
     size_t size() const { return n; }
