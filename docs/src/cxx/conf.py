f5134120169065d43748aa2b3731d8faa210ec3e
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-06-04 13:00:55 +0200

    simplify sphinx conf.py

--- docs/src/cxx/conf.py
+++ docs/src/cxx/conf.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python
-import os
-import subprocess
 import sphinx_rtd_theme
+
 
 project = 'TreeLas'
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 extensions = ['breathe']
-breathe_projects = { 'treelas': '../../xml' }
-source_suffix = {
-    '.rst': 'restructuredtext',
-}
+breathe_projects = {'treelas': '../../xml'}
+source_suffix = {'.rst': 'restructuredtext'}
 master_doc = 'index'
 copyright = '2019, Elias Kuthe'
 author = 'Elias Kuthe'
