b4b149527d5f202eb083d4bc5faf7e4dd6b2145b
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-06-04 13:21:43 +0200

    move introductory part to module documentation

--- docs/src/python/index.rst
+++ docs/src/python/index.rst
@@ -1,20 +1,5 @@
-TreeLas
-=======
-
 This is the documentation of the Python Module of TreeLas.
 For the Julia documentation see `here <../index.html>`_.
-
-This is a *fused Lasso* Solver for tree graphs, i.e. we aim to **minimize** the following objective function
-
-.. math::
-   f(x) = \frac{1}{2} \sum_{i=1}^n \mu_i (x_i - y_i)^2 + \sum_{(i,j) \in E} \lambda_{ij} |x_i - x_j|
-
-Hereby is
- * :math:`y_i` the input signal for node :math:`i`,
- * :math:`\mu_i \geq 0` is the node weight for node :math:`i`,
- * :math:`E` the edges, usually from a tree, and
- * :math:`\lambda_{ij} > 0` is the edge weight for :math:`(i,j)`.
-
 
 .. automodule:: treelas
 
--- python/treelas/__init__.py
+++ python/treelas/__init__.py
@@ -2,6 +2,17 @@
 TreeLas: a Fused Lasso Solver for Trees
 =======================================
 
+The software herein aims to **minimize** the following objective function
+
+.. math::
+   f(x) = \\frac{1}{2} \\sum_{i=1}^n \\mu_i (x_i - y_i)^2 + \\sum_{(i,j) \\in E} \\lambda_{ij} |x_i - x_j|
+
+Hereby is
+ * :math:`y_i` the input signal for node :math:`i`,
+ * :math:`\mu_i \geq 0` is the node weight for node :math:`i`,
+ * :math:`E` the edges, usually from a tree, and
+ * :math:`\lambda_{ij} > 0` is the edge weight for :math:`(i,j)`.
+
 
 Tree Edges
 ----------
@@ -15,7 +26,7 @@ be arbitrary and the i-th element lam[i] contains the information for edge
 Details
 -------
 
-See https://github.com/eqt/treelas 
+See https://github.com/eqt/treelas
 """
 from .tree import Tree, TreeInstance
 from .segment import line_mean
