8fdf91f3a2795a292e9254988450ddddb4faa6d3
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-06-25 22:31:04 +0200

    change default build directory

--- cxx/test/.dir-locals.el
+++ cxx/test/.dir-locals.el
@@ -3,10 +3,5 @@
 
 ((c++-mode
    ;; (c-offsets-alist . (inextern-lang . 0))
-   (compile-command . "cd ../build && make tests && ./tests")
+   (compile-command . "cd ../../build && make tests && ./tests")
    (c-file-style . "stroustrup")))
-
-
-
-
-
