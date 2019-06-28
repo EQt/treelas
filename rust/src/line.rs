85985dff25a8ff7c00bb87d5cb0557b3e2a01ff6
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-06-28 18:44:40 +0200

    PWL: outsourced it

--- rust/src/line.rs
+++ rust/src/line.rs
@@ -1,18 +1,55 @@
 use crate::pwl::{clip_front, Event};
 use std::ops::Range;
 
+struct PWL {
+    offset: f64,
+    r: Range<usize>,
+}
+
+impl PWL {
+    fn clip_front(
+        &mut self,
+        event: &mut [Event],
+        offset: f64,
+        slope: f64,
+    ) -> Option<f64> {
+        let (o, s) =
+            clip_front(&mut self.r, event, offset + self.offset, slope);
+        self.offset += o;
+        if s.abs() > 1e-9 {
+            let x = -o / s;
+            self.r.start -= 1;
+            event[self.r.start] = Event {
+                x: x,
+                slope: s,
+            };
+            Some(x)
+        } else {
+            None
+        }
+    }
+}
+
 pub struct LineDP {
-    event: Vec<Event>,
     lb: Vec<f64>,
     ub: Vec<f64>,
+    event: Vec<Event>,
+    pwl: PWL,
 }
 
 impl LineDP {
-    pub fn new(n: usize) -> Self {
+    pub fn new<'a>(n: usize) -> Self {
         let mut dp = LineDP {
             event: Vec::with_capacity(2 * n),
             lb: Vec::with_capacity(n),
             ub: Vec::with_capacity(n),
+            pwl: PWL {
+                offset: 0.0,
+                r: Range {
+                    start: n,
+                    end: n - 1,
+                },
+            },
         };
         dp.event.resize_with(2 * n, Default::default);
         dp.lb.resize(n, std::f64::NAN);
@@ -29,17 +66,11 @@ impl LineDP {
         assert!(n == x.len());
         assert!(mu.len() >= n);
         assert!(x.len() - 1 >= lam.len());
-        let mut r = Range { start: n, end: n-1 };
-        let mut offset = 0.0;
         for i in 0..n {
-            let (o, s) = clip_front(&mut r, &self.event, mu[i], offset - lam[i]);
-            offset += o;
-            r.start -= 1;
-            if s.abs() > 1e-9 {
-                let x = -o/s;
-                self.event[r.start] = Event{ x: x, slope: s };
-                self.lb[i] = x;
-            }
+            self.lb[i] = self
+                .pwl
+                .clip_front(&mut self.event, -lam[i], mu[i])
+                .unwrap_or(-std::f64::INFINITY);
         }
         unimplemented!();
     }
