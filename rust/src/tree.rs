5f22e6d4e540017c4c1a5b574ca297f45fb17101
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-06-28 14:56:43 +0200

    checks for TreeDP::solve

--- rust/src/tree.rs
+++ rust/src/tree.rs
@@ -8,7 +8,22 @@ impl TreeDP {
     pub fn solve<W1, W2>(x: &mut [f64], y: &[f64], lam: W1, mu: W2)
     where
         W1: graphidx::weights::Weights<f64>,
+        W2: graphidx::weights::Weights<f64>,
     {
+        let n = y.len();
+        assert!(n == x.len());
+        assert!(mu.len() >= n);
+        assert!(x.len() - 1 >= lam.len());
         unimplemented!();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    // use super::*;
+
+    #[test]
+    fn test_treedp() {
+        assert!(true);
     }
 }
