bb57d5880a893723e3fc0c9450b982e4e9859a2e
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-06-28 17:30:35 +0200

    vecdeque

--- rust/src/event.rs
+++ rust/src/event.rs
@@ -23,6 +23,12 @@ impl Event {
 ///
 /// How can the position be returned where the loop will end?
 /// How can this be implemented for clip_back?
+///
+/// [`VecDeque`] is not the solution, as `head` and `tail` are
+/// initialized to 0 in the standard implementation and cannot be
+/// altered.
+///
+/// [`VecDeque`]: https://doc.rust-lang.org/std/collections/struct.VecDeque.html
 pub fn clip_front(events: &[Event], slope: f64, offset: f64) -> (f64, f64) {
     let mut slope = slope;
     let mut offset = offset;
