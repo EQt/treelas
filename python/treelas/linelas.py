e08bdaf24e36b893eed8b866e903ca3bf90767d0
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-08-27 16:08:07 +0200

    unified clip

--- python/treelas/linelas.py
+++ python/treelas/linelas.py
@@ -5,9 +5,7 @@ on line graphs.
 TODO
 ====
 
-- [ ]. Unify clip_front/clip_back
 - [ ]. Integrate mu = 0
-- [ ]. Move into treelas
 """
 from __future__ import annotations
 import numpy as np
@@ -36,17 +34,6 @@ class Event:
         return (e2.offset() - e1.offset()) / (e1.slope - e2.slope)
 
 
-def clip_front(elements: deque, slope: float, offset: float):
-    print(f"clip_front: ({slope}, {offset}): {pformat(elements)}")
-    while elements and slope * elements[0].x + offset < 0:
-        e = elements.popleft()
-        offset += e.offset()
-        slope += e.slope
-    x = - offset/slope
-    elements.appendleft(Event(x, slope))
-    return x
-
-
 def clip(elements: deque, slope: float, offset: float, forward: bool):
     dir = 'F' if forward else 'R'
     first = -1 if forward else 0
@@ -71,12 +58,12 @@ def line_lasso(y: np.ndarray, lam: float, mu = ConstantWeights(1.0)):
 
     lam0, lam1 = 0.0, lam
     for i in range(n-1):
-        lb[i] = clip_front(event, mu[i], -mu[i] * y[i] -lam0 + lam1)
+        lb[i] = clip(event, +mu[i], -mu[i] * y[i] - lam0 + lam1, forward=True)
         ub[i] = clip(event, -mu[i], +mu[i] * y[i] - lam0 + lam1, forward=False)
         lam0, lam1 = lam1, lam
 
     x = np.full(n, np.nan)
-    x[-1] = clip_front(event, mu[-1], -mu[-1] * y[-1] - lam + 0.0)
+    x[-1] = clip(event, mu[-1], -mu[-1] * y[-1] - lam + 0.0, forward=True)
     for i in range(n-1)[::-1]:
         x[i] = np.clip(x[i+1], lb[i], ub[i])
     return x
