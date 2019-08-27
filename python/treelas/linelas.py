6acf89a4298066ef42927bb4be773b29cb38f215
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-08-27 16:22:20 +0200

    minor changes

--- python/treelas/linelas.py
+++ python/treelas/linelas.py
@@ -34,15 +34,17 @@ class Event:
 
 
 class DeQue:
+    """
+    Double ended queue
+    """
     def __init__(self):
         from collections import deque
-
         self._e = deque()
 
-    def pop(self, forward):
+    def pop(self, forward: bool):
         return self._e.popleft() if forward else self._e.pop()
 
-    def peek(self, forward):
+    def peek(self, forward: bool):
         return self._e[-1 if forward else 0]
 
     def append(self, x, forward: bool):
@@ -51,17 +53,21 @@ class DeQue:
         else:
             self._e.append(x)
 
-    def __bool__(self):
+    def __bool__(self) -> bool:
         return bool(self._e)
 
-    def __len__(self):
+    def __len__(self) -> len:
         return len(self._e)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return repr(self._e)
 
 
 def clip(elements: DeQue, slope: float, offset: float, forward: bool) -> float:
+    """
+    Clip from little to big indexes (forward) or the other
+    way round (not forward)
+    """
     dir = 'F' if forward else 'R'
     print(f"clip: ({slope}, {offset}, {dir}): {pformat(elements)}")
     while elements and slope * elements.peek(forward).x + offset < 0:
@@ -73,7 +79,7 @@ def clip(elements: DeQue, slope: float, offset: float, forward: bool) -> float:
     return x
 
 
-def line_lasso(y: np.ndarray, lam: float, mu = ConstantWeights(1.0)):
+def line_lasso(y: np.ndarray, lam: float, mu=ConstantWeights(1.0)) -> np.ndarray:
     n = len(y)
     lb = np.full(n, np.nan)
     ub = np.full(n, np.nan)
