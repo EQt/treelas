6037be3555a5b6fba3e8dd6d85767c812c193bf6
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-08-27 20:27:07 +0200

    convert to numpy arrays for most data types

--- python/test/test_linelas.py
+++ python/test/test_linelas.py
@@ -25,10 +25,11 @@ class Instance:
         type(self)._nr += 1
         assert isinstance(data, dict)
         self.fname = fname
+        for d in ['lam', 'mu', 'x', 'y']:
+            if d in data and isinstance(data[d], list):
+                data[d] = np.array(data[d])
         self.y = np.array(data['y'], dtype=float)
         self.lam = data['lam']
-        if isinstance(self.lam, list):
-            self.lam = np.array(self.lam)
         self.mu = data.get('mu', 1.0)
         self.nr = type(self)._nr
         self.name = str(data['name']) if 'name' in data else None
@@ -53,7 +54,7 @@ insts = Instance.from_toml(data_dir("test", "lines.toml"))
 
 @pytest.mark.parametrize("i", insts)
 def test_dual(i):
-    alpha = np.cumsum(i.x - i.y)
+    alpha = np.cumsum(i.mu * (i.x - i.y))
     assert np.isclose(alpha[-1], 0)
     assert all(np.abs(alpha[:-1]) <= i.lam * (1 + 1e-8))
 
