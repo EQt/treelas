1d8958ca3d8dcc7480c14039fce5282449f96f53
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-08-27 19:01:18 +0200

    improved docstrings in the tests

--- python/test/test_zero.py
+++ python/test/test_zero.py
@@ -1,10 +1,14 @@
+"""
+Test latent nodes (i.e. `mu[i] = 0`);
+in particular that decreasing lambdas are handled correctly.
+"""
 import numpy as np
 import pytest
 from treelas import TreeInstance
 
 
 def test_nan():
-    """Test that decreasing lambdas are handled correctly"""
+    """`y` contains `NaN`"""
     t = TreeInstance(y=np.array( [0., np.nan, np.nan]),
                      mu=np.array([1., 0., 0.]),
                      lam=np.array([1.0, 0.5, 0.3]),
@@ -21,13 +25,14 @@ def test_nan():
 
 
 def test_zero3():
-    """Test that decreasing lambdas are handled correctly"""
+    """Tree with 3 nodes and zero input"""
     t = TreeInstance(y=np.array( [0., 0., 0.]),
                      mu=np.array([1., 0., 0.]),
                      lam=np.array([1.0, 0.5, 0.3]),
                      parent=np.array([1, 2, 2], dtype=np.int32),
                      root=2)
     t.solve()
+    assert np.allclose(t.x, 0)
     alpha = t.dual
     assert np.where(np.isnan(alpha))[0].tolist() == [t.root], \
         f'alpha={alpha}, root={t.root}'
@@ -39,13 +44,19 @@ def test_zero3():
 
 
 def test_zero4():
-    """Test that decreasing lambdas are handled correctly"""
+    """
+    Tree with 4 nodes, actually a line graph where just the two end
+    nodes contain information
+    """
     t = TreeInstance(y=np.array( [0., 0., 0., 2.]),
                      mu=np.array([1., 0., 0., 1.]),
                      lam=np.array([1.0, 0.3, np.nan, 1.0]),
                      parent=np.array([1, 2, 2, 2], dtype=np.int32),
                      root=2)
     t.solve()
+    assert np.allclose(t.x, [0.3, 0.3, 1.7, 1.7])
+    if False:
+        t.show(wait=False)
     alpha = t.dual
     assert np.where(np.isnan(alpha))[0].tolist() == [t.root], \
         f'alpha={alpha}, root={t.root}'
