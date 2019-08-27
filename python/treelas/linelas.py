4a15c846ee2e1f2fedcd5b1d750b99cdd56f77e0
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-08-27 15:59:51 +0200

    unify offset and t

--- python/treelas/linelas.py
+++ python/treelas/linelas.py
@@ -47,13 +47,15 @@ def clip_front(elements: deque, slope: float, offset: float, t: float):
     return x
 
 
-def clip_back(elements: deque, slope: float, offset: float, t: float):
-    print(f"clip_back:  ({slope}, {offset}, {t:+}): {pformat(elements)}")
-    while elements and slope * elements[-1].x + offset > t:
-        e = elements.pop()
+def clip(elements: deque, slope: float, offset: float, forward: bool):
+    dir = 'F' if forward else 'R'
+    first = -1 if forward else 0
+    print(f"clip: ({slope}, {offset}, {dir}): {pformat(elements)}")
+    while elements and slope * elements[first].x + offset > 0:
+        e = elements.popleft() if forward else elements.pop()
         offset -= e.offset()
         slope -= e.slope
-    x = (t - offset)/slope
+    x = - offset/slope
     elements.append(Event(x, -slope))
     return x
 
@@ -67,7 +69,7 @@ def line_lasso(y: np.ndarray, lam: float, mu = ConstantWeights(1.0)):
     lam0, lam1 = 0.0, lam
     for i in range(n-1):
         lb[i] = clip_front(event, mu[i], -mu[i] * y[i] -lam0, -lam1)
-        ub[i] = clip_back(event, mu[i], -mu[i] * y[i] +lam0, +lam1)
+        ub[i] = clip(event, mu[i], -mu[i] * y[i] +lam0 - lam1, forward=False)
         lam0, lam1 = lam1, lam
 
     x = np.full(n, np.nan)
