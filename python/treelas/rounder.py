2c25aed8eb3eaa5efaf5815db1db4f2939f1d7b4
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-08-27 15:32:21 +0200

    import only from local

---
+++ python/treelas/rounder.py
@@ -0,0 +1,20 @@
+"""
+Utility functions regarding rounding operations
+"""
+def _int_or_round(x, digits=8):
+    """x is an `int` or return `_fround`"""
+    if isinstance(x, float) and x.is_integer():
+        return int(x)
+    return _round(x, digits=digits)
+
+
+def _round(x, digits=8):
+    if hasattr(x, "__round__"):
+        return round(x, digits)
+    return x
+
+
+def _fround(x, digits=8) -> str:
+    if hasattr(x, "__round__"):
+        return f"{round(x, digits):+}"
+    return str(x)
