22332ebe0c32de4dcb130f135d0aab39a0fa61b9
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-09-02 19:08:24 +0200

    more tests

--- python/test/test_tree0.py
+++ python/test/test_tree0.py
@@ -9,16 +9,29 @@ def tree0():
     return TreeInstance.load(test_dir("tree0.toml"))
 
 
-def test_repr(tree0):
-    ti = tree0
-    assert repr(ti).startswith("TreeInstance(root=35,\n")
+@pytest.fixture
+def tree1():
+    return TreeInstance.load(test_dir("tree0.1.toml"))
 
 
-def test_solve_len(tree0):
-    ti = tree0
-    x = ti.solve().x
-    assert len(x) == len(ti)
+def test_repr0(tree0):
+    assert repr(tree0).startswith("TreeInstance(root=35,\n")
+
+
+def test_repr1(tree1):
+    assert repr(tree1).startswith("TreeInstance(root=17,\n")
+
+
+def test_solve_len0(tree0):
+    x = tree0.solve().x
+    assert len(x) == len(tree0)
     assert len(x) == 36
+
+
+def test_solve_len1(tree1):
+    x = tree1.solve().x
+    assert len(x) == len(tree1)
+    assert len(x) == 18
 
 
 def test_solve_x(tree0):
@@ -34,3 +47,4 @@ def test_solve_x(tree0):
 
 
 def test_gap_x(tree0):
+    pass
