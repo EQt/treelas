76efcc8635019f2bcdcfd1e5921238a5fe2fa5d8
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-09-02 15:34:15 +0200

    test write read

--- python/test/test_tree.py
+++ python/test/test_tree.py
@@ -64,6 +64,13 @@ def test_tree5_write_simple(tree5):
         assert 'parent' in io
 
 
+def test_tree5_write_read(tree5):
+    ti = tree5
+    ti.save('tree5.h5')
+    t2 = TreeInstance.load('tree5.h5')
+    assert ti == t2, f"\n\n{ti}\n\n{t2}\n"
+
+
 def test_rtree(n=5, seed=2018, eps=1e-14):
     t = Tree.random(n, seed=seed)
     assert t.n == n
