e1fe01f6a5e85e99e1090e69ac2a760e8f08dd7f
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-09-03 21:15:27 +0200

    wip

--- cxx/line/line_dp.cpp
+++ cxx/line/line_dp.cpp
@@ -5,9 +5,22 @@
 #include <vector>
 
 #include "../event.hpp"
+#include "../range.hpp"
 #include "utils/timer.hpp"
 #include "bits/weights.hpp"
 #include "bits/positive.hpp"
+
+
+template<int step, typename Tdouble, typename float_ = double>
+inline float_
+clip(Event *elem,
+     Range &pq,
+     Tdouble slope,
+     float_ offset)
+{
+    const auto *e = &elem[pq.stop];
+}
+
 
 
 template<typename float_, typename Wlam, typename Wmu>
@@ -27,5 +40,11 @@ line_las(
     Event *event = event_.data();
     float_
         *ub = ub_.data(),
-        *lb = x;
+        *lb = x,
+        lam0 = float_(0.0);
+
+    for (size_t i = 0; i < n-1; i++) {
+        lb[i] = clip<+1>(event, +mu[i], -mu[i] * y[i] - lam0 + lam[i]);
+        ub[i] = clip<-1>(event, -mu[i], +mu[i] * y[i] - lam0 + lam[i]);
+    }
 }
