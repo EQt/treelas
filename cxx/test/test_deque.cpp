5ba8ef5744ba18c7c3b43fed2990851cb871040b
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-09-04 17:12:05 +0200

    more unit tests

--- cxx/test/test_deque.cpp
+++ cxx/test/test_deque.cpp
@@ -8,3 +8,31 @@ TEST(DeQue, empty_bool)
     ASSERT_FALSE(q);
     ASSERT_EQ(q.size(), 0);
 }
+
+
+TEST(DeQue, one_push)
+{
+    DeQue<int> q (1);
+    ASSERT_FALSE(q);
+    ASSERT_EQ(q.size(), 0);
+
+    q.push<false>(42);
+    ASSERT_TRUE(q);
+    ASSERT_EQ(q.size(), 1);
+    ASSERT_EQ(q.peek<true>(), 42);
+    ASSERT_EQ(q.peek<false>(), 42);
+}
+
+
+TEST(DeQue, fbf)
+{
+    DeQue<int> q (2);
+    q.push<true>(1);
+    q.push<false>(5);
+    q.push(2);
+
+    ASSERT_TRUE(q);
+    ASSERT_EQ(q.size(), 3);
+    ASSERT_EQ(q.peek<true>(), 2);
+    ASSERT_EQ(q.peek<false>(), 5);
+}
