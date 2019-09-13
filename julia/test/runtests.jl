ff9b779af90f881afda135dcb780d60382566fca
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-09-13 15:20:25 +0200

    no extra test needed

--- julia/test/runtests.jl
+++ julia/test/runtests.jl
@@ -1,7 +1,6 @@
 #!/usr/bin/env julia
 using Test
 
-include(joinpath("..", "..", "graphidx", "julia", "test", "runtests.jl"))
 include("test_queues.jl")
 include("test_tree_dp.jl")
 include("test_tree_dp.old.jl")
