0c6bf8b830b8d816e2cc7263217342dc0f4ea226
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-09-13 22:39:50 +0200

    update

--- python/test/test_tree.py
+++ python/test/test_tree.py
@@ -6,21 +6,7 @@ import os
 from treelas import Tree, TreeInstance
 
 
-def test_children3():
-    t = Tree.from_prufer([1, 2])
-    assert t.root == 3
-    assert (t.parent == [1, 2, 3, 3]).all()
-    (t.children[0] == []).all()
-    for i in range(1, 4):
-        assert (t.children[i] == [i-1]).all()
-
-
-def test_degree3():
-    t = Tree.from_prufer([1, 2])
-    assert (t.degree == [1, 2, 2, 1]).all()
-
-
-@pytest.fixture
+o@pytest.fixture
 def tree5(request):
     #         0  1  2  3  4  5  6  7  8  9
     parent = [0, 0, 1, 2, 3, 0, 7, 8, 3, 8]
