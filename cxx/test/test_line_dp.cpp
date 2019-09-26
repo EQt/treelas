4a78ce561b8d716af7b91f0e5fbecd08baffce67
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-09-26 20:09:35 +0200

    line_dp

--- cxx/test/test_line_dp.cpp
+++ cxx/test/test_line_dp.cpp
@@ -1,18 +1,20 @@
-#include <gtest/gtest.h>
+#include <doctest/doctest.h>
 #include <vector>
 
 #include "../line_dp.hpp"
 
 
-TEST(line_dp, line00)
+TEST_CASE("line_dp: line00")
 {
+    using doctest::Approx;
+
     const std::vector<double> y {1, 2, 1};
     const auto lam = ConstantWeights<>(0.1);
     std::vector<double> x;
     x.resize(y.size());
 
     line_las(y.size(), x.data(), y.data(), lam);
-    EXPECT_DOUBLE_EQ(x[0], 1.1);
-    EXPECT_DOUBLE_EQ(x[1], 1.8);
-    ASSERT_DOUBLE_EQ(x[2], 1.1);
+    CHECK(1.1 == Approx(x[0]));
+    CHECK(1.8 == Approx(x[1]));
+    REQUIRE(1.1 == Approx(x[2]));
 }
