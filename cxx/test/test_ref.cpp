b1a2093bf85271d2294f1427d90cacbd1433c6d9
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-09-26 20:08:18 +0200

    test_ref

--- cxx/test/test_ref.cpp
+++ cxx/test/test_ref.cpp
@@ -1,11 +1,11 @@
-#include <gtest/gtest.h>
+#include <doctest/doctest.h>
 #include <vector>
 #include "../event.hpp"
 #include "../range.hpp"
 
 
 
-TEST(reference, problem_clip_front)
+TEST_CASE("reference: problem_clip_front")
 {
     std::vector<Event3> elements =
         {{9, -99, 9},
@@ -17,22 +17,22 @@ TEST(reference, problem_clip_front)
     const auto ecopy (elements);
 
     Range pq {1, 3};
-    ASSERT_EQ(elements, ecopy);
+    REQUIRE(ecopy == elements);
     {   // simulate old clip_front
         auto &e = elements[pq.start];
-        ASSERT_EQ(e, ecopy[1]);
+        REQUIRE(ecopy[1] == e);
         while (pq.start <= pq.stop) {
             e = elements[++pq.start];
         }
     }
-    ASSERT_EQ(pq.start, 4);
-    ASSERT_NE(elements, ecopy);
-    ASSERT_EQ(elements, std::vector<Event3> (
-                  {ecopy[0], ecopy[4], ecopy[2], ecopy[3], ecopy[4]}));
+    REQUIRE(4 == pq.start);
+    REQUIRE(ecopy != elements);
+    REQUIRE(elements == std::vector<Event3> (
+                {ecopy[0], ecopy[4], ecopy[2], ecopy[3], ecopy[4]}));
 }
 
 
-TEST(reference, solved_clip_front)
+TEST_CASE("reference: solved_clip_front")
 {
     std::vector<Event3> elements =
         {{9, -99, 9},
@@ -44,16 +44,16 @@ TEST(reference, solved_clip_front)
     const auto ecopy (elements);
 
     Range pq {1, 3};
-    ASSERT_EQ(elements, ecopy);
+    REQUIRE(ecopy == elements);
     {   // simulate old clip_front
         const auto *e = &elements[pq.start];
-        ASSERT_EQ(*e, ecopy[1]);
+        REQUIRE(ecopy[1] == *e);
         while (pq.start <= pq.stop) {
             e = &elements[++pq.start];
         }
     }
-    ASSERT_EQ(pq.start, 4);
-    ASSERT_EQ(elements, ecopy);
+    REQUIRE(4 == pq.start);
+    REQUIRE(ecopy == elements);
 }
 
 
