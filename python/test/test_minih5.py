329efa058d2b81efe994d0ef3bf79247055e0bf4
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-09-29 22:26:56 +0200

    update graphidx

--- python/test/test_minih5.py
+++ python/test/test_minih5.py
@@ -6,7 +6,7 @@ from data import data_dir
 
 
 def load_mini_h5():
-    with h5py.File(data_dir('tree.mini.h5')) as io:
+    with h5py.File(data_dir('tree.mini.h5'), 'r') as io:
         y = io['y'][:]
         parent = io['parent'][:]
         xt = io['xt'][:]
