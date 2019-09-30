d0eea539331e953a8d19de3d4590acf7583025ca
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-09-30 15:08:46 +0200

    started to implement clip

--- rust/src/pwl.rs
+++ rust/src/pwl.rs
@@ -1,3 +1,4 @@
+use crate::generics::Bool;
 use std::ops::Range;
 
 #[derive(Debug, PartialEq)]
@@ -19,6 +20,34 @@ impl Event {
     fn offset(&self) -> f64 {
         -self.x / self.slope
     }
+}
+
+pub fn clip<Forward: Bool>(
+    elements: &mut [Event],
+    pq: &mut Range<usize>,
+    mut slope: f64,
+    mut offset: f64,
+) -> f64 {
+    let mut start = pq.start;
+    let mut stop = pq.end;
+    let mut e = &elements[if Forward::is_true() {
+        start
+    } else {
+        stop
+    }];
+    while start <= stop && slope * e.x + offset < 0.0 {
+        offset += e.offset();
+        slope += e.slope;
+        let next = if Forward::is_true() {
+            start += 1;
+            start
+        } else {
+            stop -= 1;
+            stop
+        };
+        e = &elements[next];
+    }
+    return e.x;
 }
 
 /// # Problem
