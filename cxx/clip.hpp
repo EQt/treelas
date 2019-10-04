a346ef4795a7b794cac2c3d1c33bf7bc681f9df1
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-10-04 13:46:01 +0200

    re-use code

--- cxx/clip.hpp
+++ cxx/clip.hpp
@@ -162,42 +162,9 @@ clip_fronw(
     double slope,
     double offset,
     const double t,
-    const double lower_bound)
+    const double /* lower_bound */)
 {
-//    CLIP_START();
-    const int start0 = pq.start;
-    const Event *e = &elements[start0];
-    while (pq.start <= pq.stop && slope * e->x + offset < t) {
-        offset += e->offset();
-        slope += e->slope;
-        e = &elements[++pq.start];
-    }
-
-    if (std::abs(slope) < SLOPE_EPS) {
-        return pq.start == start0 ? lower_bound : elements[--pq.start].x;
-    }
-
-    const double x = (t - offset)/slope;
-    /* // this should never happen
-    if (std::abs(slope) < SLOPE_EPS) {
-        if (pq.start <= pq.stop) {
-            x = (e-1)->x;
-        } else {
-            x = lower_bound;
-        }
-    }
-    */
-    /* -t because the next time this event will be triggered,
-       it will have included t in the offset */
-    elements[--pq.start] = Event({x, slope});
-
-// #ifdef DEBUG
-//     // if (x > upper_bound - t)
-//     //     CLIP_THROW("clip_front: x is too big");
-//     if (x < lower_bound + t)
-//         CLIP_THROW("clip_front: x is too small");
-// #endif
-    return x;
+    return clip<true, true>(elements, pq, slope, offset - t);
 }
 
 
@@ -209,28 +176,7 @@ clip_backw(
     double slope,
     double offset,
     const double t,
-    const double upper_bound)
+    const double /* upper_bound */)
 {
-    const auto stop0 = pq.stop;
-//    CLIP_START();
-    const Event *e = &elements[stop0];
-    while (pq.start <= pq.stop && slope * e->x + offset > t) {
-        offset -= e->offset();
-        slope -= e->slope;
-        e = &elements[--pq.stop];
-    }
-
-    if (std::abs(slope) < SLOPE_EPS) {
-        return pq.stop == stop0 ? upper_bound : elements[++pq.stop].x;
-    }
-
-    const double x = (t - offset)/slope;
-    elements[++pq.stop] = Event({x, -slope});
-// #ifdef DEBUG
-//     if (x > upper_bound + t)
-//         CLIP_THROW("clip_back: x is too big");
- //     // if (x < lower_bound - t)
-//     //     CLIP_THROW("clip_back: x is too big");
-// #endif
-    return x;
+    return clip<false, true>(elements, pq, -slope, -offset + t);
 }
