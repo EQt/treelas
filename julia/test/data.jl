00e09e2006ef4f0c9a14b052f474eac50ca5bc42
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-10-05 16:13:18 +0200

    return shorter filenames

--- julia/test/data.jl
+++ julia/test/data.jl
@@ -2,4 +2,4 @@ import TreeLas
 
 
 data_dir(paths...) =
-    joinpath(dirname(pathof(TreeLas)), "..", "..", "data", paths...)
+    joinpath(dirname(pathof(TreeLas)), "..", "..", "data", paths...) |> realpath
