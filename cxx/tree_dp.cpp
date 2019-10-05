3be321a5b1240cba4ebbe872b5398856cd88ecd5
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-10-05 17:24:47 +0200

    only template instantiation go into tree_dp.cpp

--- cxx/tree_dp.cpp
+++ cxx/tree_dp.cpp
@@ -1,143 +1,7 @@
 /*
   Dynamic programming solver for tree graphs.
  */
-#include <iostream>
-#include <stdexcept>
-#include <limits>
-
-#include <graphidx/bits/clamp.hpp>
-#include <graphidx/bits/finite.hpp>
-#include <graphidx/bits/minmax.hpp>
-#include <graphidx/bits/weights.hpp>
-#include <graphidx/std/stack.hpp>
-#include <graphidx/utils/timer.hpp>
-
-#include "clip.hpp"
-#include "merge.hpp"
-
-
-struct TreeDPStatus
-{
-    TreeDPStatus(const size_t n) :
-        childs(n) {
-        lb.reserve(n);
-        elements_.reserve(2*n),
-        pq.reserve(n),
-        proc_order.reserve(n);
-        dfs_stack.reserve(n);
-    }
-
-    ChildrenIndex childs;
-    stack<int> dfs_stack;
-
-    std::vector<int> proc_order;
-    std::vector<Event> elements_;
-    std::vector<Range> pq;
-    std::vector<double> lb;
-};
-
-
-template <bool merge_sort,
-          bool lazy_sort,
-          bool check = true,
-          typename Wlam,
-          typename Wmu>
-inline const double*
-tree_dp(
-    const size_t n,
-    double *x,
-    const double *y,
-    const int *parent,
-    const Wlam &lam,
-    const Wmu &mu,
-    const int root,
-    TreeDPStatus &s)
-
-{
-    auto *elements = s.elements_.data();
-    auto &pq = s.pq;
-    auto
-        *lb = s.lb.data(),
-        *ub = x;
-    auto *sig = lb;
-    const auto &childs = s.childs;
-    const auto &proc_order = s.proc_order;
-
-    {   Timer _ ("lb init");
-        std::fill(lb, lb + n, 0);
-    }
-    {   Timer _ ("children index");
-        s.childs.reset(n, parent, root);
-    }
-
-    init_queues(n, pq, s.proc_order, childs, s.dfs_stack, root);
-
-    {   Timer _ ("forward");
-        for (auto i : proc_order) {
-            const auto sig_i = sig[i];  // backup before it is set in next line
-            if (!merge_sort && lazy_sort)
-                sort_events(pq[i], elements);
-            lb[i] = clip<+1, check>(elements, pq[i],
-                                    +mu[i], -mu[i]*y[i] - sig_i + lam[i]);
-            ub[i] = clip<-1, check>(elements, pq[i],
-                                    -mu[i], +mu[i]*y[i] - sig_i + lam[i]);
-            sig[parent[i]] +=
-                (check && mu[i] <= EPS) ? std::min(lam[i], sig_i) : lam[i];
-            if (merge_sort)
-                pq[parent[i]] = merge2(pq[parent[i]], pq[i], elements);
-            else {
-                pq[parent[i]] = merge(pq[parent[i]], pq[i], elements);
-                if (!lazy_sort)
-                    sort_events(pq[parent[i]], elements);
-            }
-        }
-    }
-
-    {   Timer _ ("backtrace");
-        const auto r = root;
-        if (!merge_sort && lazy_sort)
-            sort_events(pq[r], elements);
-        x[r] = clip<+1, check>(elements, pq[r],
-                               +mu[r], -mu[r]*y[r] -sig[r] + 0.0);
-        for (long int j = (long int)(n-2); j >= 0; j--) {
-            const auto v = proc_order[j];
-            x[v] = clamp(x[parent[v]], lb[v], ub[v]);
-        }
-    }
-
-    Timer::startit("free");
-    return x;
-}
-
-
-/**
-   Paramters:
-    x   Output solution (if NULL, allocate it); x == y possible.
- */
-template <bool merge_sort,
-          bool lazy_sort,
-          bool check = true,
-          typename Wlam,
-          typename Wmu>
-inline const double*
-tree_dp(
-    const size_t n,
-    double *x,
-    const double *y,
-    const int *parent,
-    const Wlam &lam,
-    const Wmu &mu,
-    const int root)
-{
-    Timer timer ("memory alloc");
-    if (x == nullptr)
-        x = new double[n];
-    TreeDPStatus s(n);
-    timer.stop();
-    return tree_dp<merge_sort, lazy_sort, check, Wlam, Wmu>(
-        n, x, y, parent, lam, mu, root, s);
-}
-
+#include "tree_dp.hpp"
 
 template <bool merge_sort, bool lazy_sort>
 const double*
@@ -169,17 +33,6 @@ tree_dp_w(
     const double *mu,
     const int root)
 {
-    {   Timer _ ("check finite");
-        check_all_finite(y,  n, "y");
-        check_all_finite(mu, n, "mu");
-        const size_t i = find_non_finite(lam, n);
-        if (i < n && int(i) != root) {
-            throw std::runtime_error(std::string("lam[") + std::to_string(i) +
-                                     "] = " + std::to_string(lam[i]) +
-                                     " ; root = " + std::to_string(root));
-        }
-    }
-
     if (parent[root] != root) {     // verify root property
         throw std::invalid_argument(std::string("parent[") +
                                     std::to_string(root) + "] = " +
@@ -196,6 +49,7 @@ tree_dp_w(
 }
 
 
+// template instantiation
 template
 double const*
 tree_dp_w<true, true>(
@@ -220,7 +74,6 @@ tree_dp_w<false, true>(
     int);
 
 
-// template instantiation
 template
 const double*
 tree_dp<true, false>(    // tree_dp<true, true> does not make sense
--- cxx/tree_dp.hpp
+++ cxx/tree_dp.hpp
@@ -1,5 +1,142 @@
 #pragma once
 
+#include <iostream>
+#include <stdexcept>
+#include <limits>
+
+#include <graphidx/bits/clamp.hpp>
+#include <graphidx/bits/finite.hpp>
+#include <graphidx/bits/minmax.hpp>
+#include <graphidx/bits/weights.hpp>
+#include <graphidx/std/stack.hpp>
+#include <graphidx/utils/timer.hpp>
+
+#include "clip.hpp"
+#include "merge.hpp"
+
+
+struct TreeDPStatus
+{
+    TreeDPStatus(const size_t n) :
+        childs(n) {
+        lb.reserve(n);
+        elements_.reserve(2*n),
+        pq.reserve(n),
+        proc_order.reserve(n);
+        dfs_stack.reserve(n);
+    }
+
+    ChildrenIndex childs;
+    stack<int> dfs_stack;
+
+    std::vector<int> proc_order;
+    std::vector<Event> elements_;
+    std::vector<Range> pq;
+    std::vector<double> lb;
+};
+
+
+template <bool merge_sort,
+          bool lazy_sort,
+          bool check = true,
+          typename Wlam,
+          typename Wmu>
+inline const double*
+tree_dp(
+    const size_t n,
+    double *x,
+    const double *y,
+    const int *parent,
+    const Wlam &lam,
+    const Wmu &mu,
+    const int root,
+    TreeDPStatus &s)
+
+{
+    auto *elements = s.elements_.data();
+    auto &pq = s.pq;
+    auto
+        *lb = s.lb.data(),
+        *ub = x;
+    auto *sig = lb;
+    const auto &childs = s.childs;
+    const auto &proc_order = s.proc_order;
+
+    {   Timer _ ("lb init");
+        std::fill(lb, lb + n, 0);
+    }
+    {   Timer _ ("children index");
+        s.childs.reset(n, parent, root);
+    }
+
+    init_queues(n, pq, s.proc_order, childs, s.dfs_stack, root);
+
+    {   Timer _ ("forward");
+        for (auto i : proc_order) {
+            const auto sig_i = sig[i];  // backup before it is set in next line
+            if (!merge_sort && lazy_sort)
+                sort_events(pq[i], elements);
+            lb[i] = clip<+1, check>(elements, pq[i],
+                                    +mu[i], -mu[i]*y[i] - sig_i + lam[i]);
+            ub[i] = clip<-1, check>(elements, pq[i],
+                                    -mu[i], +mu[i]*y[i] - sig_i + lam[i]);
+            sig[parent[i]] +=
+                (check && mu[i] <= EPS) ? std::min(lam[i], sig_i) : lam[i];
+            if (merge_sort)
+                pq[parent[i]] = merge2(pq[parent[i]], pq[i], elements);
+            else {
+                pq[parent[i]] = merge(pq[parent[i]], pq[i], elements);
+                if (!lazy_sort)
+                    sort_events(pq[parent[i]], elements);
+            }
+        }
+    }
+
+    {   Timer _ ("backtrace");
+        const auto r = root;
+        if (!merge_sort && lazy_sort)
+            sort_events(pq[r], elements);
+        x[r] = clip<+1, check>(elements, pq[r],
+                               +mu[r], -mu[r]*y[r] -sig[r] + 0.0);
+        for (long int j = (long int)(n-2); j >= 0; j--) {
+            const auto v = proc_order[j];
+            x[v] = clamp(x[parent[v]], lb[v], ub[v]);
+        }
+    }
+
+    Timer::startit("free");
+    return x;
+}
+
+
+/**
+   Paramters:
+    x   Output solution (if NULL, allocate it); x == y possible.
+ */
+template <bool merge_sort,
+          bool lazy_sort,
+          bool check = true,
+          typename Wlam,
+          typename Wmu>
+inline const double*
+tree_dp(
+    const size_t n,
+    double *x,
+    const double *y,
+    const int *parent,
+    const Wlam &lam,
+    const Wmu &mu,
+    const int root)
+{
+    Timer timer ("memory alloc");
+    if (x == nullptr)
+        x = new double[n];
+    TreeDPStatus s(n);
+    timer.stop();
+    return tree_dp<merge_sort, lazy_sort, check, Wlam, Wmu>(
+        n, x, y, parent, lam, mu, root, s);
+}
+
 
 template <bool merge_sort, bool lazy_sort = false>
 const double*
