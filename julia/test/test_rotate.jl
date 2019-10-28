d8213323e980aa835d868ce32104023d8b62126d
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-10-28 18:30:40 +0100

    extract method

--- julia/test/test_rotate.jl
+++ julia/test/test_rotate.jl
@@ -6,28 +6,15 @@ import GraphIdx.Tree: ChildrenIndex, hierarchy_string, lowest_common_ancestors
 import GraphIdx.Tree: dfs_finish
 using Test
 
-@testset "Rotate Grid(2, 3)               " begin
-    g = GraphIdx.Grid.GridGraph(2, 3)
-    if false
-        GraphIdx.enumerate_edges(g) do i, u, v, _
-            println(i, ":", u, " ", v)
-        end
-    end
-    
-    pi = [1, 1, 1, 3, 3, 5]
-    cidx = ChildrenIndex(pi)
-    @test hierarchy_string(cidx) == """
-1
-├─2
-└─3
-  ├─4
-  └─5
-    └─6
-"""
+
+function extract_rotate(
+    g::Graph,
+    lam::Wlam,
+    pi::Vector{Int},
+    cidx::ChildrenIndex,
+)::Tuple{Vector{Float64},Vector{Float64}} where {Graph,Wlam}
     n = GraphIdx.num_nodes(g)
     m = GraphIdx.num_edges(g)
-    lam = Float64.(collect(1:m))
-
     non_tree = Vector{Pair{Int,Int}}()
     non_tree_enum = Vector{Int}()
     sizehint!(non_tree, m - n + 1)
@@ -57,12 +44,42 @@ using Test
         end
     end
 
+    tlam0 = copy(tlam)
     for v in dfs_finish(pi)
         tlam[v] += ldiff[v]
         ldiff[pi[v]] += ldiff[v]
         ldiff[v] = 0
     end
+    return tlam0, tlam
+end
+
+
+
+@testset "Rotate Grid(2, 3)               " begin
+    g = GraphIdx.Grid.GridGraph(2, 3)
+    if false
+        GraphIdx.enumerate_edges(g) do i, u, v, _
+            println(i, ":", u, " ", v)
+        end
+    end
+    
+    pi = [1, 1, 1, 3, 3, 5]
+    cidx = ChildrenIndex(pi)
+    @test hierarchy_string(cidx) == """
+1
+├─2
+└─3
+  ├─4
+  └─5
+    └─6
+"""
+    n = GraphIdx.num_nodes(g)
+    m = GraphIdx.num_edges(g)
+    lam = Float64.(collect(1:m))
+
+    tlam0, tlam = extract_rotate(g, lam, pi, cidx)
     @test tlam[2:end] ≈ [7, 10, 15, 12, 10]
+    @test tlam0[2:end] ≈ [1, 4, 2, 5, 3]
 end
 
 end
