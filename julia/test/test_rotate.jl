b1aaeda69f76cca0a2144caf1c3b7c7f18af6c06
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-10-28 18:40:32 +0100

    another test

--- julia/test/test_rotate.jl
+++ julia/test/test_rotate.jl
@@ -12,7 +12,7 @@ function extract_rotate(
     lam::Wlam,
     pi::Vector{Int},
     cidx::ChildrenIndex,
-)::Tuple{Vector{Float64},Vector{Float64}} where {Graph,Wlam}
+) where {Graph,Wlam}
     n = GraphIdx.num_nodes(g)
     m = GraphIdx.num_edges(g)
     non_tree = Vector{Pair{Int,Int}}()
@@ -30,10 +30,8 @@ function extract_rotate(
             push!(non_tree_enum, i)
         end
     end
-    @test non_tree == [2 => 4, 4 => 6]
     nidx = IncidenceIndex(n, non_tree)
     lca = lowest_common_ancestors(cidx, pi, nidx)
-    @test lca == [1, 3]
     ldiff = zeros(Float64, n)
     for v = 1:n
         for (_, i) in nidx[v]
@@ -43,16 +41,14 @@ function extract_rotate(
             end
         end
     end
-
     tlam0 = copy(tlam)
     for v in dfs_finish(pi)
         tlam[v] += ldiff[v]
         ldiff[pi[v]] += ldiff[v]
         ldiff[v] = 0
     end
-    return tlam0, tlam
+    return tlam0, tlam, non_tree, lca
 end
-
 
 
 @testset "Rotate Grid(2, 3)               " begin
@@ -77,9 +73,35 @@ end
     m = GraphIdx.num_edges(g)
     lam = Float64.(collect(1:m))
 
-    tlam0, tlam = extract_rotate(g, lam, pi, cidx)
+    tlam0, tlam, non_tree, lca = extract_rotate(g, lam, pi, cidx)
     @test tlam[2:end] ≈ [7, 10, 15, 12, 10]
     @test tlam0[2:end] ≈ [1, 4, 2, 5, 3]
+    @test non_tree == [2 => 4, 4 => 6]
+    @test lca == [1, 3]
+end
+
+
+@testset "Rotate Grid(2, 3) 2             " begin
+    g = GraphIdx.Grid.GridGraph(2, 3)
+    pi = [1, 1, 1, 6, 3, 5]
+    cidx = ChildrenIndex(pi)
+    @test hierarchy_string(cidx) == """
+1
+├─2
+└─3
+  └─5
+    └─6
+      └─4
+"""
+    n = GraphIdx.num_nodes(g)
+    m = GraphIdx.num_edges(g)
+    lam = Float64.(collect(1:m))
+
+    tlam0, tlam, non_tree, lca = extract_rotate(g, lam, pi, cidx)
+    @test non_tree == [3 => 4, 2 => 4]
+    @test lca == [3, 1]
+    @test tlam0[2:end] ≈ [1, 4, 7, 5, 3]
+    @test tlam[2:end] ≈ (tlam0[2:end] .+ 6) + [0, 0, 2, 2, 2]
 end
 
 end
