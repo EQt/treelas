70f9dbd38aea1cabd2d753796c34917b9944ca47
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-11-03 18:34:31 +0100

    also test without checks

--- cxx/test/test_line.cpp
+++ cxx/test/test_line.cpp
@@ -8,6 +8,8 @@
 
 TEST_CASE("line_las")
 {
+    using doctest::Approx;
+
     SUBCASE("line_las3")
     {
         std::vector<double> y {1.0, 0.0, 0.5};
@@ -50,13 +52,27 @@ TEST_CASE("line_las")
     }
 
 
-    SUBCASE("test_line.py: line1")
+    SUBCASE("test_line.py: line1 with checks")
     {
-        using doctest::Approx;
-
-        std::vector<double> y {1.0, 0.0, 0.5};
+        const std::vector<double> y {1.0, 0.0, 0.5};
         ConstantWeights<double> lam (0.5);
         auto x = line_las(y, lam);
+        for (size_t i = 0; i < y.size(); i++) {
+            INFO(i);
+            CHECK(Approx(0.5) == x[i]);
+        }
+    }
+
+
+    SUBCASE("test_line.py: line1 without checks")
+    {
+        const std::vector<double> y {1.0, 0.0, 0.5};
+        ConstantWeights<double> lam (0.5);
+        std::vector<double> x;
+        x.resize(y.size());
+        line_las<double, ConstantWeights<double>, UnitWeights<double>, false>(
+            y.size(), x.data(), y.data(), lam, UnitWeights<double>()
+        );
         for (size_t i = 0; i < y.size(); i++) {
             INFO(i);
             CHECK(Approx(0.5) == x[i]);
