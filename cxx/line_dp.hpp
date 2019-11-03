302c5d66a0e5d6a2f60f896e3068a57cf9d25e02
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-11-03 19:19:19 +0100

    line break

--- cxx/line_dp.hpp
+++ cxx/line_dp.hpp
@@ -80,10 +80,11 @@ line_las(
         Timer _ ("forward");
         DEBUG && printf("\n");
         for (size_t i = 0; i < n-1; i++) {
-            DEBUG && printf("mu[%d] = %f, y[i] = %f, lam0 = %f, lam[i] = %f\n", int(i), mu[i], y[i], lam0, lam[i]);
+            DEBUG && printf("mu[%d] = %f, y[i] = %f, lam0 = %f, lam[i] = %f\n",
+                            int(i), mu[i], y[i], lam0, lam[i]);
             lb[i] = clip<+1, CHECK>(el, pq, +mu[i], -mu[i]*y[i] - lam0 + lam[i]);
             ub[i] = clip<-1, CHECK>(el, pq, -mu[i], +mu[i]*y[i] - lam0 + lam[i]);
-            lam0 = (CHECK && mu[i] < EPS) ? std::min(lam0, lam[i]) : lam[i];
+            lam0 = (!CHECK || mu[i] > EPS) ? lam[i] : std::min(lam0, lam[i]);
         }
     }
     {
