34c850ec00f5f91d06589ab7da2573fa4e8bbde0
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-11-04 15:10:50 +0100

    test all implementations

--- python/test/test_line_c.py
+++ python/test/test_line_c.py
@@ -1,15 +1,21 @@
+import pytest
 import numpy as np
-from treelas import line_lasc
+from treelas import line_lasc, line_las2, line_las3
 
 
-def test_cline1():
+LINE_IMPL = [line_lasc, line_las2, line_las3]
+
+
+@pytest.mark.parametrize("line_lasc", LINE_IMPL)
+def test_cline1(line_lasc):
     y = np.array([1, 0, 0.5])
     lam = 0.5
     x = line_lasc(y, lam)
     assert (x == 0.5).all(), repr(x)
 
 
-def test_cline2():
+@pytest.mark.parametrize("line_lasc", LINE_IMPL)
+def test_cline2(line_lasc):
     """with pre-allocated output array"""
     y = np.array([1, 0, 0.5])
     lam = 0.5
@@ -18,7 +24,8 @@ def test_cline2():
     assert (x == 0.5).all()
 
 
-def test_cline3():
+@pytest.mark.parametrize("line_lasc", LINE_IMPL)
+def test_cline3(line_lasc):
     """different lambda"""
     y = np.array([1, 0, 0.5])
     lam = 0.1
