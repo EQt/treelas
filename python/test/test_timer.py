77b94c78cdb17d2406fb41fcbb479b31b1ee11e5
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-11-05 12:32:08 +0100

    make it a proper unit test

--- python/test/test_timer.py
+++ python/test/test_timer.py
@@ -1,5 +1,12 @@
-import treelas._treelas as t
+from treelas._treelas import timer
+import treelas._treelas as tl
 
-t._pass_timer_ref()
-tim = t.Timer()
-t._pass_timer_ref(tim)
+
+def test_timer():
+    assert tl._pass_timer_ref() <= 0
+    with timer.TimerQuiet():
+        tim = timer.Timer()
+        d = tl._pass_timer_ref(tim)
+        assert d >= 0.05
+        assert d <= 0.05 * 1.01
+        assert d == float(tim)
