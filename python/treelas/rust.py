fa9e0f016ea20cd79e8e3a93a12812aca3040504
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-11-05 20:21:34 +0100

    access the rust library

---
+++ python/treelas/rust.py
@@ -0,0 +1,45 @@
+"""
+Wrapper to the Rust library
+"""
+import ctypes
+from os import path
+import numpy as np
+
+
+_libname = "libtreelars.so"
+_libfname = path.join(path.dirname(__file__),
+                     "..", "..", "rust", "target", "release", _libname)
+
+if path.exists(_libfname):
+    _lib = ctypes.CDLL(_libfname)
+    _lib.line_las.restype = ctypes.c_double
+    _lib.line_las.argtypes = [
+        ctypes.c_size_t,
+        ctypes.POINTER(ctypes.c_double),
+        ctypes.POINTER(ctypes.c_double),
+        ctypes.c_double,
+    ]
+
+    def line_las(
+            y: np.ndarray,
+            lam: float,
+            out: np.ndarray = None,
+            verbose: bool = False,
+    ) -> np.ndarray:
+        y = y.reshape(-1).astype(float)
+        if out is None:
+            out = np.empty_like(y)
+        else:
+            assert out.dtype == np.dtype(float)
+            assert out.shape == y.shape
+        duration = _lib.line_las(
+            len(y),
+            out.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
+            y.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
+            lam
+        )
+        if verbose:
+            print(duration/len(y))
+        return out
+
+    
