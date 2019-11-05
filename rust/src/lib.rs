6d096c38e2d643419e989fcef7f4b907afb123f5
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-11-05 19:35:45 +0100

    provide extern C interface

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,3 +3,23 @@ pub mod instance;
 pub mod line;
 pub mod pwl;
 pub mod tree;
+
+
+#[no_mangle]
+pub extern "C" fn line_las(
+    n: usize,
+    x: *mut f64,
+    y: *const f64,
+    lam: f64,
+) -> f64 {
+    let mut x: &mut [f64] = unsafe { std::slice::from_raw_parts_mut(x, n) };
+    let y = unsafe { std::slice::from_raw_parts(y, n) };
+    let lam = graphidx::weights::ConstantWeights::new(lam);
+    let mu = graphidx::weights::ConstantWeights::new(1.0);
+    let mut solver = line::LineDP::new(y.len());
+    solver.solve::<_, _, generics::False>(&mut x, &y, &lam, &mu);
+    0.1
+}
+
+
+
