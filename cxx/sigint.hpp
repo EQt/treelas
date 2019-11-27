18ef36806f5687a16bd22a56adeea8f0a01a65f6
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-11-27 17:26:04 +0100

    some research on how to catch [Ctrl]-[C] signals in pybind11

---
+++ cxx/sigint.hpp
@@ -0,0 +1,8 @@
+/* Deal with [Ctrl-C] interruptions
+
+   https://config9.com/linux/allowing-ctrl-c-to-interrupt-a-python-c-extension/
+
+   https://stackoverflow.com/a/53134263
+
+   https://github.com/NERSC/timemory/search?q=sigint&unscoped_q=sigint
+*/
