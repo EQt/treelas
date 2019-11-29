29c79201cc5fe03416b59088af42e13055e595e2
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2019-11-30 00:26:37 +0100

    outsource

--- python/treelas/linelas.py
+++ python/treelas/linelas.py
@@ -9,6 +9,7 @@ from pprint import pformat as _fmt
 from typing import Optional
 from graphidx.weights import Ones, Weights
 from .rounder import _fround, _int_or_round
+from .queue import DeQue
 
 
 DEBUG = False
@@ -35,44 +36,6 @@ class Event:
     @staticmethod
     def intersect(e1: Event, e2: Event) -> Optional[float]:
         return (e2.offset() - e1.offset()) / (e1.slope - e2.slope)
-
-
-class DeQue:
-    """
-    Double ended queue
-    """
-    def __init__(self, n=None):
-        from collections import deque
-        from collections.abc import Iterable
-        self._e = deque(n) if isinstance(n, Iterable) else deque() 
-
-    def pop(self, forward: bool = True):
-        return self._e.popleft() if forward else self._e.pop()
-
-    def peek(self, forward: bool = True):
-        return self._e[0 if forward else -1]
-
-    def push(self, x, forward: bool = True):
-        if forward:
-            self._e.appendleft(x)
-        else:
-            self._e.append(x)
-
-    def __bool__(self) -> bool:
-        return bool(self._e)
-
-    def __len__(self) -> len:
-        return len(self._e)
-
-    def __repr__(self) -> str:
-        name = type(self).__name__
-        amen = type(self._e).__name__
-        r = repr(self._e)
-        assert r.startswith(amen)
-        return name + r[len(amen):]
-
-    def __iter__(self):
-        return iter(self._e)
 
 
 def clip(elem: DeQue[Event], slope: float, offset: float, forward: bool) -> float:
---
+++ python/treelas/queue.py
@@ -0,0 +1,36 @@
+class DeQue:
+    """
+    Double ended queue
+    """
+    def __init__(self, n=None):
+        from collections import deque
+        from collections.abc import Iterable
+        self._e = deque(n) if isinstance(n, Iterable) else deque() 
+
+    def pop(self, forward: bool = True):
+        return self._e.popleft() if forward else self._e.pop()
+
+    def peek(self, forward: bool = True):
+        return self._e[0 if forward else -1]
+
+    def push(self, x, forward: bool = True):
+        if forward:
+            self._e.appendleft(x)
+        else:
+            self._e.append(x)
+
+    def __bool__(self) -> bool:
+        return bool(self._e)
+
+    def __len__(self) -> len:
+        return len(self._e)
+
+    def __repr__(self) -> str:
+        name = type(self).__name__
+        amen = type(self._e).__name__
+        r = repr(self._e)
+        assert r.startswith(amen)
+        return name + r[len(amen):]
+
+    def __iter__(self):
+        return iter(self._e)
