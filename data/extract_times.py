a9b121b9bf8449c3d12342d1c10af678c5e14832
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2020-01-30 23:29:40 +0100

    start to collect times

--- data/extract_times.py
+++ data/extract_times.py
@@ -7,6 +7,7 @@ def extract_time_n(fname):
     time_ms = float(re.match(r' --> +(\d+\.\d+)ms', last_line).group(1))
     base, _, alg = fname.rpartition('.')
     tl_fname = re.match('((.*/)*[a-z]+_\d+)*', base).group(1) + ".treelas"
-    num_nodes = len(h5py.File(tl_fname)["parent"])
+    with h5py.File(tl_fname, 'r') as io:
+        num_nodes = len(io["parent"])
     time_per_node_ns = time_ms * 1e-3 /  num_nodes * 1e9
     return time_per_node_ns, num_nodes
