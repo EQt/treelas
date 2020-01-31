f8cfbe19a1cd21bd17495deaa421bb79009fe455
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2020-01-31 01:50:47 +0100

    don't depend on HDF5

--- data/extract_times.py
+++ data/extract_times.py
@@ -1,16 +1,14 @@
 import re
-import h5py
 import pandas as pd
+from os import path
 
 
 def extract_time_n(fname):
-    first_line = open(fname).readlines()[0]
     last_line = open(fname).readlines()[-1]
     time_ms = float(re.match(r' --> +(\d+\.\d+)ms', last_line).group(1))
     base, _, alg = fname.rpartition('.')
-    tl_fname = first_line
-    with h5py.File(tl_fname, 'r') as io:
-        num_nodes = len(io["parent"])
+    num_nodes = int(re.search(
+        " *n = ([0-9_]+)", open(fname).read()).group(1))
     time_per_node_ns = time_ms * 1e-3 /  num_nodes * 1e9
     return time_per_node_ns, num_nodes
 
