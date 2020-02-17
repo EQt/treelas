25d2fa1eef68974878b58c9f4c454fef7cd789f2
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2020-02-17 18:42:09 +0100

    skip test on windows

--- julia/test/test_cxx.jl
+++ julia/test/test_cxx.jl
@@ -14,11 +14,13 @@ const cxx_jl = joinpath(dirname(pathof(TreeLas)), "cxx.jl")
     syms = Set((String(m.captures[1])
                 for m in eachmatch(r"(_Z\w+)", read(open(cxx_jl), String))))
 
-    Libdl.dlopen(Cxx.lib) do tl
-        @testset "Check DLL symbols" begin
-            for s in syms
-                @testset "Check symbol $s" begin
-                    @test Libdl.dlsym_e(tl, s) != Ptr{Nothing}()
+    @static if !Sys.iswindows()
+        Libdl.dlopen(Cxx.lib) do tl
+            @testset "Check DLL symbols" begin
+                for s in syms
+                    @testset "Check symbol $s" begin
+                        @test Libdl.dlsym_e(tl, s) != Ptr{Nothing}()
+                    end
                 end
             end
         end
