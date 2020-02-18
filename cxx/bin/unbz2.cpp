dab8d11e8a4d17586542437518d52a58bc733625
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2020-02-18 12:26:19 +0100

    Make unbz2 usable

--- cxx/bin/unbz2.cpp
+++ cxx/bin/unbz2.cpp
@@ -1,20 +1,34 @@
+#include <cstdio>
 #include <fstream>
-#include <string>      // stoi
-#include <memory>      // unique_ptr
+#include <string>         // stoi
+#include <memory>         // unique_ptr
+#include <argparser.hpp>
 #include <graphidx/io/bz2istream.hpp>
 
 
 int
 main(int argc, char *argv[])
 {
-    const char *infn = "belgium.bz2";
-    const char *outfn = "belgium.txt";
-    if (argc > 1)
-        infn = argv[1];
-    if (argc > 2)
-        outfn = argv[2];
-    const int buf_size = argc > 3 ? std::stoi(argv[3]) : 2*4096;
-    const bool only_read = argc > 4;
+
+    ArgParser ap (
+        "unbz2 <infile> [outfile]\n"
+        "\n"
+        "Decompress <infile>"
+    );
+    ap.add_option('r', "only-read", "Don't write (for benchmarking)");
+    ap.add_option('b', "buf-size",  "Buffer size [8192]", "INT", "8192");
+    ap.parse(&argc, argv);
+
+    if (argc <= 1) {
+        fprintf(stderr, "No input file to decompress!\n\nUSAGE:\n");
+        ap.print_usage(stderr);
+        return 1;
+    }
+
+    const char *infn = argv[1];
+    const char *outfn =  (argc >= 2) ? argv[2] : "out.txt";
+    const int buf_size = atoi(ap.get_option("buf-size"));
+    const bool only_read = ap.has_option("only-read");
 
     BZ2IStream io (infn, buf_size);
     {
