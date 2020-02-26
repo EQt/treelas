60503c8428f9a9df3c49c9ec45c48b635357a77f
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2020-02-26 16:15:13 +0100

    reproduce that bug

---
+++ python/test/test_randomspan.py
@@ -0,0 +1,28 @@
+import pytest
+import numpy as np
+from graphidx import GridGraph
+from graphidx.idx import BiAdjacent
+from graphidx import random_spanning_tree
+from treelas import tree_dp
+
+
+def test_grid_3x3(width):
+    from graphidx.tree import Tree
+
+    t = Tree(pi)
+    t.show()
+    graph = BiAdjacent(GridGraph(3, 3))
+    pi = random_spanning_tree(graph, seed=0)
+    assert all(pi == [3, 0, 1, 6, 7, 8, 6, 6, 7])
+    np.random.seed(0)
+    y = np.random.randn(len(pi))
+    x = tree_dp(y=y, parent=pi, lam=0.2)
+
+
+@pytest.mark.parametrize("width", [3, 10, 50, 100, 200])
+def test_grid(width):
+    graph = BiAdjacent(GridGraph(width, width))
+    pi = random_spanning_tree(graph, seed=0)
+    np.random.seed(0)
+    y = np.random.randn(len(pi))
+    x = tree_dp(y=y, parent=pi, lam=0.2)
