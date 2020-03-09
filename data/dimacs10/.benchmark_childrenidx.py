eff063838c6f3b8f7c8890e1dd37e8809d0184ec
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2020-03-09 15:08:37 +0100

    proper function

--- data/dimacs10/.benchmark_childrenidx.py
+++ data/dimacs10/.benchmark_childrenidx.py
@@ -4,24 +4,29 @@ from graphidx.py.children import PyChildrenIndex
 from graphidx.timer import Timer
 
 
-fname = "europe_0.tree"
+def benchmark(fname: str):
+    with Timer("load hdf5"):
+        with h5py.File(fname) as io:
+            pi = io["parent"][()]
 
-with Timer("load hdf5"):
-    with h5py.File(fname) as io:
-        pi = io["parent"][()]
+    with Timer("pybind", end="\n"):
+        (idx, vals) = groupby(pi, verbose=True)
 
-with Timer("pybind", end="\n"):
-    (idx, vals) = groupby(pi, verbose=True)
+    with Timer("pybind", end="\n"):
+        cidx = ChildrenIndex(pi, verbose=True)
 
-with Timer("pybind", end="\n"):
-    cidx = ChildrenIndex(pi, verbose=True)
+    with Timer("pybind", end="\n"):
+        cidx2 = ChildrenIndex([0]).reset(pi, verbose=True)
 
-with Timer("pybind", end="\n"):
-    cidx2 = ChildrenIndex([0]).reset(pi, verbose=True)
+    with Timer("PyChildrenIndex", end="\n"):
+        pidx = PyChildrenIndex.compute(pi)
 
-with Timer("PyChildrenIndex", end="\n"):
-    pidx = PyChildrenIndex.compute(pi)
+    return (idx, vals), pidx
 
 
-assert all(idx == pidx.idx+1)
-assert all(vals[1:] == pidx.pi[:-1])
+if __name__ == '__main__':
+    (idx, vals), pidx = benchmark("europe_0.tree")
+
+    with Timer("check index"):
+        assert all(idx == pidx.idx+1)
+        assert all(vals[1:] == pidx.pi[:-1])
