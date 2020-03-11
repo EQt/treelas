c2d2f0cc6e475a0eb8a404581b7acd8ff0e3f2ee
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2020-03-11 16:59:40 +0100

    windows tests should work, again

--- ci/setup_julia.ps1
+++ ci/setup_julia.ps1
@@ -1,5 +1,5 @@
 julia -e "import Pkg; Pkg.develop(Pkg.PackageSpec(path=\`"deps/graphidx/julia\`"))"
 julia -e "import Pkg; Pkg.develop(Pkg.PackageSpec(path=\`"julia\`"))"
 julia -e "import Pkg; Pkg.instantiate()"
-# julia -e "import Pkg; Pkg.build(\`"TreeLas\`")"
-# type C:\projects\treelas\julia\deps\build.log
+julia -e "import Pkg; Pkg.build(\`"TreeLas\`")"
+type C:\projects\treelas\julia\deps\build.log
