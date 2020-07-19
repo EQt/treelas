87c7856e167ce862959d8f71f04733dee16f1162
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2020-07-19 15:46:30 +0200

    better usage help

--- cxx/bin/grid.cpp
+++ cxx/bin/grid.cpp
@@ -51,9 +51,9 @@ main(int argc, char *argv[])
             "Generate edges of grid graph and store them as HDF5 file"
         );
         ap.add_option('o', "out", "Output file", "FILE", "grid.h5");
-        ap.add_option('i', "img", "Image (HDF5) => sizes", "FILE");
-        ap.add_option('w', "width", "Image (HDF5) => sizes", "INT", "5");
-        ap.add_option('h', "height", "Image (HDF5) => sizes", "INT", "3");
+        ap.add_option('i', "img", "Take dimensions from HDF5 file", "FILE");
+        ap.add_option('w', "width", "Width (number of columns)", "INT", "5");
+        ap.add_option('h', "height", "Height (number of rows)", "INT", "3");
         ap.parse(&argc, argv);
 
         int n1 = -1, n2 = -2;
