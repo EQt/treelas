00117037c356cf1f310eee16d521a607a51561cf
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2020-07-24 17:11:34 +0200

    typo

--- julia/test/runtests.jl
+++ julia/test/runtests.jl
@@ -12,7 +12,7 @@ include("test_tree0.jl")
 include("test_mgt.jl")
 include("test_dual.jl")
 include("test_gaplax.jl")
-include("test_cyclegaplas.jl")
+include("test_cyclgaplas.jl")
 include("test_cyclegap.jl")
 
 # include("test_tree_dp.old.jl")
