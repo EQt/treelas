bc051785b77c946cf97691f63f8bb8b1e6b508ea
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2020-07-24 12:37:13 +0200

    rename it

--- julia/src/cyclegap.jl
+++ julia/src/cyclegap.jl
@@ -30,7 +30,7 @@ struct GapMem{N}
     gamma::Vector{Float64}
     tree_lam::Vector{Float64}
     dp_mem::TreeDPMem
-    mst_mem::PrimMstMem
+    mst::PrimMstMem
     wgraph::WeightedGraph
     tree::RootedTree
     tree_alpha::Vector{Float64}
@@ -45,9 +45,9 @@ function GapMem(y::Array{Float64,N}, graph::Graph, lambda::Weights{Float64}) whe
     tree_lam = Vector{Float64}(undef, n)
     tree_alpha = tree_lam
     egraph = collect(graph)
-    mst_mem = PrimMstMem(egraph)
+    mst = PrimMstMem(egraph)
     lam = Float64[lambda[i] for i=1:m]
-    tree = RootedTree(root_node, mst_mem.parent)
+    tree = RootedTree(root_node, mst.parent)
     GapMem(
         copy(y),                        # x
         similar(y),                     # z
@@ -55,8 +55,8 @@ function GapMem(y::Array{Float64,N}, graph::Graph, lambda::Weights{Float64}) whe
         Vector{Float64}(undef, m),      # gamma
         tree_lam,
         TreeDPMem(n),
-        mst_mem,
-        WeightedGraph(mst_mem.neighbors, lam),
+        mst,
+        WeightedGraph(mst.neighbors, lam),
         tree,
         tree_alpha,
         egraph,
@@ -87,11 +87,11 @@ function gaplas!(
     mu::W2
 ) where {N, GraphT<:Graph, W2<:Weights{Float64}}
     gap_vec!(mem.gamma, mem.x, mem.alpha, mem.wgraph, -1.0)
-    prim_mst_edges(mem.gamma, mem.tree.root, mem.mst_mem)
+    prim_mst_edges(mem.gamma, mem.tree.root, mem.mst)
     mem.z .= y
     extract_non_tree!(
         graph,
-        mem.mst_mem.parent,
+        mem.mst.parent,
         mem.z,
         mem.alpha,
         mem.tree_lam,
@@ -99,8 +99,8 @@ function gaplas!(
     )
     tree_dp!(mem.x, mem.z, mem.tree, GraphIdx.Vec(mem.tree_lam), mu, mem.dp_mem)
     mem.tree_alpha .= vec(mem.x) .- vec(mem.z)
-    dual!(mem.tree_alpha, mem.dp_mem.proc_order, mem.mst_mem.parent)
-    update_tree!(mem.alpha, mem.tree_alpha, mem.mst_mem.selected, mem.egraph, mem.mst_mem.parent)
+    dual!(mem.tree_alpha, mem.dp_mem.proc_order, mem.mst.parent)
+    update_tree!(mem.alpha, mem.tree_alpha, mem.mst.selected, mem.egraph, mem.mst.parent)
 
 end
 
