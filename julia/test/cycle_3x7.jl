6abca170b5de0ddf3723af703fd2a113e4d8d00e
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2020-07-28 18:11:36 +0200

    make it a function

--- julia/test/cycle_3x7.jl
+++ julia/test/cycle_3x7.jl
@@ -7,21 +7,25 @@ import GraphIdx.Grid: GridGraph
 import TreeLas: Cycle
 import TreeLas: CycleGap
 
-include("demo3x7.jl")
 
-lambda = Const(1.0)
-graph = GridGraph(size(y)...)
-cmem = CycleGap.CycMem(y, graph, lambda);
-mem = cmem.gap_mem;
-CycleGap.gap_vec!(mem.gamma, mem.x, mem.alpha, mem.wgraph, -1.0)
-CycleGap.prim_mst_edges(mem.gamma, mem.tree.root, mem.mst)
-cmem.cycles = Cycle.CycleBasis(graph, mem.mst.parent)
-tlam0, tlam = Cycle.extract_rotate(cmem.cycles, lambda)
-cycles = cmem.cycles
+function show(y, lambda = Const(1.0))
+    graph = GridGraph(size(y)...)
+    cmem = CycleGap.CycMem(y, graph, lambda);
+    mem = cmem.gap_mem;
+    CycleGap.gap_vec!(mem.gamma, mem.x, mem.alpha, mem.wgraph, -1.0)
+    CycleGap.prim_mst_edges(mem.gamma, mem.tree.root, mem.mst)
+    cmem.cycles = Cycle.CycleBasis(graph, mem.mst.parent)
+    tlam0, tlam = Cycle.extract_rotate(cmem.cycles, lambda)
+    cycles = cmem.cycles
+
+    GraphViz.dot(graph, cycles, i -> "[ label=\"$(Int(tlam[i]))\"]")
+end
 
 
-GraphViz.dot(graph, cycles, i -> "[ label=\"$(Int(tlam[i]))\"]")
-
+@time tasks = [@async show(randn(size)) for size in [(3, 7), (4, 6), (4, 6)]]
+for t in tasks
+    wait(t)
+end
 
 
 end#module
