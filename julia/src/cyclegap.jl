e5fbf0c46cfd1c4582b73944c58ed4bdca9c6980
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2020-07-29 15:34:36 +0200

    extract method

--- julia/src/cyclegap.jl
+++ julia/src/cyclegap.jl
@@ -128,13 +128,9 @@ function gaplas(
 end
 
 
-function gaplas!(
-    mem::GapMem{N, W1},
-    y::Array{Float64,N},
-    graph::GraphT,
-    lambda::Weights{Float64},
-    mu::W2,
-) where {N, GraphT<:Graph, W1<:Weights{Float64}, W2<:Weights{Float64}}
+function _extract_tree!(
+    mem::GapMem{N, W1}, y::Array{Float64,N}, graph::Graph, lambda::Weights{Float64}
+) where {N, W1<:Weights{Float64}}
     gap_vec!(mem.gamma, mem.x, mem.alpha, mem.wgraph, -1.0)
     prim_mst_edges(mem.gamma, mem.tree.root, mem.mst)
     mem.z .= y
@@ -146,6 +142,17 @@ function gaplas!(
         mem.tree_lam,
         lambda,
     )
+end
+
+
+function gaplas!(
+    mem::GapMem{N, W1},
+    y::Array{Float64,N},
+    graph::GraphT,
+    lambda::Weights{Float64},
+    mu::W2,
+) where {N, GraphT<:Graph, W1<:Weights{Float64}, W2<:Weights{Float64}}
+    _extract_tree!(mem, y, graph, lambda)
     tree_dp!(mem.x, mem.z, mem.tree, mem.tree_lam, mu, mem.dp_mem)
     mem.tree_alpha .= vec(mem.x) .- vec(mem.z)
     dual!(mem.tree_alpha, mem.dp_mem.proc_order, mem.mst.parent)
@@ -167,17 +174,7 @@ function gaplas!(
     mu::W2,
 ) where {N, GraphT<:Graph, W1<:Weights{Float64}, W2<:Weights{Float64}}
     mem = cmem.gap_mem
-    gap_vec!(mem.gamma, mem.x, mem.alpha, mem.wgraph, -1.0)
-    prim_mst_edges(mem.gamma, mem.tree.root, mem.mst)
-    mem.z .= y
-    extract_non_tree!(
-        graph,
-        mem.mst.parent,
-        mem.z,
-        mem.alpha,
-        mem.tree_lam,
-        lambda,
-    )
+    _extract_tree!(mem, y, graph, lambda)
 
     cmem.cycles = Cycle.CycleBasis(graph, mem.mst.parent)
     tlam0, tlam = Cycle.extract_rotate(cmem.cycles, lambda)
