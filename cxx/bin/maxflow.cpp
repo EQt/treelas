dc73fa4a921cd625664dd8f6249c7ca5f6a6a09a
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2020-07-31 18:02:00 +0200

    another example

--- cxx/bin/maxflow.cpp
+++ cxx/bin/maxflow.cpp
@@ -7,6 +7,7 @@
     http://www.andres.sc/graph.html
  */
 #include <iostream>
+#include <tuple>
 #include <vector>
 #include <lemon/static_graph.h>
 #include <lemon/preflow.h>
@@ -17,7 +18,7 @@ std::vector<bool>
 min_cut(
     const size_t n,
     const Arcs arcs,
-    const float_t capacities[],
+    const float_t *capacities,
     const int source,
     const int target)
 {
@@ -41,8 +42,20 @@ min_cut(
 }
 
 
-int
-main()
+void
+print_min_cut(const std::vector<bool> &above)
+{
+    std::cout << "min_cut = {";
+    for (size_t i = 0; i < above.size(); i++)
+        if (above[i]) {
+            std::cout << " " << i << ",";
+        }
+    std::cout << "}" << std::endl;
+}
+
+
+void
+test_example1()
 {
     /*
         ->(3)\
@@ -63,19 +76,45 @@ main()
         {3, 2}, // 5
     };
     double capacities[] = {5, 10, 10, 15, 5};
-    auto above = min_cut(
-        n,
-        arcs,
-        capacities,
-        0,
-        2);
+    auto above = min_cut(n, arcs, capacities, 0, 2);
+    print_min_cut(above);
+}
 
-    std::cout << "above = {";
-    for (int i = 0; i < (int)n; i++)
-        if (above[i]) {
-            std::cout << " " << i << ",";
-        }
-    std::cout << "}" << std::endl;
+
+void
+test_example2()
+{
+    // http://chengw1005.blogspot.com/2015/11/graph-network-flow-problems.html
+    std::vector<std::tuple<int, int, double>> warcs {
+        {0, 2, 13},
+        {0, 1, 16},
+        {1, 2, 10},
+        {1, 3, 12},
+        {2, 1, 4},
+        {2, 4, 14},
+        {3, 2, 9},
+        {3, 5, 20},
+        {4, 3, 7},
+        {4, 5, 4}};
+
+    std::vector<std::pair<int, int>> arcs;
+    std::vector<double> cap;
+    for (auto &wa: warcs) {
+        arcs.push_back(std::make_pair(std::get<0>(wa), std::get<1>(wa)));
+        cap.push_back(std::get<2>(wa));
+    }
+    const int source = 0, target = 5;
+    const size_t n = 6;
+    auto above = min_cut(n, arcs, cap.data(), source, target);
+    print_min_cut(above);
+}
+
+
+int
+main()
+{
+    // test_example1();
+    test_example2();
     return 0;
 }
 
