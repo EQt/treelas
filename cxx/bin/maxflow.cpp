47ce477ba54193d652c51adfbf17597f988015e4
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2020-08-01 16:21:36 +0200

    outsource min_cut algorithm

--- cxx/bin/maxflow.cpp
+++ cxx/bin/maxflow.cpp
@@ -1,45 +1,7 @@
-/*
-  Useful resources
-    https://gist.github.com/huanyud/45f98d8bf8d6df66d3e7ab3e9a85af90
-    https://github.com/zxqfl/flow/blob/master/bindings.cpp
-
-  Alternative library?
-    http://www.andres.sc/graph.html
- */
 #include <iostream>
 #include <tuple>
-#include <vector>
-#include <lemon/static_graph.h>
-#include <lemon/preflow.h>
 
-
-template <typename Arcs, typename float_t = double>
-std::vector<bool>
-min_cut(
-    const size_t n,
-    const Arcs arcs,
-    const float_t *capacities,
-    const int source,
-    const int target)
-{
-    using Graph = lemon::StaticDigraph;
-    using MaxFlow = lemon::Preflow<Graph, Graph::ArcMap<float_t>>;
-
-    const auto m = arcs.size();
-    Graph graph;
-    graph.build(n, arcs.begin(), arcs.end());
-    Graph::ArcMap<double> capacity(graph);
-    for (int i = 0; i < int(m); i++)
-        capacity[graph.arcFromId(i)] = capacities[i];
-
-    MaxFlow mf (graph, capacity, graph.nodeFromId(source), graph.nodeFromId(target));
-    mf.runMinCut();
-
-    std::vector<bool> above (n, false);
-    for (int i = 0; i < (int)n; i++)
-        above[i] = mf.minCut(graph.nodeFromId(i));
-    return above;
-}
+#include "min_cut.hpp"
 
 
 void
