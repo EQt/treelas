79cd99457997519f679f049aa1f98b86bd211603
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2020-09-09 17:27:57 +0200

    mark whole function as unsafe (suggested by clippy)

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,19 +5,22 @@ pub mod line;
 pub mod pwl;
 pub mod tree;
 
+/// # Safety
+/// Will crash if `x` or `y` does not point to an array of at least `n` times `f64`
+/// elements
 #[no_mangle]
-pub extern "C" fn line_las(
+pub unsafe extern "C" fn line_las(
     n: usize,
     x: *mut f64,
     y: *const f64,
     lam: f64,
 ) -> f64 {
-    let mut x: &mut [f64] = unsafe { std::slice::from_raw_parts_mut(x, n) };
-    let y = unsafe { std::slice::from_raw_parts(y, n) };
+    let mut x: &mut [f64] = std::slice::from_raw_parts_mut(x, n);
+    let y = std::slice::from_raw_parts(y, n);
     let lam = graphidx::weights::Const::new(lam);
     let mu = graphidx::weights::Ones::<f64>::new();
     let mut solver = line::LineDP::new(y.len());
     let start_time = Instant::now();
-    solver.solve::<_, _>(&mut x, &y, &lam, &mu);
+    solver.solve(&mut x, &y, &lam, &mu);
     start_time.elapsed().as_secs_f64()
 }
