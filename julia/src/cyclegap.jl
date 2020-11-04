fcbe0fdba842ff5aadcd1e84ba71302616cb4d82
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2020-11-04 16:21:15 +0100

    some documentation

--- julia/src/cyclegap.jl
+++ julia/src/cyclegap.jl
@@ -1,3 +1,7 @@
+"""
+Test an idea about a variant of iterated gap trees:
+Include the non-tree edges by rotate the capacities in the associated cycles.
+"""
 module CycleGap
 
 import Printf: @printf
