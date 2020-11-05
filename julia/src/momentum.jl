b28f8cfc4531732d1dcb63d27189771baaeef0bf
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2020-11-05 16:14:35 +0100

    implemented simple learning rate

--- julia/src/momentum.jl
+++ julia/src/momentum.jl
@@ -4,23 +4,31 @@ Implement a variant of MGT using momentum accelaration.
 *Experimental*
 """
 module Momentum
+import GraphIdx
 import GraphIdx: Graph, Weights
 import TreeLas: GapLas
 import TreeLas.GapLas: gaplas, GapMem, gaplas!
 
 
 struct MomMem{N, WL<:Weights{Float64}}
+    learn::Float64
     x::Array{Float64, N}
+    z::Array{Float64, N}
+    α::Vector{Float64}
     gmem::GapMem{N, WL}
     gamma::Vector{Float64}
 end
 
 
 function MomMem(y::Array{Float64,N}, graph::Graph, lambda::Weights{Float64}) where {N}
+    m = GraphIdx.num_edges(graph)
     gmem = GapMem(y, graph, lambda)
     gamma = gmem.gamma
     x = gmem.x
-    MomMem(x, gmem, gamma)
+    z = similar(x)
+    α = similar(gmem.alpha)
+    learn = 0.5
+    MomMem(learn, x, z, α, gmem, gamma)
 end
 
 
@@ -31,7 +39,14 @@ function gaplas!(
     lambda::Weights{Float64},
     mu::Weights{Float64},
 ) where {N, W1<:Weights{Float64}}
+    mem.z .= mem.gmem.x
+    mem.α .= mem.gmem.alpha
     GapLas.gaplas!(mem.gmem, y, graph, lambda, mu)
+    let x = mem.gmem.x, α = mem.gmem.alpha, β = mem.α, learn = mem.learn
+        @. x = learn * x + (1 - learn) * mem.z
+        @. α = learn * α + (1 - learn) * β
+    end
 end
+
 
 end # CycleGap
