e568886009fa181bc15711c72ae75e361d423a47
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2020-11-05 11:10:38 +0100

    move all remaining stuff into GapLas module that is unrelated to CycleGap

--- julia/src/cyclegap.jl
+++ julia/src/cyclegap.jl
@@ -7,19 +7,18 @@ Include the non-tree edges by rotate the capacities in the associated cycles.
 """
 module CycleGap
 
-import Printf: @printf
-
 import GraphIdx
 import GraphIdx: Graph, EdgeGraph, Edge, Weights, WeightedGraph
 import GraphIdx: PrimMstMem, prim_mst_edges
 import GraphIdx.Tree: RootedTree
 
-import TreeLas.GapLas: GapMem
 import TreeLas.TreeDP: TreeDPMem, tree_dp!
-import TreeLas.Dual: dual!, gap_vec!
-import TreeLas.MGT: extract_non_tree!, update_tree!
+import TreeLas.Dual: dual!
+import TreeLas.MGT: update_tree!
 import TreeLas: Cycle
 import TreeLas.Cycle: CycleBasis
+import TreeLas: GapLas
+import TreeLas.GapLas: GapMem, gaplas
 
 
 mutable struct CycMem{N, WL<:Weights{Float64}}
@@ -42,28 +41,6 @@ function CycMem(
         CycleBasis(GraphIdx.num_edges(graph), GraphIdx.num_nodes(graph)),
         tree_lam,
     )
-end
-
-
-function gaplas(
-    y::Array{Float64,N},
-    graph::GraphT,
-    lambda::Weights{Float64},
-    mu::W2,
-    ::Type{Mem} = GapMem;
-    max_iter::Int = 5,
-    verbose::Bool = true,
-)::Array{Float64,N} where {N, GraphT<:Graph, W2<:Weights{Float64}, Mem}
-    mem = Mem(y, graph, lambda)
-    for it in 1:max_iter
-        gaplas!(mem, y, graph, lambda, mu)
-        if verbose
-            let gap = -sum(mem.gamma)
-                @printf("%4d %12.4f\n", it, gap)
-            end
-        end
-    end
-    return mem.x
 end
 
 
@@ -93,45 +70,7 @@ function gaplas(
 end
 
 
-function _extract_tree!(
-    mem::GapMem{N, W1}, y::Array{Float64,N}, graph::Graph, lambda::Weights{Float64}
-) where {N, W1<:Weights{Float64}}
-    gap_vec!(mem.gamma, mem.x, mem.alpha, mem.wgraph, -1.0)
-    prim_mst_edges(mem.gamma, mem.tree.root, mem.mst)
-    mem.z .= y
-    extract_non_tree!(
-        graph,
-        mem.mst.parent,
-        mem.z,
-        mem.alpha,
-        mem.tree_lam,
-        lambda,
-    )
-end
-
-
-function gaplas!(
-    mem::GapMem{N, W1},
-    y::Array{Float64,N},
-    graph::Graph,
-    lambda::Weights{Float64},
-    mu::Weights{Float64},
-) where {N, W1<:Weights{Float64}}
-    _extract_tree!(mem, y, graph, lambda)
-    tree_dp!(mem.x, mem.z, mem.tree, mem.tree_lam, mu, mem.dp_mem)
-    mem.tree_alpha .= vec(mem.x) .- vec(mem.z)
-    dual!(mem.tree_alpha, mem.dp_mem.proc_order, mem.mst.parent)
-    update_tree!(
-        mem.alpha,
-        mem.tree_alpha,
-        mem.mst.selected,
-        mem.egraph,
-        mem.mst.parent,
-    )
-end
-
-
-function gaplas!(
+function GapLas.gaplas!(
     cmem::CycMem{N, W1},
     y::Array{Float64,N},
     graph::GraphT,
@@ -139,7 +78,7 @@ function gaplas!(
     mu::W2,
 ) where {N, GraphT<:Graph, W1<:Weights{Float64}, W2<:Weights{Float64}}
     mem = cmem.gap_mem
-    _extract_tree!(mem, y, graph, lambda)
+    GapLas._extract_tree!(mem, y, graph, lambda)
 
     cmem.cycles = Cycle.CycleBasis(graph, mem.mst.parent)
     tlam0, tlam = Cycle.extract_rotate(cmem.cycles, lambda)
--- julia/src/gaplas.jl
+++ julia/src/gaplas.jl
@@ -3,11 +3,14 @@ Re-factoring and re-implementation of MGT
 """
 module GapLas
 
+import Printf: @printf
 import GraphIdx
 import GraphIdx: Graph, EdgeGraph, Edge, Weights, WeightedGraph
 import GraphIdx: PrimMstMem, prim_mst_edges
 import GraphIdx.Tree: RootedTree
 import TreeLas.TreeDP: TreeDPMem, tree_dp!
+import TreeLas.Dual: dual!, gap_vec!
+import TreeLas.MGT: extract_non_tree!, update_tree!
 
 
 struct GapMem{N, WL<:Weights{Float64}}
@@ -48,6 +51,80 @@ function GapMem(y::Array{Float64,N}, graph::Graph, lambda::Weights{Float64}) whe
         tree,
         tree_alpha,
         egraph,
+    )
+end
+
+
+"""
+General framework for iterated tree optimizer:
+1. Allocate memory (of type `Mem`)
+2. Call `gaplas!(mem, y, graph, λ, μ)` in every iteration (`max_iter` many times).
+
+!!! note
+
+    `mem` must contain a `gamma` field
+
+"""
+function gaplas(
+    y::Array{Float64,N},
+    graph::GraphT,
+    lambda::Weights{Float64},
+    mu::W2,
+    ::Type{Mem} = GapMem;
+    max_iter::Int = 5,
+    verbose::Bool = true,
+)::Array{Float64,N} where {N, GraphT<:Graph, W2<:Weights{Float64}, Mem}
+    mem = Mem(y, graph, lambda)
+    for it in 1:max_iter
+        gaplas!(mem, y, graph, lambda, mu)
+        if verbose
+            let gap = -sum(mem.gamma)
+                @printf("%4d %12.4f\n", it, gap)
+            end
+        end
+    end
+    return mem.x
+end
+
+
+"""
+Called in every iteration.
+Implements the actual optimization, i.e.
+"""
+function gaplas!(
+    mem::GapMem{N, W1},
+    y::Array{Float64,N},
+    graph::Graph,
+    lambda::Weights{Float64},
+    mu::Weights{Float64},
+) where {N, W1<:Weights{Float64}}
+    _extract_tree!(mem, y, graph, lambda)
+    tree_dp!(mem.x, mem.z, mem.tree, mem.tree_lam, mu, mem.dp_mem)
+    mem.tree_alpha .= vec(mem.x) .- vec(mem.z)
+    dual!(mem.tree_alpha, mem.dp_mem.proc_order, mem.mst.parent)
+    update_tree!(
+        mem.alpha,
+        mem.tree_alpha,
+        mem.mst.selected,
+        mem.egraph,
+        mem.mst.parent,
+    )
+end
+
+
+function _extract_tree!(
+    mem::GapMem{N, W1}, y::Array{Float64,N}, graph::Graph, lambda::Weights{Float64}
+) where {N, W1<:Weights{Float64}}
+    gap_vec!(mem.gamma, mem.x, mem.alpha, mem.wgraph, -1.0)
+    prim_mst_edges(mem.gamma, mem.tree.root, mem.mst)
+    mem.z .= y
+    extract_non_tree!(
+        graph,
+        mem.mst.parent,
+        mem.z,
+        mem.alpha,
+        mem.tree_lam,
+        lambda,
     )
 end
 
