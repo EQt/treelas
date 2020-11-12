7cb7ce9f5978f843ca026846168db80af2fccf50
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2020-11-12 17:53:25 +0100

    include some tests

--- julia/test/test_momentum.jl
+++ julia/test/test_momentum.jl
@@ -14,6 +14,8 @@ lam = GraphIdx.Const(0.1)
 @testset "Momentum Demo 3x7              " begin
     graph = GraphIdx.Grid.GridGraph(n1, n2)
     x = Momentum.gaplas(copy(y), graph, lam, max_iter=10, Momentum.MomMem, verbose=true)
+    @test all(minimum(y) .<= x)
+    @test all(maximum(y) .>= x)
     if false    # compare to old implementation?
         x2 = MGT.gaplas(copy(y), graph, lam, max_iter=10, learn=0.85, verbose=true)
     end
