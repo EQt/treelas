85f80f98ea98ddc684306ac11ea0694df7b58ac2
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2020-12-09 22:46:10 +0100

    prefer CMakeWrapper to CMake

--- julia/deps/build.jl
+++ julia/deps/build.jl
@@ -1,5 +1,5 @@
 import Libdl
-import CMake:cmake
+import CMakeWrapper:cmake_executable
 
 const build_dir = joinpath(dirname(@__FILE__), "build")
 const root_dir = joinpath(dirname(@__FILE__), "..", "..")
@@ -19,8 +19,8 @@ if !isdir(build_dir)
 end
 
 cd(build_dir) do
-    run(`$cmake $root_dir $cmake_opts`)
-    run(`$cmake --build . --target $target --config $build_config`)
+    run(`$cmake_executable $root_dir $cmake_opts`)
+    run(`$cmake_executable --build . --target $target --config $build_config`)
     @static if Sys.iswindows()
         @assert isfile(joinpath(build_config, "treelas.$(Libdl.dlext)"))
     else
