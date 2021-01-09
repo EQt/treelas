169a0cb7c63baeea752d20a62bab913ca744c433
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2021-01-09 15:25:32 +0100

    format!

--- cxx/edges.hpp
+++ cxx/edges.hpp
@@ -2,31 +2,30 @@
    Additional functions to handle edges.
  */
 #pragma once
-#include "graphidx/idx/incidence.hpp"
 #include <functional>
-
+#include <graphidx/idx/incidence.hpp>
 #include <graphidx/idx/biadjacent.hpp>
 
 
-template <typename int_t = int>
+template <typename int_t>
 void
-edges(const BiAdjacentIndex<int_t> &idx, std::function<void(int_t, int_t)> f)
+edges(const IncidenceIndex<int_t> &idx, std::function<void(int_t, int_t, int_t)> f)
 {
-    for (int_t v = 0; v < (int_t) idx.num_nodes(); v++) {
-	for (int_t u : idx[v]) {
-	    f(v, u);
-	}
+    for (int_t u = 0; u < (int_t)idx.num_nodes(); u++) {
+        for (const auto [v, ei] : idx[u]) {
+            f(u, v, ei);
+        }
     }
 }
 
 
-template <typename int_t = int>
+template <typename int_t>
 void
-edges(const IncidenceIndex<int_t> &idx, std::function<void(int_t, int_t, int_t)> f)
+edges(const BiAdjacentIndex<int_t> &idx, std::function<void(int_t, int_t)> f)
 {
-    for (int_t u = 0; u < (int_t) idx.num_nodes(); u++) {
-	for (const auto [v, ei] : idx[u]) {
-	    f(u, v, ei);
-	}
+    for (int_t v = 0; v < (int_t)idx.num_nodes(); v++) {
+        for (int_t u : idx[v]) {
+            f(v, u);
+        }
     }
 }
