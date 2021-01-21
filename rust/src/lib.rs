749aeaa3bfc72f78beb9bbbc2a5af92421c619aa
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2021-01-21 20:42:12 +0100

    visibility...

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,20 +1,16 @@
+pub use graphidx;
 use std::time::Instant;
-pub mod generics;
+mod generics;
 pub mod instance;
 pub mod line;
 pub mod pwl;
 pub mod tree;
 
 /// # Safety
-/// Will crash if `x` or `y` does not point to an array of at least `n` times `f64`
+/// Will crash if `x` or `y` do not point to an array of at least `n` times `f64`
 /// elements
 #[no_mangle]
-pub unsafe extern "C" fn line_las(
-    n: usize,
-    x: *mut f64,
-    y: *const f64,
-    lam: f64,
-) -> f64 {
+pub unsafe extern "C" fn line_las(n: usize, x: *mut f64, y: *const f64, lam: f64) -> f64 {
     let mut x: &mut [f64] = std::slice::from_raw_parts_mut(x, n);
     let y = std::slice::from_raw_parts(y, n);
     let lam = graphidx::weights::Const::new(lam);
