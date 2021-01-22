fa04f24240423ac8f898df15ef622b6893bbe264
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2021-01-22 19:50:09 +0100

    fn line_las_f32 in libtreelars

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -22,3 +22,16 @@ pub unsafe extern "C" fn line_las(n: usize, x: *mut f64, y: *const f64, lam: f64
     solver.solve(&mut x, &y, &lam, &mu);
     start_time.elapsed().as_secs_f64()
 }
+
+/// Like `line_las` but with 32bit float arithmetic, underneath.
+#[no_mangle]
+pub unsafe extern "C" fn line_las_f32(n: usize, x: *mut f32, y: *const f32, lam: f32) -> f64 {
+    let mut x: &mut [f32] = std::slice::from_raw_parts_mut(x, n);
+    let y = std::slice::from_raw_parts(y, n);
+    let lam = graphidx::weights::Const::new(lam);
+    let mu = graphidx::weights::Ones::<f32>::new();
+    let mut solver = line_32::LineDP::new(y.len());
+    let start_time = Instant::now();
+    solver.solve(&mut x, &y, &lam, &mu);
+    start_time.elapsed().as_secs_f64()
+}
