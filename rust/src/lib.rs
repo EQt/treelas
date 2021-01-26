fe407cc5ff2a25490f8e1596b9f45a4db9694daf
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2021-01-26 10:59:21 +0100

    make float public

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,6 +1,6 @@
 pub use graphidx;
 use std::time::Instant;
-mod float;
+pub mod float;
 mod generics;
 pub mod instance;
 pub mod line;
@@ -13,12 +13,7 @@ pub mod tree;
 /// Will crash if `x` or `y` do not point to an array of at least `n` times `f64`
 /// elements
 #[no_mangle]
-pub unsafe extern "C" fn line_las(
-    n: usize,
-    x: *mut f64,
-    y: *const f64,
-    lam: f64,
-) -> f64 {
+pub unsafe extern "C" fn line_las(n: usize, x: *mut f64, y: *const f64, lam: f64) -> f64 {
     let mut x: &mut [f64] = std::slice::from_raw_parts_mut(x, n);
     let y = std::slice::from_raw_parts(y, n);
     let lam = graphidx::weights::Const::new(lam);
@@ -31,12 +26,7 @@ pub unsafe extern "C" fn line_las(
 
 /// Like `line_las` but with 32bit float arithmetic, underneath.
 #[no_mangle]
-pub unsafe extern "C" fn line_las_f32(
-    n: usize,
-    x: *mut f32,
-    y: *const f32,
-    lam: f32,
-) -> f64 {
+pub unsafe extern "C" fn line_las_f32(n: usize, x: *mut f32, y: *const f32, lam: f32) -> f64 {
     let mut x: &mut [f32] = std::slice::from_raw_parts_mut(x, n);
     let y = std::slice::from_raw_parts(y, n);
     let lam = graphidx::weights::Const::new(lam);
