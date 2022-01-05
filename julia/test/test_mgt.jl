84e81e0e60d3c15fe9e16aa2244cb8f54023cb8b
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2022-01-05 21:51:48 +0100

    new syntax in Julia 1.7

--- julia/test/test_mgt.jl
+++ julia/test/test_mgt.jl
@@ -33,7 +33,7 @@ end
     root = 1
     s = collect(1:n) |> x -> reshape(x, n1, n2)
     @test prettyarr(s) == """
-    3×7 Array{Int64,2}:
+    3×7 Matrix{Int64}:
      1  4  7  10  13  16  19
      2  5  8  11  14  17  20
      3  6  9  12  15  18  21
