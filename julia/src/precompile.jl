313f860466cbb0bc28d4737266bf1b9a9c343bd7
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2022-01-06 20:45:53 +0100

    strange constants changed with new Julia versions

--- julia/src/precompile.jl
+++ julia/src/precompile.jl
@@ -1,5 +1,3 @@
-isdefined(TreeLas, Symbol("##gaplas#19")) && precompile(Tuple{getfield(TreeLas.MGT, Symbol("##gaplas#19")), Base.Iterators.Pairs{Symbol, Real, Tuple{Symbol, Symbol}, NamedTuple{(:max_iter, :learn), Tuple{Int64, Float64}}}, typeof(TreeLas.MGT.gaplas), Array{Float64, 2}, Array{Tuple{Int64, Int64}, 1}, Array{Float64, 1}})
-isdefined(TreeLas, Symbol("#gaplas##kw")) && precompile(Tuple{getfield(TreeLas.MGT, Symbol("#gaplas##kw")), NamedTuple{(:max_iter, :learn), Tuple{Int64, Float64}}, typeof(TreeLas.MGT.gaplas), Array{Float64, 2}, Array{Tuple{Int64, Int64}, 1}, Array{Float64, 1}})
 precompile(Tuple{typeof(TreeLas.Dual.dual!), Array{Float64, 1}, Array{Int64, 1}, Array{Int64, 1}, Float64})
 precompile(Tuple{typeof(TreeLas.MGT.duality_check), Array{Float64, 1}, Array{Float64, 1}})
 precompile(Tuple{typeof(TreeLas.MGT.update_tree!), Array{Float64, 1}, Array{Float64, 1}, Array{Int64, 1}, GraphIdx.EdgeGraph, Array{Int64, 1}})
